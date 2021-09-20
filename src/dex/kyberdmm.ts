47e9472a167f9f1af96c3a8e9423c5d7b1460691
Nazar Duchak <nazar@iovlabs.org>
2021-09-20 20:10:32 +0300

    feat: kyber dmm

---
+++ src/dex/kyberdmm.ts
@@ -0,0 +1,103 @@
+import { Interface, JsonFragment } from '@ethersproject/abi';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { SwapSide } from '../constants';
+import { AdapterExchangeParam, Address, SimpleExchangeParam } from '../types';
+import { IDex } from './idex';
+import { SimpleExchange } from './simple-exchange';
+import KyberDmmABI from '../abi/kyberdmm.abi.json';
+
+export type KyberDmmData = {
+  router: Address;
+  path: Address[];
+  pools: {
+    address: Address;
+    direction: boolean;
+    fee: number;
+  }[];
+  factory: Address;
+};
+type KyberDmmParam = [
+  srcAmount: string,
+  destToken: string,
+  pools: string[],
+  path: string[],
+  to: string,
+  deadline: string,
+];
+
+enum KyberDMMFunctions {
+  swapExactTokensForTokens = 'swapExactTokensForTokens',
+  swapExactTokensForETH = 'swapExactTokensForETH',
+}
+
+export class KyberDmm
+  extends SimpleExchange
+  implements IDex<KyberDmmData, KyberDmmParam>
+{
+  static dexKeys = ['kyberdmm'];
+  exchangeRouterInterface: Interface;
+  needWrapNative = true;
+
+  constructor(
+    augustusAddress: Address,
+    private network: number,
+    provider: JsonRpcProvider,
+  ) {
+    super(augustusAddress, provider);
+    this.exchangeRouterInterface = new Interface(KyberDmmABI as JsonFragment[]);
+  }
+
+  getAdapterParam(
+    srcToken: string,
+    destToken: string,
+    srcAmount: string,
+    destAmount: string,
+    data: KyberDmmData,
+    side: SwapSide,
+  ): AdapterExchangeParam {
+    const payload = this.abiCoder.encodeParameter(
+      {
+        ParentStruct: {
+          poolPath: 'address[]',
+        },
+      },
+      { poolPath: [data.pools[0].address] },
+    );
+    return {
+      targetExchange: data.router,
+      payload,
+      networkFee: '0',
+    };
+  }
+
+  async getSimpleParam(
+    srcToken: string,
+    destToken: string,
+    srcAmount: string,
+    destAmount: string,
+    data: KyberDmmData,
+    side: SwapSide,
+  ): Promise<SimpleExchangeParam> {
+    const swapFunctionParams: KyberDmmParam = [
+      srcAmount,
+      destAmount,
+      [data.pools[0].address],
+      data.path,
+      this.augustusAddress,
+      Number.MAX_SAFE_INTEGER.toString(),
+    ];
+    const swapData = this.exchangeRouterInterface.encodeFunctionData(
+      KyberDMMFunctions.swapExactTokensForTokens,
+      swapFunctionParams,
+    );
+
+    return this.buildSimpleParamWithoutWETHConversion(
+      srcToken,
+      srcAmount,
+      destToken,
+      destAmount,
+      swapData,
+      data.router,
+    );
+  }
+}
