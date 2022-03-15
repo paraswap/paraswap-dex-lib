87f7978badbe5d3237c8df4d075b9bd8cd600283
Verisana <account@0112.pro>
2022-03-15 18:33:43 +0100

    feat: create types for weth

--- src/dex/weth/types.ts
+++ src/dex/weth/types.ts
@@ -1,21 +1,11 @@
-import { Address } from '../../types';
-
-export type PoolState = {
-  // TODO: poolState is the state of event 
-  // subsrciber. This should be the minimum
-  // set of parameters required to compute 
-  // pool prices. Complete me!
-};
-
-export type WethData = {
-  // TODO: WethData is the dex data that is
-  // returned by the API that can be used for 
-  // tx building. The data structure should be minimal.
-  // Complete me!
-};
+export type WethData = {};
 
 export type DexParams = {
-  // TODO: DexParams is set of parameters the can
-  // be used to initiate a DEX fork. 
-  // Complete me! 
+  contractAddress: string;
+  poolGasCost: number;
+};
+
+export enum WethFunctions {
+  deposit = 'deposit',
+  withdraw = 'withdraw',
 }
