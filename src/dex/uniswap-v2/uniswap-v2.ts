63a176ed090dc1a5b861fa5efb30c44052d04a0b
Shresth Agrawal <shresthagrawal.31@gmail.com>
2022-03-16 11:47:36 -0400

    Fix limitPools in UniswapV2

--- src/dex/uniswap-v2/uniswap-v2.ts
+++ src/dex/uniswap-v2/uniswap-v2.ts
@@ -546,6 +546,10 @@ export class UniswapV2
       const from = wrapETH(_from, this.network);
       const to = wrapETH(_to, this.network);
 
+      if (from.address.toLowerCase() === to.address.toLowerCase()) {
+        return null;
+      }
+
       const tokenAddress = [
         from.address.toLowerCase(),
         to.address.toLowerCase(),
@@ -555,15 +559,8 @@ export class UniswapV2
 
       const poolIdentifier = `${this.dexKey}_${tokenAddress}`;
 
-      if (
-        limitPools &&
-        (limitPools.length !== 1 || limitPools[0] !== poolIdentifier)
-      )
+      if (limitPools && limitPools.every(p => p !== poolIdentifier))
         return null;
-
-      if (from.address.toLowerCase() === to.address.toLowerCase()) {
-        return null;
-      }
 
       await this.batchCatchUpPairs([[from, to]], blockNumber);
 
