6b6f884946264343c96858ee5b8e4a783f63efe9
Verisana <account@0112.pro>
2022-03-18 15:44:17 +0100

    test: move Balancer e2e tests

--- src/dex/balancer-v1/balancer-v1-e2e.test.ts
+++ src/dex/balancer-v1/balancer-v1-e2e.test.ts
@@ -25,17 +25,17 @@ import { JsonRpcProvider } from '@ethersproject/providers';
           - Token -> ETH swap
           - Token -> Token swap
 
-  The template already enumerates the basic structure which involves 
+  The template already enumerates the basic structure which involves
   testing simpleSwap, multiSwap, megaSwap contract methods for
-  ETH <> TOKEN and TOKEN <> TOKEN swaps. You should replace tokenA and 
+  ETH <> TOKEN and TOKEN <> TOKEN swaps. You should replace tokenA and
   tokenB with any two highly liquid tokens on BalancerV1 for the tests
-  to work. If the tokens that you would like to use are not defined in 
+  to work. If the tokens that you would like to use are not defined in
   Tokens or Holders map, you can update the './tests/constants-e2e'
 
-  Other than the standard cases that are already added by the template 
-  it is highly recommended to add test cases which could be specific 
-  to testing BalancerV1 (Eg. Tests based on poolType, special tokens, 
-  etc). 
+  Other than the standard cases that are already added by the template
+  it is highly recommended to add test cases which could be specific
+  to testing BalancerV1 (Eg. Tests based on poolType, special tokens,
+  etc).
 
   You can run this individual test script by running:
   `npx jest src/dex/<dex-name>/<dex-name>-e2e.tests.ts`
@@ -43,76 +43,192 @@ import { JsonRpcProvider } from '@ethersproject/providers';
   (This comment should be removed from the final implementation)
 */
 
-describe('BalancerV1 E2E', () => {
-  describe('BalancerV1 MAINNET', () => {
-    const dexKey = 'BalancerV1';
-    const network = Network.MAINNET;
-    const tokens = Tokens[network];
-    const holders = Holders[network];
-    const provider = new JsonRpcProvider(ProviderURL[network]);
-    // TODO: Modify the tokenASymbol, tokenBSymbol, tokenAAmount;
-    const tokenASymbol: string = 'tokenASymbol';
-    const tokenBSymbol: string = 'tokenBSymbol';
-    const tokenAAmount: string = 'tokenAAmount';
-    const tokenBAmount: string = 'tokenBAmount';
-    const ethAmount = '1000000000000000000';
-    // TODO: Add any direct swap contractMethod name if it exists
-    // TODO: If buy is not supported remove the buy contract methods
-    const contractMethods: { [side: sell]: ContractMethod } = {
-      [SwapSide.SELL]: [
+// Balancer doesn't support USDT
+describe('BalancerV1 E2E Mainnet', () => {
+  const dexKey = 'BalancerV1';
+  const network = Network.MAINNET;
+  const tokens = Tokens[network];
+  const holders = Holders[network];
+  const provider = new JsonRpcProvider(ProviderURL[network]);
+
+  describe('Simpleswap SELL', () => {
+    it('ETH -> USDC', async () => {
+      await testE2E(
+        tokens.ETH,
+        tokens.USDC,
+        holders.ETH,
+        '7000000000000000000',
+        SwapSide.SELL,
+        dexKey,
         ContractMethod.simpleSwap,
+        network,
+        provider,
+      );
+    });
+    it('USDC -> ETH', async () => {
+      await testE2E(
+        tokens.USDC,
+        tokens.ETH,
+        holders.USDC,
+        '2000000000',
+        SwapSide.SELL,
+        dexKey,
+        ContractMethod.simpleSwap,
+        network,
+        provider,
+      );
+    });
+    it('USDC -> LINK', async () => {
+      await testE2E(
+        tokens.USDC,
+        tokens.LINK,
+        holders.USDC,
+        '2000000000',
+        SwapSide.SELL,
+        dexKey,
+        ContractMethod.simpleSwap,
+        network,
+        provider,
+      );
+    });
+    it('WBTC -> LINK', async () => {
+      await testE2E(
+        tokens.WBTC,
+        tokens.LINK,
+        holders.WBTC,
+        '200000000',
+        SwapSide.SELL,
+        dexKey,
+        ContractMethod.simpleSwap,
+        network,
+        provider,
+      );
+    });
+  });
+
+  describe('Multiswap SELL', () => {
+    it('ETH -> WBTC', async () => {
+      await testE2E(
+        tokens.ETH,
+        tokens.WBTC,
+        holders.ETH,
+        '7000000000000000000',
+        SwapSide.SELL,
+        dexKey,
         ContractMethod.multiSwap,
-        ContractMethod.megaSwap,
-      ],
-      [SwapSide.BUY]: [ContractMethod.simpleBuy, ContractMethod.buy],
-    };
+        network,
+        provider,
+      );
+    });
+    it('USDC -> ETH', async () => {
+      await testE2E(
+        tokens.USDC,
+        tokens.ETH,
+        holders.USDC,
+        '2000000000',
+        SwapSide.SELL,
+        dexKey,
+        ContractMethod.multiSwap,
+        network,
+        provider,
+      );
+    });
+    it('USDC -> WBTC', async () => {
+      await testE2E(
+        tokens.USDC,
+        tokens.WBTC,
+        holders.USDC,
+        '2000000000',
+        SwapSide.SELL,
+        dexKey,
+        ContractMethod.multiSwap,
+        network,
+        provider,
+      );
+    });
+  });
 
-    [SwapSide.SELL, SwapSide.BUY].forEach((side: SwapSide) =>
-      sellContractMethods.forEach((contractMethod: ContractMethod) => {
-        describe(`${contractMethod}`, () => {
-          it('ETH -> TOKEN', async () => {
-            await testE2E(
-              tokens['ETH'],
-              tokens[tokenASymbol],
-              holders['ETH'],
-              side === SwapSide.SELL ? ethAmount : tokenAAmount,
-              side,
-              dexKey,
-              contractMethod,
-              network,
-              provider,
-            );
-          });
-          it('TOKEN -> ETH', async () => {
-            await testE2E(
-              tokens[tokenASymbol],
-              tokens['ETH'],
-              holders[tokenASymbol],
-              side === SwapSide.SELL ? tokenAAmount: ethAmount,
-              side,
-              dexKey,
-              contractMethod,
-              network,
-              provider,
-            );
-          });
-          it('TOKEN -> TOKEN', async () => {
-            await testE2E(
-              tokens[tokenASymbol],
-              tokens[tokenBSymbol],
-              holders[tokenASymbol],
-              side === SwapSide.SELL ? tokenAAmount: tokenBAmount,
-              side,
-              dexKey,
-              contractMethod,
-              network,
-              provider,
-            );
-          });
-        });
-      }),
-    );
+  describe('SimpleBuy BUY', () => {
+    it('ETH -> WBTC', async () => {
+      await testE2E(
+        tokens.ETH,
+        tokens.WBTC,
+        holders.ETH,
+        '35000000',
+        SwapSide.BUY,
+        dexKey,
+        ContractMethod.simpleBuy,
+        network,
+        provider,
+      );
+    });
+    it('USDC -> ETH', async () => {
+      await testE2E(
+        tokens.USDC,
+        tokens.ETH,
+        holders.USDC,
+        '1000000000000000000',
+        SwapSide.BUY,
+        dexKey,
+        ContractMethod.simpleBuy,
+        network,
+        provider,
+      );
+    });
+    it('USDC -> WETH', async () => {
+      await testE2E(
+        tokens.USDC,
+        tokens.WETH,
+        holders.USDC,
+        '1000000000000000000',
+        SwapSide.BUY,
+        dexKey,
+        ContractMethod.simpleBuy,
+        network,
+        provider,
+      );
+    });
+  });
 
-    // TODO: Add any aditional test cases required to test BalancerV1
+  describe('Buy BUY', () => {
+    it('ETH -> WBTC', async () => {
+      await testE2E(
+        tokens.ETH,
+        tokens.WBTC,
+        holders.ETH,
+        '35000000',
+        SwapSide.BUY,
+        dexKey,
+        ContractMethod.buy,
+        network,
+        provider,
+      );
+    });
+    it('USDC -> ETH', async () => {
+      await testE2E(
+        tokens.USDC,
+        tokens.ETH,
+        holders.USDC,
+        '1000000000000000000',
+        SwapSide.BUY,
+        dexKey,
+        ContractMethod.buy,
+        network,
+        provider,
+      );
+    });
+    it('USDC -> WETH', async () => {
+      await testE2E(
+        tokens.USDC,
+        tokens.WETH,
+        holders.USDC,
+        '1000000000000000000',
+        SwapSide.BUY,
+        dexKey,
+        ContractMethod.buy,
+        network,
+        provider,
+      );
+    });
   });
 });
