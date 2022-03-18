04366a80b99bafe9180ac425a9aa6cef67383e4f
Verisana <account@0112.pro>
2022-03-18 19:13:47 +0100

    feat: create new types for BalancerV1

--- src/dex/balancer-v1/types.ts
+++ src/dex/balancer-v1/types.ts
@@ -5,26 +5,93 @@ export interface Token {
   address: string;
   balance: BigNumber;
   decimals: number;
-  denomWeight: BigNumber;
+  denormWeight: BigNumber;
 }
 
-export interface Pool {
+export interface SubgraphPoolBase {
+  id: string;
+  address: string;
+  tokens: Token[];
+}
+
+export type PoolState = {
   id: string;
   swapFee: BigNumber;
   totalWeight: BigNumber;
   tokens: Token[];
   tokensList: string[];
-}
-
-export type PoolState = {
-  [address: string]: Pool;
 };
 
-export type BalancerV1Data = {
+export type PoolStateMap = { [address: string]: PoolState };
+
+export type BalancerSwaps = {
   pool: Address;
+  tokenInParam: string;
+  tokenOutParam: string;
+  maxPrice: string;
+}[];
+
+export type BalancerV1Data = {
   exchangeProxy: Address;
+  pool: Address;
+};
+
+export type OptimizedBalancerV1Data = {
+  exchangeProxy: Address;
+  swaps: BalancerSwaps;
 };
 
 export type DexParams = {
   subgraphURL: string;
 };
+
+type BalancerBatchEthInSwapExactInParam = [
+  swaps: BalancerSwaps,
+  destToken: string,
+  destAmount: string,
+];
+type BalancerBatchEthOutSwapExactInParam = [
+  swaps: BalancerSwaps,
+  srcToken: string,
+  srcAmount: string,
+  destAmount: string,
+];
+type BalancerBatchSwapExactInParam = [
+  swaps: BalancerSwaps,
+  srcToken: string,
+  destToken: string,
+  srcAmount: string,
+  destAmount: string,
+];
+type BalancerBatchEthInSwapExactOutParam = [
+  swaps: BalancerSwaps,
+  destToken: string,
+];
+type BalancerBatchEthOutSwapExactOutParam = [
+  swaps: BalancerSwaps,
+  srcToken: string,
+  maxTotalAmountIn: string,
+];
+type BalancerBatchSwapExactOutParam = [
+  swaps: BalancerSwaps,
+  srcToken: string,
+  destToken: string,
+  maxTotalAmountIn: string,
+];
+
+export type BalancerParam =
+  | BalancerBatchEthInSwapExactInParam
+  | BalancerBatchEthOutSwapExactInParam
+  | BalancerBatchSwapExactInParam
+  | BalancerBatchEthInSwapExactOutParam
+  | BalancerBatchEthOutSwapExactOutParam
+  | BalancerBatchSwapExactOutParam;
+
+export enum BalancerFunctions {
+  batchEthInSwapExactIn = 'batchEthInSwapExactIn',
+  batchEthOutSwapExactIn = 'batchEthOutSwapExactIn',
+  batchSwapExactIn = 'batchSwapExactIn',
+  batchEthInSwapExactOut = 'batchEthInSwapExactOut',
+  batchEthOutSwapExactOut = 'batchEthOutSwapExactOut',
+  batchSwapExactOut = 'batchSwapExactOut',
+}
