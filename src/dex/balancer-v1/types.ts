848ce83376e6dc484d2fd11810b43324ff94cec0
Verisana <account@0112.pro>
2022-03-18 14:46:36 +0100

    feat: update types for BalancerV1

--- src/dex/balancer-v1/types.ts
+++ src/dex/balancer-v1/types.ts
@@ -1,21 +1,30 @@
 import { Address } from '../../types';
+import BigNumber from 'bignumber.js';
+
+export interface Token {
+  address: string;
+  balance: BigNumber;
+  decimals: number;
+  denomWeight: BigNumber;
+}
+
+export interface Pool {
+  id: string;
+  swapFee: BigNumber;
+  totalWeight: BigNumber;
+  tokens: Token[];
+  tokensList: string[];
+}
 
 export type PoolState = {
-  // TODO: poolState is the state of event 
-  // subsrciber. This should be the minimum
-  // set of parameters required to compute 
-  // pool prices. Complete me!
+  [address: string]: Pool;
 };
 
 export type BalancerV1Data = {
-  // TODO: BalancerV1Data is the dex data that is
-  // returned by the API that can be used for 
-  // tx building. The data structure should be minimal.
-  // Complete me!
+  pool: Address;
+  exchangeProxy: Address;
 };
 
 export type DexParams = {
-  // TODO: DexParams is set of parameters the can
-  // be used to initiate a DEX fork. 
-  // Complete me! 
-}
+  subgraphURL: string;
+};
