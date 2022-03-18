4aff559cc2a14cbb3d1bc9074949242806ffebc4
Verisana <account@0112.pro>
2022-03-18 14:46:23 +0100

    feat: update configs for BalancerV1

--- src/dex/balancer-v1/config.ts
+++ src/dex/balancer-v1/config.ts
@@ -1,13 +1,21 @@
 import { DexParams } from './types';
 import { DexConfigMap } from '../../types';
 import { Network } from '../../constants';
+import { SwapSide } from 'paraswap-core';
 
 export const BalancerV1Config: DexConfigMap<DexParams> = {
   BalancerV1: {
-    // TODO: complete me!
-  }
+    [Network.MAINNET]: {
+      subgraphURL:
+        'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer',
+    },
+  },
 };
 
-export const Adapters: { [chainId: number]: { name: string; index: number }[] | null } = {
-  // TODO: add adapters for each chain 
+export const Adapters: {
+  [chainId: number]: {
+    [side: string]: { name: string; index: number }[];
+  };
+} = {
+  [Network.MAINNET]: { [SwapSide.SELL]: [{ name: 'Adapter01', index: 5 }] },
 };
