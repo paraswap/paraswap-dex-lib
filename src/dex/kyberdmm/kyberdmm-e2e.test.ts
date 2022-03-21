86885a5bd53cbfa84da7de5737ecc599f1337ed8
turcumanhoria <turcumanhoria@gmail.com>
2022-03-21 21:56:48 +0000

    Fix tests

--- src/dex/kyberdmm/kyberdmm-e2e.test.ts
+++ src/dex/kyberdmm/kyberdmm-e2e.test.ts
@@ -90,219 +90,219 @@ describe('KyberDmm E2E', () => {
     );
   });
 
-  // describe('KyberDmm POLYGON', () => {
-  //   const network = Network.POLYGON;
-  //   const tokens = Tokens[network];
-  //   const holders = Holders[network];
-  //   const provider = new JsonRpcProvider(ProviderURL[network]);
+  describe('KyberDmm POLYGON', () => {
+    const network = Network.POLYGON;
+    const tokens = Tokens[network];
+    const holders = Holders[network];
+    const provider = new JsonRpcProvider(ProviderURL[network]);
 
-  //   const tokenASymbol: string = 'USDT';
-  //   const tokenBSymbol: string = 'BUSD';
-  //   const nativeTokenSymbol = NativeTokenSymbols[network];
+    const tokenASymbol: string = 'DAI';
+    const tokenBSymbol: string = 'USDC';
+    const nativeTokenSymbol = NativeTokenSymbols[network];
 
-  //   const tokenAAmount: string = '700000000000000000000';
-  //   const tokenBAmount: string = '700000000000000000000';
-  //   const nativeTokenAmount = '7000000000000000000';
+    const tokenAAmount: string = '700000000000000000000';
+    const tokenBAmount: string = '7000000';
+    const nativeTokenAmount = '7000000000000000000';
 
-  //   const sideToContractMethods = new Map([
-  //     [
-  //       SwapSide.SELL,
-  //       [
-  //         ContractMethod.simpleSwap,
-  //         ContractMethod.multiSwap,
-  //         ContractMethod.megaSwap,
-  //       ],
-  //     ],
-  //   ]);
+    const sideToContractMethods = new Map([
+      [
+        SwapSide.SELL,
+        [
+          ContractMethod.simpleSwap,
+          ContractMethod.multiSwap,
+          ContractMethod.megaSwap,
+        ],
+      ],
+    ]);
 
-  //   sideToContractMethods.forEach((contractMethods, side) =>
-  //     contractMethods.forEach((contractMethod: ContractMethod) => {
-  //       describe(`${contractMethod}`, () => {
-  //         it(nativeTokenSymbol + ' -> TOKEN', async () => {
-  //           await testE2E(
-  //             tokens[nativeTokenSymbol],
-  //             tokens[tokenASymbol],
-  //             holders[nativeTokenSymbol],
-  //             side === SwapSide.SELL ? nativeTokenAmount : tokenAAmount,
-  //             side,
-  //             dexKey,
-  //             contractMethod,
-  //             network,
-  //             provider,
-  //           );
-  //         });
-  //         it('TOKEN -> ' + nativeTokenSymbol, async () => {
-  //           await testE2E(
-  //             tokens[tokenASymbol],
-  //             tokens[nativeTokenSymbol],
-  //             holders[tokenASymbol],
-  //             side === SwapSide.SELL ? tokenAAmount : nativeTokenAmount,
-  //             side,
-  //             dexKey,
-  //             contractMethod,
-  //             network,
-  //             provider,
-  //           );
-  //         });
-  //         it('TOKEN -> TOKEN', async () => {
-  //           await testE2E(
-  //             tokens[tokenASymbol],
-  //             tokens[tokenBSymbol],
-  //             holders[tokenASymbol],
-  //             side === SwapSide.SELL ? tokenAAmount : tokenBAmount,
-  //             side,
-  //             dexKey,
-  //             contractMethod,
-  //             network,
-  //             provider,
-  //           );
-  //         });
-  //       });
-  //     }),
-  //   );
-  // });
+    sideToContractMethods.forEach((contractMethods, side) =>
+      contractMethods.forEach((contractMethod: ContractMethod) => {
+        describe(`${contractMethod}`, () => {
+          it(nativeTokenSymbol + ' -> TOKEN', async () => {
+            await testE2E(
+              tokens[nativeTokenSymbol],
+              tokens[tokenASymbol],
+              holders[nativeTokenSymbol],
+              side === SwapSide.SELL ? nativeTokenAmount : tokenAAmount,
+              side,
+              dexKey,
+              contractMethod,
+              network,
+              provider,
+            );
+          });
+          it('TOKEN -> ' + nativeTokenSymbol, async () => {
+            await testE2E(
+              tokens[tokenASymbol],
+              tokens[nativeTokenSymbol],
+              holders[tokenASymbol],
+              side === SwapSide.SELL ? tokenAAmount : nativeTokenAmount,
+              side,
+              dexKey,
+              contractMethod,
+              network,
+              provider,
+            );
+          });
+          it('TOKEN -> TOKEN', async () => {
+            await testE2E(
+              tokens[tokenASymbol],
+              tokens[tokenBSymbol],
+              holders[tokenASymbol],
+              side === SwapSide.SELL ? tokenAAmount : tokenBAmount,
+              side,
+              dexKey,
+              contractMethod,
+              network,
+              provider,
+            );
+          });
+        });
+      }),
+    );
+  });
 
-  // describe('KyberDmm BSC', () => {
-  //   const network = Network.BSC;
-  //   const tokens = Tokens[network];
-  //   const holders = Holders[network];
-  //   const provider = new JsonRpcProvider(ProviderURL[network]);
+  describe('KyberDmm BSC', () => {
+    const network = Network.BSC;
+    const tokens = Tokens[network];
+    const holders = Holders[network];
+    const provider = new JsonRpcProvider(ProviderURL[network]);
 
-  //   const tokenASymbol: string = 'USDT';
-  //   const tokenBSymbol: string = 'BUSD';
-  //   const nativeTokenSymbol = NativeTokenSymbols[network];
+    const tokenASymbol: string = 'USDT';
+    const tokenBSymbol: string = 'BUSD';
+    const nativeTokenSymbol = NativeTokenSymbols[network];
 
-  //   const tokenAAmount: string = '700000000000000000000';
-  //   const tokenBAmount: string = '700000000000000000000';
-  //   const nativeTokenAmount = '7000000000000000000';
+    const tokenAAmount: string = '700000000000000000000';
+    const tokenBAmount: string = '700000000000000000000';
+    const nativeTokenAmount = '7000000000000000000';
 
-  //   const sideToContractMethods = new Map([
-  //     [
-  //       SwapSide.SELL,
-  //       [
-  //         ContractMethod.simpleSwap,
-  //         ContractMethod.multiSwap,
-  //         ContractMethod.megaSwap,
-  //       ],
-  //     ],
-  //   ]);
+    const sideToContractMethods = new Map([
+      [
+        SwapSide.SELL,
+        [
+          ContractMethod.simpleSwap,
+          ContractMethod.multiSwap,
+          ContractMethod.megaSwap,
+        ],
+      ],
+    ]);
 
-  //   sideToContractMethods.forEach((contractMethods, side) =>
-  //     contractMethods.forEach((contractMethod: ContractMethod) => {
-  //       describe(`${contractMethod}`, () => {
-  //         it(nativeTokenSymbol + ' -> TOKEN', async () => {
-  //           await testE2E(
-  //             tokens[nativeTokenSymbol],
-  //             tokens[tokenASymbol],
-  //             holders[nativeTokenSymbol],
-  //             side === SwapSide.SELL ? nativeTokenAmount : tokenAAmount,
-  //             side,
-  //             dexKey,
-  //             contractMethod,
-  //             network,
-  //             provider,
-  //           );
-  //         });
-  //         it('TOKEN -> ' + nativeTokenSymbol, async () => {
-  //           await testE2E(
-  //             tokens[tokenASymbol],
-  //             tokens[nativeTokenSymbol],
-  //             holders[tokenASymbol],
-  //             side === SwapSide.SELL ? tokenAAmount : nativeTokenAmount,
-  //             side,
-  //             dexKey,
-  //             contractMethod,
-  //             network,
-  //             provider,
-  //           );
-  //         });
-  //         it('TOKEN -> TOKEN', async () => {
-  //           await testE2E(
-  //             tokens[tokenASymbol],
-  //             tokens[tokenBSymbol],
-  //             holders[tokenASymbol],
-  //             side === SwapSide.SELL ? tokenAAmount : tokenBAmount,
-  //             side,
-  //             dexKey,
-  //             contractMethod,
-  //             network,
-  //             provider,
-  //           );
-  //         });
-  //       });
-  //     }),
-  //   );
-  // });
+    sideToContractMethods.forEach((contractMethods, side) =>
+      contractMethods.forEach((contractMethod: ContractMethod) => {
+        describe(`${contractMethod}`, () => {
+          it(nativeTokenSymbol + ' -> TOKEN', async () => {
+            await testE2E(
+              tokens[nativeTokenSymbol],
+              tokens[tokenASymbol],
+              holders[nativeTokenSymbol],
+              side === SwapSide.SELL ? nativeTokenAmount : tokenAAmount,
+              side,
+              dexKey,
+              contractMethod,
+              network,
+              provider,
+            );
+          });
+          it('TOKEN -> ' + nativeTokenSymbol, async () => {
+            await testE2E(
+              tokens[tokenASymbol],
+              tokens[nativeTokenSymbol],
+              holders[tokenASymbol],
+              side === SwapSide.SELL ? tokenAAmount : nativeTokenAmount,
+              side,
+              dexKey,
+              contractMethod,
+              network,
+              provider,
+            );
+          });
+          it('TOKEN -> TOKEN', async () => {
+            await testE2E(
+              tokens[tokenASymbol],
+              tokens[tokenBSymbol],
+              holders[tokenASymbol],
+              side === SwapSide.SELL ? tokenAAmount : tokenBAmount,
+              side,
+              dexKey,
+              contractMethod,
+              network,
+              provider,
+            );
+          });
+        });
+      }),
+    );
+  });
 
-  // describe('KyberDmm AVALANCHE', () => {
-  //   const network = Network.AVALANCHE;
-  //   const tokens = Tokens[network];
-  //   const holders = Holders[network];
-  //   const provider = new JsonRpcProvider(ProviderURL[network]);
+  describe('KyberDmm AVALANCHE', () => {
+    const network = Network.AVALANCHE;
+    const tokens = Tokens[network];
+    const holders = Holders[network];
+    const provider = new JsonRpcProvider(ProviderURL[network]);
 
-  //   const tokenASymbol: string = 'USDCe';
-  //   const tokenBSymbol: string = 'USDTe';
-  //   const nativeTokenSymbol = NativeTokenSymbols[network];
+    const tokenASymbol: string = 'USDCe';
+    const tokenBSymbol: string = 'USDTe';
+    const nativeTokenSymbol = NativeTokenSymbols[network];
 
-  //   const tokenAAmount: string = '700000000';
-  //   const tokenBAmount: string = '700000000';
-  //   const nativeTokenAmount = '70000000000000000';
+    const tokenAAmount: string = '7000000';
+    const tokenBAmount: string = '7000000';
+    const nativeTokenAmount = '70000000000000000';
 
-  //   const sideToContractMethods = new Map([
-  //     [
-  //       SwapSide.SELL,
-  //       [
-  //         ContractMethod.simpleSwap,
-  //         ContractMethod.multiSwap,
-  //         ContractMethod.megaSwap,
-  //       ],
-  //     ],
-  //   ]);
+    const sideToContractMethods = new Map([
+      [
+        SwapSide.SELL,
+        [
+          ContractMethod.simpleSwap,
+          ContractMethod.multiSwap,
+          ContractMethod.megaSwap,
+        ],
+      ],
+    ]);
 
-  //   sideToContractMethods.forEach((contractMethods, side) =>
-  //     contractMethods.forEach((contractMethod: ContractMethod) => {
-  //       describe(`${contractMethod}`, () => {
-  //         it(nativeTokenSymbol + ' -> TOKEN', async () => {
-  //           await testE2E(
-  //             tokens[nativeTokenSymbol],
-  //             tokens[tokenASymbol],
-  //             holders[nativeTokenSymbol],
-  //             side === SwapSide.SELL ? nativeTokenAmount : tokenAAmount,
-  //             side,
-  //             dexKey,
-  //             contractMethod,
-  //             network,
-  //             provider,
-  //           );
-  //         });
-  //         it('TOKEN -> ' + nativeTokenSymbol, async () => {
-  //           await testE2E(
-  //             tokens[tokenASymbol],
-  //             tokens[nativeTokenSymbol],
-  //             holders[tokenASymbol],
-  //             side === SwapSide.SELL ? tokenAAmount : nativeTokenAmount,
-  //             side,
-  //             dexKey,
-  //             contractMethod,
-  //             network,
-  //             provider,
-  //           );
-  //         });
-  //         it('TOKEN -> TOKEN', async () => {
-  //           await testE2E(
-  //             tokens[tokenASymbol],
-  //             tokens[tokenBSymbol],
-  //             holders[tokenASymbol],
-  //             side === SwapSide.SELL ? tokenAAmount : tokenBAmount,
-  //             side,
-  //             dexKey,
-  //             contractMethod,
-  //             network,
-  //             provider,
-  //           );
-  //         });
-  //       });
-  //     }),
-  //   );
-  // });
+    sideToContractMethods.forEach((contractMethods, side) =>
+      contractMethods.forEach((contractMethod: ContractMethod) => {
+        describe(`${contractMethod}`, () => {
+          it(nativeTokenSymbol + ' -> TOKEN', async () => {
+            await testE2E(
+              tokens[nativeTokenSymbol],
+              tokens[tokenASymbol],
+              holders[nativeTokenSymbol],
+              side === SwapSide.SELL ? nativeTokenAmount : tokenAAmount,
+              side,
+              dexKey,
+              contractMethod,
+              network,
+              provider,
+            );
+          });
+          it('TOKEN -> ' + nativeTokenSymbol, async () => {
+            await testE2E(
+              tokens[tokenASymbol],
+              tokens[nativeTokenSymbol],
+              holders[tokenASymbol],
+              side === SwapSide.SELL ? tokenAAmount : nativeTokenAmount,
+              side,
+              dexKey,
+              contractMethod,
+              network,
+              provider,
+            );
+          });
+          it('TOKEN -> TOKEN', async () => {
+            await testE2E(
+              tokens[tokenASymbol],
+              tokens[tokenBSymbol],
+              holders[tokenASymbol],
+              side === SwapSide.SELL ? tokenAAmount : tokenBAmount,
+              side,
+              dexKey,
+              contractMethod,
+              network,
+              provider,
+            );
+          });
+        });
+      }),
+    );
+  });
 });
