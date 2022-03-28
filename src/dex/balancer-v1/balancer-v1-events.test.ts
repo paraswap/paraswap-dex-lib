f4280bc4c64bc6629d484cb2a1190d8bd1e4b9ae
Verisana <account@0112.pro>
2022-03-28 17:34:23 +0100

    test: create event tests for BalancerV1

--- src/dex/balancer-v1/balancer-v1-events.test.ts
+++ src/dex/balancer-v1/balancer-v1-events.test.ts
@@ -1,62 +1,108 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { BalancerV1EventPool } from './balancer-v1';
-import { BalancerV1Config } from './config';
+import { BalancerV1, BalancerV1EventPool } from './balancer-v1';
+import { BalancerV1Config, SUBGRAPH_TIMEOUT } from './config';
 import { Network } from '../../constants';
 import { DummyDexHelper } from '../../dex-helper/index';
 import { testEventSubscriber } from '../../../tests/utils-events';
 import axios from 'axios';
-
-/*
-  README
-  ======
-
-  This test script adds unit tests for BalancerV1 event based 
-  system. This is done by fetching the state on-chain before the 
-  event block, manually pushing the block logs to the event-subsriber, 
-  comparing the local state with on-chain state. 
-
-  Most of the logic for testing is abstracted by `testEventSubscriber`.
-  You need to do two things to make the tests work: 
-  
-  1. Fetch the block numbers where certain events were released. You
-  can modify the `./scripts/fetch-event-blocknumber.ts` to get the 
-  blocknumbers for different events. Make sure to get sufficient
-  number of blockNumbers to cover all possible cases for the event
-  mutations. 
-
-  2. Complete the implementation for fetchPoolState function. The 
-  function should fetch the on-chain state of the event subscriber 
-  using just the blocknumber. 
-
-  The template tests only include the test for a single event 
-  subscriber. There can be cases where multiple event subscribers 
-  exist for a single DEX. In such cases additional tests should be 
-  added.  
-
-  You can run this individual test script by running:
-  `npx jest src/dex/<dex-name>/<dex-name>-events.tests.ts`
-
-  (This comment should be removed from the final implementation)
-*/
+import { PoolState, PoolStateMap, PoolStates } from './types';
 
 jest.setTimeout(50 * 1000);
 const dexKey = 'BalancerV1';
 const network = Network.MAINNET;
 const config = BalancerV1Config[dexKey][network];
 
+type SubgraphState = Omit<PoolState, 'tokensList' | 'publicSwap'>;
+
+async function getSubgraphPool(
+  address: string,
+  blockNumber: number,
+): Promise<SubgraphState> {
+  address = address.toLowerCase();
+
+  const query = `query ($blockNumber: Int, $address: ID!) {
+    pools: pools(block: { number: $blockNumber }, where: {id: $address}) {
+      id
+      swapFee
+      totalWeight
+      tokens {
+        address
+        balance
+        decimals
+        denormWeight
+      }
+    }
+  }`;
+
+  const variables = {
+    blockNumber,
+    address,
+  };
+
+  const data = await axios.post(
+    config.subgraphURL,
+    { query, variables },
+    { timeout: SUBGRAPH_TIMEOUT },
+  );
+  return data.data.data.pools[0];
+}
+
 async function fetchPoolState(
   balancerV1Pools: BalancerV1EventPool,
   blockNumber: number,
   poolAddress: string,
-): Promise<PoolStateType> {
-  // TODO: complete me!
+): Promise<PoolStateMap> {
+  const fetchedState = await getSubgraphPool(poolAddress, blockNumber);
+  const pools: PoolStates = {
+    pools: [
+      {
+        ...fetchedState,
+        tokensList: fetchedState.tokens.map(token => token.address),
+      },
+    ],
+  };
+
+  const poolsStates = await balancerV1Pools.getAllPoolDataOnChain(
+    pools,
+    blockNumber,
+  );
+
+  let poolStateMap: PoolStateMap = {};
+  poolsStates.pools.forEach(
+    pool => (poolStateMap[pool.id.toLowerCase()] = pool),
+  );
+
+  return poolStateMap;
 }
 
 describe('BalancerV1 Event', function () {
-  const blockNumbers: { [eventName: string]: number[] } = {
-    // TODO: complete me!
+  const testPoolAddress = '0x1eff8af5d577060ba4ac8a29a13525bb0ee2a3d5';
+  const blockNumbers: { [event: string]: [number, string][] } = {
+    LOG_JOIN: [
+      [11379497, testPoolAddress],
+      [11379555, testPoolAddress],
+      [11380893, testPoolAddress],
+      [11380933, testPoolAddress],
+      [11381493, testPoolAddress],
+      [11382001, testPoolAddress],
+    ],
+    LOG_EXIT: [
+      [11376152, testPoolAddress],
+      [11376280, testPoolAddress],
+      [11377349, testPoolAddress],
+      [11377998, testPoolAddress],
+    ],
+    LOG_SWAP: [
+      [11376453, testPoolAddress],
+      [11376550, testPoolAddress],
+      [11378762, testPoolAddress],
+      [11378773, testPoolAddress],
+      [11378776, testPoolAddress],
+      [11378856, testPoolAddress],
+      [11378870, testPoolAddress],
+    ],
   };
 
   describe('BalancerV1EventPool', function () {
@@ -69,8 +115,6 @@ describe('BalancerV1 Event', function () {
           const balancerV1Pools = new BalancerV1EventPool(
             dexKey,
             network,
-            config.vaultAddress,
-            config.subgraphURL,
             dexHelper,
             logger,
           );
@@ -81,7 +125,7 @@ describe('BalancerV1 Event', function () {
             (_blockNumber: number) =>
               fetchPoolState(balancerV1Pools, _blockNumber, poolAddress),
             blockNumber,
-            `${dexKey}_${poolAddress}`,
+            BalancerV1.getIdentifier(dexKey, poolAddress),
             dexHelper.provider,
           );
         });
