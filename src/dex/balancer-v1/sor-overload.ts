6ba6f8a4c7fe310ac3347524c6eb01d68da923f7
Verisana <account@0112.pro>
2022-03-28 12:55:01 +0100

    feat: move parsePoolPairData to sor-overloads file

--- src/dex/balancer-v1/sor-overload.ts
+++ src/dex/balancer-v1/sor-overload.ts
@@ -1,80 +1,47 @@
-import { Contract } from '@ethersproject/contracts';
-import { BaseProvider } from '@ethersproject/providers';
-import { Token as PToken } from '../../../models/token';
-import BigNumber from 'bignumber.js';
 import * as bmath from '@balancer-labs/sor/dist/bmath';
-import CustomMultiAbi from '../../abi/balancerCustomMulticall.json';
-import { Pool as OldPool, Swap } from '@balancer-labs/sor/dist/types';
-import { priceApis } from '../../price-api';
-import { Utils } from '../../utils';
-import { Address } from '../../types';
+import { Pool as OldPool } from '@balancer-labs/sor/dist/types';
+import { PoolState } from './types';
 
-const logger = global.LOGGER();
+// Original Implementation: https://github.com/balancer-labs/balancer-sor/blob/v1.0.0-1/src/helpers.ts
+// No change has been made. This function doesn't exist in older SOR and is needed to convert the new SOR Pool
+// to the older SOR Pool. The new SOR has the datatype PoolPairData which is equivalent to old SOR datatype
+// Pool.
+export function parsePoolPairData(
+  p: PoolState,
+  tokenIn: string,
+  tokenOut: string,
+): OldPool | null {
+  let tI = p.tokens.find(
+    t => t.address.toLowerCase() === tokenIn.toLowerCase(),
+  );
+  let tO = p.tokens.find(
+    t => t.address.toLowerCase() === tokenOut.toLowerCase(),
+  );
 
-const POOL_FETCH_TIMEOUT = 5000;
+  if (!tI || !tO) return null;
 
-// Original Implementation: https://github.com/balancer-labs/balancer-sor/blob/master/src/multicall.ts)
-// Modification: call the onChain querry on a paticular blockNumber
-export async function getAllPoolDataOnChain(
-  pools: SubGraphPools,
-  multiAddress: string,
-  provider: BaseProvider,
-  blockNumber: number,
-): Promise<Pools> {
-  if (pools.pools.length === 0) throw Error('There are no pools.');
+  let poolPairData = {
+    id: p.id,
+    tokenIn: tokenIn,
+    tokenOut: tokenOut,
+    decimalsIn: tI.decimals,
+    decimalsOut: tO.decimals,
+    balanceIn: bmath.bnum(tI.balance.toString()),
+    balanceOut: bmath.bnum(tO.balance.toString()),
+    weightIn: bmath.scale(
+      bmath
+        .bnum(tI.denormWeight.toString())
+        .div(bmath.bnum(p.totalWeight.toString())),
+      18,
+    ),
+    weightOut: bmath.scale(
+      bmath
+        .bnum(tO.denormWeight.toString())
+        .div(bmath.bnum(p.totalWeight.toString())),
+      18,
+    ),
+    swapFee: bmath.bnum(p.swapFee.toString()),
+  };
 
-  const contract = new Contract(multiAddress, CustomMultiAbi, provider);
-
-  let addresses: string[][] = [];
-  let total = 0;
-
-  for (let i = 0; i < pools.pools.length; i++) {
-    let pool = pools.pools[i];
-
-    addresses.push([pool.id]);
-    total++;
-    pool.tokens.forEach(token => {
-      addresses[i].push(token.address);
-      total++;
-    });
-  }
-
-  //Original: let results = await contract.getPoolInfo(addresses, total);
-  let results = await contract.getPoolInfo(addresses, total, {
-    blockTag: blockNumber,
-  });
-
-  let j = 0;
-  let onChainPools: Pools = { pools: [] };
-
-  for (let i = 0; i < pools.pools.length; i++) {
-    let tokens: Token[] = [];
-
-    let p: Pool = {
-      id: pools.pools[i].id,
-      swapFee: bmath.scale(bmath.bnum(pools.pools[i].swapFee), 18),
-      totalWeight: bmath.scale(bmath.bnum(pools.pools[i].totalWeight), 18),
-      tokens: tokens,
-      tokensList: pools.pools[i].tokensList,
-    };
-
-    pools.pools[i].tokens.forEach(token => {
-      let bal = bmath.bnum(results[j]);
-      j++;
-      p.tokens.push({
-        address: token.address,
-        balance: bal,
-        decimals: Number(token.decimals),
-        denormWeight: bmath.scale(bmath.bnum(token.denormWeight), 18),
-      });
-    });
-    onChainPools.pools.push(p);
-  }
-  return onChainPools;
-}
-
-export async function getAllPublicSwapPools(
-  URL: string,
-): Promise<SubGraphPools> {
-  return (await Utils._get(URL, POOL_FETCH_TIMEOUT)).data;
+  return poolPairData;
 }
