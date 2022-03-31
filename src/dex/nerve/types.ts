f76ddc790331067f1860f594282bbcb4f821cfb8
Verisana <account@0112.pro>
2022-03-31 16:32:32 +0100

    feat: fill types

--- src/dex/nerve/types.ts
+++ src/dex/nerve/types.ts
@@ -14,6 +14,22 @@ export interface PoolState {
   tokenPrecisionMultipliers: bigint[];
 }
 
+export type MetapoolState = PoolState & {
+  baseVirtualPrice: bigint;
+  baseCacheLastUpdated: bigint;
+  basePoolState: PoolState;
+};
+
+export interface NervePoolConfig {
+  coins: Address[];
+  address: Address;
+  name: string;
+  type: number; // 1: stable coin pool, 2: others pools
+  isMetapool: boolean;
+  lpTokenAddress: string;
+  trackCoins: boolean;
+}
+
 export type NerveData = {
   // TODO: NerveData is the dex data that is
   // returned by the API that can be used for
@@ -25,4 +41,4 @@ export type DexParams = {
   // TODO: DexParams is set of parameters the can
   // be used to initiate a DEX fork.
   // Complete me!
-}
+};
