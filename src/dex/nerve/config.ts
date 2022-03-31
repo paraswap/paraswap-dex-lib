1b92061d9bbbc28448cec8483f8159815e2bbbea
Verisana <account@0112.pro>
2022-03-31 16:31:50 +0100

    feat: fill configs with values

--- src/dex/nerve/config.ts
+++ src/dex/nerve/config.ts
@@ -1,18 +1,23 @@
 import { DexParams } from './types';
 import { DexConfigMap } from '../../types';
 import { Network } from '../../constants';
+import { ThreePool } from './pools/Three-pool';
+import { SwapSide } from 'paraswap-core';
 
 export const NerveConfig: DexConfigMap<DexParams> = {
   Nerve: {
     // TODO: complete me!
-    poolConfigs: {
-      
-    }
-  }
+    [Network.BSC]: {
+    },
+  },
 };
 
 export const Adapters: {
-  [chainId: number]: { [side: string]: { name: string; index: number }[] | null };
+  [chainId: number]: {
+    [side: string]: { name: string; index: number }[];
+  };
 } = {
-  // TODO: add adapters for each chain
+  [Network.BSC]: {
+    [SwapSide.SELL]: [{ name: 'BscAdapter01', index: 4 }],
+  },
 };
