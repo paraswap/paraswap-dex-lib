f515dcc93cc33a1ea5f1b01fc0c1f6def1a925ae
Verisana <account@0112.pro>
2022-03-31 16:33:15 +0100

    feat: create utils to work

--- src/dex/nerve/utils.ts
+++ src/dex/nerve/utils.ts
@@ -1,3 +1,20 @@
-const biginterify = (val: any) => BigInt(val);
+export const biginterify = (val: any) => BigInt(val);
 
-const stringify = (val: any) => val.toString();
+export const stringify = (val: any) => val.toString();
+
+export const ZERO = biginterify(0);
+
+export const ONE = biginterify(1);
+
+export class MathUtil {
+  static within1(a: bigint, b: bigint) {
+    return MathUtil.difference(a, b) <= ONE;
+  }
+
+  static difference(a: bigint, b: bigint) {
+    if (a > b) {
+      return a - b;
+    }
+    return b - a;
+  }
+}
