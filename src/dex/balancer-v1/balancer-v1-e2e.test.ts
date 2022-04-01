b364fc681c5efdca6e4ecd9ab9b54731b19b41f4
Verisana <account@0112.pro>
2022-04-01 23:12:39 +0100

    docs: remove hint comment in e2e test

--- src/dex/balancer-v1/balancer-v1-e2e.test.ts
+++ src/dex/balancer-v1/balancer-v1-e2e.test.ts
@@ -11,38 +11,6 @@ import {
 } from '../../constants';
 import { JsonRpcProvider } from '@ethersproject/providers';
 
-/*
-  README
-  ======
-
-  This test script should add e2e tests for BalancerV1. The tests
-  should cover as many cases as possible. Most of the DEXes follow
-  the following test structure:
-    - DexName
-      - ForkName + Network
-        - ContractMethod
-          - ETH -> Token swap
-          - Token -> ETH swap
-          - Token -> Token swap
-
-  The template already enumerates the basic structure which involves
-  testing simpleSwap, multiSwap, megaSwap contract methods for
-  ETH <> TOKEN and TOKEN <> TOKEN swaps. You should replace tokenA and
-  tokenB with any two highly liquid tokens on BalancerV1 for the tests
-  to work. If the tokens that you would like to use are not defined in
-  Tokens or Holders map, you can update the './tests/constants-e2e'
-
-  Other than the standard cases that are already added by the template
-  it is highly recommended to add test cases which could be specific
-  to testing BalancerV1 (Eg. Tests based on poolType, special tokens,
-  etc).
-
-  You can run this individual test script by running:
-  `npx jest src/dex/<dex-name>/<dex-name>-e2e.tests.ts`
-
-  (This comment should be removed from the final implementation)
-*/
-
 // Balancer doesn't support USDT
 describe('BalancerV1 E2E Mainnet', () => {
   const dexKey = 'BalancerV1';
