854fb6bc46276936e2ad6437a425ff82cb86d675
Verisana <account@0112.pro>
2022-04-05 19:56:05 +0100

    feat: create mapFromOldPoolToPoolState in utils balancerV1

--- src/dex/balancer-v1/utils.ts
+++ src/dex/balancer-v1/utils.ts
@@ -1,5 +1,6 @@
 import { PoolState, Token as SORToken } from './types';
 import { bignumberify } from '../../utils';
+import { Pool as OldPool } from '@balancer-labs/sor/dist/types';
 
 export function typecastReadOnlyToken(readOnlyToken: any): SORToken {
   return {
@@ -18,4 +19,20 @@ export function typecastReadOnlyPool(readOnlyPool: any): PoolState {
     tokens: readOnlyPool.tokens.map(typecastReadOnlyToken),
     tokensList: readOnlyPool.tokensList,
   };
+}
+
+export function mapFromOldPoolToPoolState(
+  oldPools: (OldPool | null)[],
+  pools: PoolState[],
+): PoolState[] {
+  const result = [];
+
+  for (const oldPool of oldPools) {
+    const poolState = pools.filter(
+      pool => oldPool && pool.id === oldPool.id,
+    )[0];
+    if (poolState) result.push(poolState);
+  }
+
+  return result;
 }
