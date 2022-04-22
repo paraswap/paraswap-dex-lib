de994d2e3abb4e4e7f1024abccbee676ff66b832
Louis-Amas <amaslouis@protonmail.com>
2022-04-22 14:02:38 +0200

    uniswap-v2: put aliases into lowercase
    Resolves BACK-533.

--- src/dex/uniswap-v2/constants.ts
+++ src/dex/uniswap-v2/constants.ts
@@ -46,10 +46,10 @@ export const UniswapForksWithNetwork = transformToNetworkMap({
 
 // These are exchanges used for swapOnUniswap method
 export const UniswapV2Alias: { [network: number]: string } = {
-  [Network.MAINNET]: 'UniswapV2',
-  [Network.ROPSTEN]: 'UniswapV2',
-  [Network.BSC]: 'PancakeSwap',
-  [Network.POLYGON]: 'QuickSwap',
-  [Network.AVALANCHE]: 'PangolinSwap',
-  [Network.FANTOM]: 'SpookySwap',
+  [Network.MAINNET]: 'uniswapv2',
+  [Network.ROPSTEN]: 'uniswapv2',
+  [Network.BSC]: 'pancakeswap',
+  [Network.POLYGON]: 'quickswap',
+  [Network.AVALANCHE]: 'pangolinswap',
+  [Network.FANTOM]: 'spookyswap',
 };
