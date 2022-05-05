1a0c230a1d0bc0b5115a7aaad0e696ac8d9dac76
Verisana <account@0112.pro>
2022-05-05 21:25:41 +0100

    feat: add config for BSC

--- src/dex/woo-fi/config.ts
+++ src/dex/woo-fi/config.ts
@@ -5,6 +5,37 @@ import { Network, SwapSide } from '../../constants';
 export const WooFiConfig: DexConfigMap<DexParams> = {
   WooFi: {
     // TODO: complete me!
+    [Network.BSC]: {
+      wooPPAddress: '0xbf365Ce9cFcb2d5855521985E351bA3bcf77FD3F',
+      woOracleAddress: '0x6b6fBEc7934b104e81b2046D24A990e03e17afDC',
+      quoteToken: {
+        // BUSD
+        address: '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d',
+        decimals: 18,
+      },
+      baseTokens: {
+        WBNB: {
+          address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+          decimals: 18,
+        },
+        BTCB: {
+          address: '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c',
+          decimals: 18,
+        },
+        ETH: {
+          address: '0x2170ed0880ac9a755fd29b2688956bd959f933f8',
+          decimals: 18,
+        },
+        WOO: {
+          address: '0x4691937a7508860f876c9c0a2a617e7d9e945d4b',
+          decimals: 18,
+        },
+        USDT: {
+          address: '0x55d398326f99059fF775485246999027B3197955',
+          decimals: 18,
+        },
+      },
+    },
   },
 };
 
