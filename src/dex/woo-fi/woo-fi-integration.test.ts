384f74f000fbf3a3894e2d11bfb1496087fe3034
Verisana <account@0112.pro>
2022-05-09 13:46:35 +0100

    test: update integration tests

--- src/dex/woo-fi/woo-fi-integration.test.ts
+++ src/dex/woo-fi/woo-fi-integration.test.ts
@@ -105,6 +105,41 @@ describe('WooFi', function () {
       decodeReaderResult(readerResult, wooFi.wooIfaces.PP, funcName),
     );
 
+    // const test = wooFi.math.querySellBase(
+    //   wooFi.latestState!,
+    //   TokenB.address,
+    //   TokenA.address,
+    //   amounts[1],
+    // );
+
+    // const wooPP = new WooPP(wooFi.config.quoteToken);
+    // const baseInfo =
+    //   wooFi.latestState!.tokenInfos[TokenA.address.toLowerCase()];
+    // const quoteInfo =
+    //   wooFi.latestState!.tokenInfos[TokenB.address.toLowerCase()];
+    // const baseState =
+    //   wooFi.latestState!.tokenStates[TokenA.address.toLowerCase()];
+    // const fee = wooFi.latestState!.feeRates[TokenA.address.toLowerCase()];
+    // const realRes = wooPP.QuerySellBase(
+    //   { address: TokenA.address, decimals: 18 },
+    //   new WooppTokenInfo(
+    //     new BigNumber(baseInfo.reserve.toString()),
+    //     new BigNumber(fee.toString()),
+    //     new BigNumber(baseInfo.R.toString()),
+    //   ),
+    //   new WooppTokenInfo(
+    //     new BigNumber(quoteInfo.reserve.toString()),
+    //     new BigNumber('0'),
+    //     new BigNumber(quoteInfo.R.toString()),
+    //   ),
+    //   new WooracleState(
+    //     new BigNumber(baseState.priceNow.toString()),
+    //     new BigNumber(baseState.spreadNow.toString()),
+    //     new BigNumber(baseState.coeffNow.toString()),
+    //   ),
+    //   new BigNumber(amounts[1].toString()),
+    // );
+
     expect(poolPrices![0].prices).toEqual(expectedPrices);
   });
 
@@ -161,49 +196,67 @@ describe('WooFi', function () {
     expect(poolPrices![0].prices).toEqual(expectedPrices);
   });
 
-  it('getPoolIdentifiers and getPricesVolume BUY', async function () {
+  // it('getPoolIdentifiers and getPricesVolume BUY', async function () {
+  //   const blockNumber = await dexHelper.provider.getBlockNumber();
+
+  //   const wooFi = new WooFi(network, dexKey, dexHelper);
+  //   await wooFi.initializePricing(blockNumber);
+
+  //   const pools = await wooFi.getPoolIdentifiers(
+  //     TokenA,
+  //     TokenB,
+  //     SwapSide.BUY,
+  //     blockNumber,
+  //   );
+  //   console.log(`${TokenASymbol} <> ${TokenBSymbol} Pool Identifiers: `, pools);
+
+  //   expect(pools.length).toBeGreaterThan(0);
+
+  //   const poolPrices = await wooFi.getPricesVolume(
+  //     TokenA,
+  //     TokenB,
+  //     amounts,
+  //     SwapSide.BUY,
+  //     blockNumber,
+  //     pools,
+  //   );
+  //   console.log(`${TokenASymbol} <> ${TokenBSymbol} Pool Prices: `, poolPrices);
+
+  //   expect(poolPrices).not.toBeNull();
+  //   if (wooFi.hasConstantPriceLargeAmounts) {
+  //     checkConstantPoolPrices(poolPrices!, amounts, dexKey);
+  //   } else {
+  //     checkPoolPrices(poolPrices!, amounts, SwapSide.BUY, dexKey);
+  //   }
+  // });
+
+  it('getTopPoolsForToken Base', async function () {
+    const dexHelper = new DummyDexHelper(network);
     const blockNumber = await dexHelper.provider.getBlockNumber();
 
     const wooFi = new WooFi(network, dexKey, dexHelper);
     await wooFi.initializePricing(blockNumber);
-
-    const pools = await wooFi.getPoolIdentifiers(
-      TokenA,
-      TokenB,
-      SwapSide.BUY,
-      blockNumber,
-    );
-    console.log(`${TokenASymbol} <> ${TokenBSymbol} Pool Identifiers: `, pools);
-
-    expect(pools.length).toBeGreaterThan(0);
-
-    const poolPrices = await wooFi.getPricesVolume(
-      TokenA,
-      TokenB,
-      amounts,
-      SwapSide.BUY,
-      blockNumber,
-      pools,
-    );
-    console.log(`${TokenASymbol} <> ${TokenBSymbol} Pool Prices: `, poolPrices);
-
-    expect(poolPrices).not.toBeNull();
-    if (wooFi.hasConstantPriceLargeAmounts) {
-      checkConstantPoolPrices(poolPrices!, amounts, dexKey);
-    } else {
-      checkPoolPrices(poolPrices!, amounts, SwapSide.BUY, dexKey);
-    }
-  });
-
-  it('getTopPoolsForToken', async function () {
-    const dexHelper = new DummyDexHelper(network);
-    const wooFi = new WooFi(network, dexKey, dexHelper);
 
     const poolLiquidity = await wooFi.getTopPoolsForToken(TokenA.address, 10);
     console.log(`${TokenASymbol} Top Pools:`, poolLiquidity);
 
     if (!wooFi.hasConstantPriceLargeAmounts) {
       checkPoolsLiquidity(poolLiquidity, TokenA.address, dexKey);
+    }
+  });
+
+  it('getTopPoolsForToken Quote', async function () {
+    const dexHelper = new DummyDexHelper(network);
+    const blockNumber = await dexHelper.provider.getBlockNumber();
+
+    const wooFi = new WooFi(network, dexKey, dexHelper);
+    await wooFi.initializePricing(blockNumber);
+
+    const poolLiquidity = await wooFi.getTopPoolsForToken(TokenB.address, 10);
+    console.log(`${TokenBSymbol} Top Pools:`, poolLiquidity);
+
+    if (!wooFi.hasConstantPriceLargeAmounts) {
+      checkPoolsLiquidity(poolLiquidity, TokenB.address, dexKey);
     }
   });
 });
