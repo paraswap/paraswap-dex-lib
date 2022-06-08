4e07e4de6fdbe7a53dbde17f3999e93f175f84a2
Verisana <account@0112.pro>
2022-06-08 14:44:26 +0300

    fix: set proper brackets for bit operations in BitMap

--- src/dex/uniswap-v3/contract-math/BitMath.ts
+++ src/dex/uniswap-v3/contract-math/BitMath.ts
@@ -5,7 +5,7 @@ import {
   BI_MAX_UINT64,
   BI_MAX_UINT8,
 } from '../../../bigint-constants';
-import { boolToBigint, _require } from './utils';
+import { _require } from './utils';
 
 export class BitMath {
   static mostSignificantBit(x: bigint): bigint {
@@ -49,42 +49,42 @@ export class BitMath {
     _require(x > 0, '', { x }, 'x > 0');
 
     let r = 255n;
-    if (x & boolToBigint(BI_MAX_UINT128 > 0n)) {
+    if ((x & BI_MAX_UINT128) > 0n) {
       r -= 128n;
     } else {
       x >>= 128n;
     }
-    if (x & boolToBigint(BI_MAX_UINT64 > 0n)) {
+    if ((x & BI_MAX_UINT64) > 0n) {
       r -= 64n;
     } else {
       x >>= 64n;
     }
-    if (x & boolToBigint(BI_MAX_UINT32 > 0n)) {
+    if ((x & BI_MAX_UINT32) > 0n) {
       r -= 32n;
     } else {
       x >>= 32n;
     }
-    if (x & boolToBigint(BI_MAX_UINT16 > 0n)) {
+    if ((x & BI_MAX_UINT16) > 0n) {
       r -= 16n;
     } else {
       x >>= 16n;
     }
-    if (x & boolToBigint(BI_MAX_UINT8 > 0n)) {
+    if ((x & BI_MAX_UINT8) > 0n) {
       r -= 8n;
     } else {
       x >>= 8n;
     }
-    if (x & boolToBigint(BigInt('0xf') > 0n)) {
+    if ((x & BigInt('0xf')) > 0n) {
       r -= 4n;
     } else {
       x >>= 4n;
     }
-    if (x & boolToBigint(BigInt('0x3') > 0n)) {
+    if ((x & BigInt('0x3')) > 0n) {
       r -= 2n;
     } else {
       x >>= 2n;
     }
-    if (x & boolToBigint(BigInt('0x1') > 0n)) r -= 1n;
+    if ((x & BigInt('0x1')) > 0n) r -= 1n;
     return r;
   }
 }
