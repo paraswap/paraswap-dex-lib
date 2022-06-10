b30a77b9b4b9fcce109dd2240d73e698a2b0e3b0
Verisana <account@0112.pro>
2022-06-10 11:21:51 +0300

    fix: remove spaces in config name field for Axial (Nerve)

--- src/dex/nerve/config.ts
+++ src/dex/nerve/config.ts
@@ -219,7 +219,7 @@ export const NerveConfig: DexConfigMap<DexParams> = {
           },
         },
         SCALES: {
-          name: 'The Scales',
+          name: 'Scales',
           address: '0xfD24d41B7C4C7C8Cd363Dd3FF6f49C99c8280430',
           coins: [
             {
@@ -247,7 +247,7 @@ export const NerveConfig: DexConfigMap<DexParams> = {
           },
         },
         HERO: {
-          name: 'The Hero',
+          name: 'Hero',
           address: '0xa0f6397FEBB03021F9BeF25134DE79835a24D76e',
           coins: [
             {
