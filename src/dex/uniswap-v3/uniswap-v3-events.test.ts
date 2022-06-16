7617182a0594f9cef133f97e320fea5a3f686789
Verisana <account@0112.pro>
2022-06-16 17:49:36 +0300

    test: add events for the poolAddress

--- src/dex/uniswap-v3/uniswap-v3-events.test.ts
+++ src/dex/uniswap-v3/uniswap-v3-events.test.ts
@@ -22,8 +22,20 @@ async function fetchPoolState(
 }
 
 describe('UniswapV3 Event', function () {
+  const poolAddress = '0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640';
   const blockNumbers: { [eventName: string]: number[] } = {
-    // TODO: complete me!
+    // topic0 - 0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67
+    ['Swap']: [
+      14973668, 14973666, 14973665, 14973664, 14973663, 4973662, 14973661,
+    ],
+    // topic0 - 0x0c396cd989a39f4459b5fa1aed6a9a8dcdbc45908acfd67e028cd568da98982c
+    ['Burn']: [14973650, 14973586, 14973558, 14973552, 14973547],
+    // topic0 - 0x7a53080ba414158be7ec69b987b5fb7d07dee101fe85488f0853ae16239d0bde
+    ['Mint']: [14973657, 14973641, 14973619, 14973589, 14973552],
+    // topic0 - 0x973d8d92bb299f4af6ce49b52a8adb85ae46b9f214c4c4fc06ac77401237b133
+    ['SetFeeProtocol']: [],
+    // topic0 - 0xac49e518f90a358f652e4400164f05a5d8f7e35e7747279bc3a93dbf584e125a
+    ['IncreaseObservationCardinalityNext']: [13125816, 12733621, 12591465],
   };
 
   describe('UniswapV3EventPool', function () {
