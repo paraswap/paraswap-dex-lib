a30f6af00139bdc10db6c1b7348c5205a743fb71
Verisana <account@0112.pro>
2022-06-20 18:16:37 +0300

    feat: update script with new checks and logs

--- src/dex/uniswap-v3/check-pool-bitmap-ranges.ts
+++ src/dex/uniswap-v3/check-pool-bitmap-ranges.ts
@@ -38,9 +38,21 @@ async function getBitmap(
   }));
 
   try {
+    // console.log(
+    //   `Start fetching bitMaps for ${pool}. Indexes from ${indexes[0]} to ${
+    //     indexes.slice(-1)[0]
+    //   }`,
+    // );
+    // const start = Date.now();
     const result = await multicallContract.methods
       .aggregate(callData)
       .call({}, blockNumber);
+
+    // console.log(
+    //   `Done fetching bitMaps for ${pool}. Indexes from ${indexes[0]} to ${
+    //     indexes.slice(-1)[0]
+    //   } took ${Math.floor((Date.now() - start) / 1000)} sec.`,
+    // );
 
     const decoded = result.returnData.map((d: string): bigint =>
       BigInt(poolIface.decodeFunctionResult('tickBitmap', d)[0]),
@@ -119,18 +131,35 @@ async function getPools(): Promise<SubgraphResult[]> {
   const upperTickBitmap = Number(BI_MAX_INT16);
   const chunks = 20;
   const poolsNumToProcess = 1;
-  const poolStartToProcess = 0;
+  const poolStartToProcess = 19;
 
-  let globalMin = Number(BI_MAX_INT16);
-  let globalMax = Number(BI_MIN_INT16);
-  const indexesCounter: Record<number, number> = {};
+  // Calculated from 18 first pools
+  let globalMin = {
+    index: -3466,
+    value: 16777216n,
+    pool: '0x5777d92f208679db4b9778590fa3cab3ac9e2168',
+  };
+
+  let globalMax = {
+    index: 3465,
+    value:
+      6901746346790563787434755862277025452451108972170386555162524223799296n,
+    pool: '0x5777d92f208679db4b9778590fa3cab3ac9e2168',
+  };
+
+  const indexesCounter: Record<number, number> = JSON.parse(
+    // Last result for 18
+    `{"0":8,"1":2,"2":4,"3":5,"4":5,"5":6,"6":2,"7":2,"8":3,"9":3,"10":2,"11":3,"12":1,"13":2,"14":2,"15":3,"16":2,"17":3,"18":2,"19":3,"20":2,"21":2,"22":1,"23":2,"24":1,"25":2,"26":1,"28":2,"31":1,"37":1,"38":1,"39":1,"42":1,"44":2,"53":2,"57":6,"62":1,"67":1,"68":1,"69":1,"70":1,"71":1,"72":2,"73":1,"74":1,"75":2,"76":1,"77":1,"78":2,"79":1,"80":1,"81":1,"82":1,"83":1,"84":1,"85":1,"86":1,"87":1,"89":2,"90":1,"91":1,"92":1,"93":2,"94":1,"96":1,"97":1,"98":1,"99":1,"100":1,"101":1,"102":1,"103":1,"104":1,"105":2,"106":1,"107":2,"110":1,"113":1,"114":1,"116":1,"123":1,"124":1,"125":2,"134":1,"136":1,"137":1,"138":1,"139":1,"140":1,"143":2,"152":1,"161":1,"177":1,"296":1,"346":6,"1076":1,"1077":1,"3465":1,"-3466":1,"-1080":1,"-1079":1,"-347":6,"-120":1,"-119":1,"-117":2,"-110":2,"-109":2,"-108":4,"-106":2,"-97":1,"-96":1,"-78":2,"-1":6,"-58":6,"-20":2,"-36":2,"-18":3,"-107":1,"-2":3,"-6":3,"-27":1,"-26":1,"-24":1,"-23":1,"-22":1,"-21":1,"-17":1,"-16":1,"-15":2,"-14":1,"-13":2,"-12":2,"-11":1,"-10":3,"-9":2,"-8":3,"-5":3,"-3":3,"-7":1,"-4":1,"-162":1,"-153":1,"-144":1,"-135":1,"-126":1,"-104":1,"-100":1,"-99":1,"-90":1,"-86":1,"-85":1,"-83":1,"-82":1,"-81":1,"-80":1,"-79":1,"-77":1,"-76":1,"-75":1,"-74":1,"-73":1,"-72":1,"-71":1,"-70":1,"-68":1,"-67":1,"-66":1,"-63":1,"-61":1,"-54":1,"-45":1}`,
+  );
 
   const chunkedPools = _.chunk(pools, poolsNumToProcess);
 
   for (const [index, chunkedPool] of chunkedPools
     .slice(poolStartToProcess)
     .entries()) {
-    console.log(`\nStart processing #${poolStartToProcess + index} pools...`);
+    console.log(
+      `\nStart processing batch #${poolStartToProcess + index} pools...`,
+    );
     const start = Date.now();
     await Promise.all(
       chunkedPool.map(async pool => {
@@ -142,8 +171,15 @@ async function getPools(): Promise<SubgraphResult[]> {
           chunks,
         );
 
-        let newGlobalMinPosition = { index: globalMin, value: 0n };
-        let newGlobalMaxPosition = { index: globalMax, value: 0n };
+        let newGlobalMinPosition = {
+          index: globalMin.index,
+          value: globalMin.value,
+        };
+        let newGlobalMaxPosition = {
+          index: globalMax.index,
+          value: globalMax.value,
+        };
+
         Object.keys(bitMaps).map(v => {
           const parsed = Number(v);
           indexesCounter[parsed] =
@@ -163,26 +199,46 @@ async function getPools(): Promise<SubgraphResult[]> {
           return parsed;
         });
 
-        if (newGlobalMinPosition.index !== globalMin) {
+        if (newGlobalMinPosition.index !== globalMin.index) {
           console.log(
             `Found new globalMin=${newGlobalMinPosition.index} in pool ${pool.id} and value=${newGlobalMinPosition.value}`,
           );
-          globalMin = newGlobalMinPosition.index;
+          globalMin.index = newGlobalMinPosition.index;
+          globalMin.value = newGlobalMinPosition.value;
+          globalMin.pool = pool.id;
         }
-        if (newGlobalMaxPosition.index !== globalMax) {
+        if (newGlobalMaxPosition.index !== globalMax.index) {
           console.log(
             `Found new globalMax=${newGlobalMaxPosition.index} in pool ${pool.id} and value=${newGlobalMaxPosition.value}`,
           );
-          globalMax = newGlobalMaxPosition.index;
+          globalMax.index = newGlobalMaxPosition.index;
+          globalMax.value = newGlobalMaxPosition.value;
+          globalMax.pool = pool.id;
         }
       }),
     );
 
     console.log(
-      `Done processing #${index} pools Took ${Math.floor(
+      `Done processing batch #${
+        poolStartToProcess + index
+      } pools Took ${Math.floor(
         (Date.now() - start) / 1000,
       )} sec. Current indexesCounter state is:`,
     );
     console.log(JSON.stringify(indexesCounter));
+    console.log('\nCurrent globalMin:');
+    console.log(
+      JSON.stringify(
+        globalMin,
+        (key, value) => (typeof value === 'bigint' ? value.toString() : value), // return everything else unchanged
+      ),
+    );
+    console.log('\nCurrent globalMax:');
+    console.log(
+      JSON.stringify(
+        globalMax,
+        (key, value) => (typeof value === 'bigint' ? value.toString() : value), // return everything else unchanged
+      ),
+    );
   }
 })();
