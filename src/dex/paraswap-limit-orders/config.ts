68b7f40d23f9b3099b826c8b17d1517d48c8761c
Verisana <account@0112.pro>
2022-06-22 11:02:56 +0300

    feat: add configs for all chains

--- src/dex/paraswap-limit-orders/config.ts
+++ src/dex/paraswap-limit-orders/config.ts
@@ -7,6 +7,21 @@ export const ParaswapLimitOrdersConfig: DexConfigMap<DexParams> = {
     [Network.ROPSTEN]: {
       rfqAddress: '0x34268C38fcbC798814b058656bC0156C7511c0E4',
     },
+    [Network.AVALANCHE]: {
+      rfqAddress: '0x34302c4267d0dA0A8c65510282Cc22E9e39df51f',
+    },
+    [Network.BSC]: {
+      rfqAddress: '0x8DcDfe88EF0351f27437284D0710cD65b20288bb',
+    },
+    [Network.FANTOM]: {
+      rfqAddress: '0x2DF17455B96Dde3618FD6B1C3a9AA06D6aB89347',
+    },
+    [Network.MAINNET]: {
+      rfqAddress: '0xe92b586627ccA7a83dC919cc7127196d70f55a06',
+    },
+    [Network.POLYGON]: {
+      rfqAddress: '0xF3CD476C3C4D3Ac5cA2724767f269070CA09A043',
+    },
   },
 };
 
