c1120642133b9a36a64d1b664e806b93dadbc5c1
Verisana <account@0112.pro>
2022-06-30 10:49:58 +0300

    feat: move LO constants to dex-lib

--- src/dex/paraswap-limit-orders/constant.ts
+++ src/dex/paraswap-limit-orders/constant.ts
@@ -1,1 +1,10 @@
 export const ONE_ORDER_GASCOST = 65500;
+
+// In one swap we can use only MAX_ORDERS_COUNT_FOR_SWAP for pricing and tx building
+export const MAX_ORDERS_USED_FOR_SWAP = BigInt(
+  process.env.MAX_ORDERS_USED_FOR_SWAP || '3',
+);
+
+// When we try to filter small orders, we have this multiplication factor which is used
+// (amount / (MAX_ORDERS_USED_FOR_SWAP * MAX_ORDERS_MULTI_FACTOR)) > takerBalance
+export const MAX_ORDERS_MULTI_FACTOR = 2n;
