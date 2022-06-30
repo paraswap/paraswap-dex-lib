3baa62e59169741237cdebb240a514013fb94a3c
Verisana <account@0112.pro>
2022-06-30 11:30:22 +0300

    feat: make MAX_ORDERS_MULTI_FACTOR as env variable

--- src/dex/paraswap-limit-orders/constant.ts
+++ src/dex/paraswap-limit-orders/constant.ts
@@ -1,10 +1,14 @@
 export const ONE_ORDER_GASCOST = 65500;
 
 // In one swap we can use only MAX_ORDERS_COUNT_FOR_SWAP for pricing and tx building
+// TODO: Must be replaced later with configuration service
 export const MAX_ORDERS_USED_FOR_SWAP = BigInt(
   process.env.MAX_ORDERS_USED_FOR_SWAP || '3',
 );
 
 // When we try to filter small orders, we have this multiplication factor which is used
 // (amount / (MAX_ORDERS_USED_FOR_SWAP * MAX_ORDERS_MULTI_FACTOR)) > takerBalance
-export const MAX_ORDERS_MULTI_FACTOR = 2n;
+// TODO: Must be replaced later with configuration service
+export const MAX_ORDERS_MULTI_FACTOR = BigInt(
+  process.env.MAX_ORDERS_MULTI_FACTOR || '2',
+);
