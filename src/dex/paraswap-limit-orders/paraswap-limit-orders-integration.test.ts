6b930008690f47158634f81bd910705cf5ad50e7
Verisana <account@0112.pro>
2022-07-01 15:16:20 +0300

    test: update tests fro Dex LO integration

--- src/dex/paraswap-limit-orders/paraswap-limit-orders-integration.test.ts
+++ src/dex/paraswap-limit-orders/paraswap-limit-orders-integration.test.ts
@@ -24,21 +24,55 @@ const TokenB = Tokens[network][TokenBSymbol];
 
 const amounts = [
   0n,
-  10n * BI_POWS[TokenA.decimals],
-  20n * BI_POWS[TokenA.decimals],
-  30n * BI_POWS[TokenA.decimals],
+  1n * BI_POWS[TokenA.decimals],
+  2n * BI_POWS[TokenA.decimals],
+  3n * BI_POWS[TokenA.decimals],
+  4n * BI_POWS[TokenA.decimals],
+  5n * BI_POWS[TokenA.decimals],
+  6n * BI_POWS[TokenA.decimals],
+  7n * BI_POWS[TokenA.decimals],
+  75n * BI_POWS[TokenA.decimals - 1],
+  8n * BI_POWS[TokenA.decimals],
+];
+
+const expectedPricesOnSell = [
+  0n,
+  100000000n,
+  197500000n,
+  295000000n,
+  391000000n,
+  487000000n,
+  583000000n,
+  675000000n,
+  722750000n,
+  770500000n,
 ];
 
 const amountsBuy = [
   0n,
   100n * BI_POWS[TokenB.decimals],
-  1100n * BI_POWS[TokenB.decimals],
-  11000n * BI_POWS[TokenB.decimals],
+  200n * BI_POWS[TokenB.decimals],
+  300n * BI_POWS[TokenB.decimals],
+  400n * BI_POWS[TokenB.decimals],
+  500n * BI_POWS[TokenB.decimals],
+  600n * BI_POWS[TokenB.decimals],
+  700n * BI_POWS[TokenB.decimals],
+  825n * BI_POWS[TokenB.decimals],
+  950n * BI_POWS[TokenB.decimals],
 ];
 
-const expectedPricesOnSell = [0n, 39130434n];
-
-const expectedPricesOnBuy = [];
+const expectedPricesOnBuy = [
+  0n,
+  1000000000000000000n,
+  2025641025641025641n,
+  3052083333333333333n,
+  4093750000000000000n,
+  5135416666666666666n,
+  6177083333333333333n,
+  7261780104712041884n,
+  8570680628272251308n,
+  9879581151832460732n,
+];
 
 const dexKey = 'ParaSwapLimitOrders';
 
@@ -52,16 +86,20 @@ const dummyOrderBook = {
   [tokenABKey]: [
     // Orders: Maker = USDC, Taker = WETH
     {
-      swappableMakerBalance: (1000n * BI_POWS[TokenB.decimals]).toString(),
+      swappableMakerBalance: (100n * BI_POWS[TokenB.decimals]).toString(),
+      swappableTakerBalance: (1n * BI_POWS[TokenA.decimals]).toString(),
+    },
+    {
+      swappableMakerBalance: (195n * BI_POWS[TokenB.decimals]).toString(),
       swappableTakerBalance: (2n * BI_POWS[TokenA.decimals]).toString(),
     },
     {
-      swappableMakerBalance: (10000n * BI_POWS[TokenB.decimals]).toString(),
-      swappableTakerBalance: (23n * BI_POWS[TokenA.decimals]).toString(),
+      swappableMakerBalance: (480n * BI_POWS[TokenB.decimals]).toString(),
+      swappableTakerBalance: (5n * BI_POWS[TokenA.decimals]).toString(),
     },
     {
-      swappableMakerBalance: (100000n * BI_POWS[TokenB.decimals]).toString(),
-      swappableTakerBalance: (234n * BI_POWS[TokenA.decimals]).toString(),
+      swappableMakerBalance: (955n * BI_POWS[TokenB.decimals]).toString(),
+      swappableTakerBalance: (10n * BI_POWS[TokenA.decimals]).toString(),
     },
   ],
 };
@@ -101,14 +139,15 @@ describe('ParaSwapLimitOrders', function () {
     const poolPrices = await paraSwapLimitOrders.getPricesVolume(
       TokenA,
       TokenB,
-      [260n * BI_POWS[TokenA.decimals]],
+      [0n, 20n * BI_POWS[TokenA.decimals], 260n * BI_POWS[TokenA.decimals]],
       SwapSide.SELL,
       blockNumber,
       pools,
     );
     console.log(`${TokenASymbol} <> ${TokenBSymbol} Pool Prices: `, poolPrices);
 
-    expect(poolPrices![0].prices[0].toString()).toEqual('0');
+    expect(poolPrices![0].prices[1].toString()).toEqual('0');
+    expect(poolPrices![0].prices[2].toString()).toEqual('0');
   });
 
   it('getPoolIdentifiers and getPricesVolume SELL', async function () {
@@ -140,11 +179,11 @@ describe('ParaSwapLimitOrders', function () {
     }
 
     // Check pricing values
-    // for (let i = 0; i < expectedPricesOnSell.length; i++) {
-    //   expect(poolPrices![0].prices[i].toString()).toEqual(
-    //     expectedPricesOnSell[i].toString(),
-    //   );
-    // }
+    for (let i = 0; i < expectedPricesOnSell.length; i++) {
+      expect(poolPrices![0].prices[i].toString()).toEqual(
+        expectedPricesOnSell[i].toString(),
+      );
+    }
   });
   it('getPoolIdentifiers and getPricesVolume BUY Unswappable amount', async function () {
     const pools = await paraSwapLimitOrders.getPoolIdentifiers(
@@ -160,14 +199,19 @@ describe('ParaSwapLimitOrders', function () {
     const poolPrices = await paraSwapLimitOrders.getPricesVolume(
       TokenA,
       TokenB,
-      [2000n * BI_POWS[TokenB.decimals]],
+      [
+        0n,
+        1700n * BI_POWS[TokenB.decimals],
+        2000000n * BI_POWS[TokenB.decimals],
+      ],
       SwapSide.BUY,
       blockNumber,
       pools,
     );
     console.log(`${TokenASymbol} <> ${TokenBSymbol} Pool Prices: `, poolPrices);
 
-    expect(poolPrices![0].prices[0].toString()).toEqual('0');
+    expect(poolPrices![0].prices[1].toString()).toEqual('0');
+    expect(poolPrices![0].prices[2].toString()).toEqual('0');
   });
 
   it('getPoolIdentifiers and getPricesVolume BUY', async function () {
@@ -195,7 +239,13 @@ describe('ParaSwapLimitOrders', function () {
     if (paraSwapLimitOrders.hasConstantPriceLargeAmounts) {
       checkConstantPoolPrices(poolPrices!, amounts, dexKey);
     } else {
-      checkPoolPrices(poolPrices!, amounts, SwapSide.BUY, dexKey, false);
+      checkPoolPrices(poolPrices!, amountsBuy, SwapSide.BUY, dexKey);
+    }
+
+    for (let i = 0; i < expectedPricesOnBuy.length; i++) {
+      expect(poolPrices![0].prices[i].toString()).toEqual(
+        expectedPricesOnBuy[i].toString(),
+      );
     }
   });
 
