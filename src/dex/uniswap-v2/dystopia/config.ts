2cde79381ac8676432afa87136902b0bcbebe3a9
Verisana <account@0112.pro>
2022-08-01 14:00:31 +0300

    feat: update Dystopia configs with new adapter

--- src/dex/uniswap-v2/dystopia/config.ts
+++ src/dex/uniswap-v2/dystopia/config.ts
@@ -20,15 +20,16 @@ export const DystopiaConfig: DexConfigMap<DexParams> = {
     [Network.FANTOM]: {
       factoryAddress: '0x9d3591719038752db0c8bEEe2040FfcC3B2c6B9c',
       // ParaSwap-compatible Router with stable pools support
+      // TODO: Update router address once it is deployed
       router: '0x0E98A8e5ca6067B98d10Eb6476ec30E232346402',
       initCode:
         '0x5442fb448d86f32a7d2a9dc1a457e64bf5a6c77415d98802aac4fb5a9dc5ecd9',
       stableFee: 2000,
       volatileFee: 500,
+      poolGasCost: 180 * 1000,
 
-      // TODO: Not correct, need to update for event based
+      // TODO: Not correct, need to update implementation for event based
       feeCode: 2000,
-      poolGasCost: 18000000000000 * 1000,
     },
   },
 };
@@ -36,5 +37,8 @@ export const DystopiaConfig: DexConfigMap<DexParams> = {
 export const Adapters: Record<number, AdapterMappings> = {
   [Network.POLYGON]: {
     [SwapSide.SELL]: [{ name: 'PolygonAdapter02', index: 3 }],
+  },
+  [Network.FANTOM]: {
+    [SwapSide.SELL]: [{ name: 'FantomAdapter01', index: 10 }],
   },
 };
