e971fb70bfe8dd4e94f23ad7c5975df6921a9a0e
Mohamed Hassen Mami <mohamed@paraswap.io>
2022-08-16 10:49:49 +0200

    solidly: restore dystopia integration tests

---
+++ src/dex/solidly/solidly-integration.test.ts
@@ -0,0 +1,224 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { DummyDexHelper } from '../../dex-helper';
+import { Network, SwapSide } from '../../constants';
+import { checkPoolPrices, checkPoolsLiquidity } from '../../../tests/utils';
+import { BI_POWS } from '../../bigint-constants';
+import { Solidly } from './solidly';
+import { Tokens } from '../../../tests/constants-e2e';
+import { Interface, Result } from '@ethersproject/abi';
+import solidlyPairABI from '../../abi/solidly/SolidlyPair.json';
+
+const amounts18 = [0n, BI_POWS[18], 2000000000000000000n];
+
+function getReaderCalldata(
+  exchangeAddress: string,
+  readerIface: Interface,
+  amounts: bigint[],
+  funcName: string,
+  tokenIn: string,
+) {
+  return amounts.map(amount => ({
+    target: exchangeAddress,
+    callData: readerIface.encodeFunctionData(funcName, [amount, tokenIn]),
+  }));
+}
+
+function decodeReaderResult(
+  results: Result,
+  readerIface: Interface,
+  funcName: string,
+) {
+  return results.map(result => {
+    const parsed = readerIface.decodeFunctionResult(funcName, result);
+    return BigInt(parsed[0]._hex);
+  });
+}
+
+const constructCheckOnChainPricing =
+  (dexHelper: DummyDexHelper) =>
+  async (
+    soldily: Solidly,
+    funcName: string,
+    blockNumber: number,
+    prices: bigint[],
+    exchangeAddress: string,
+    tokenIn: string,
+    amounts: bigint[],
+  ) => {
+    const readerIface = new Interface(solidlyPairABI as any);
+
+    const readerCallData = getReaderCalldata(
+      exchangeAddress,
+      readerIface,
+      amounts.slice(1),
+      funcName,
+      tokenIn,
+    );
+    console.log('readerCallData', readerCallData);
+    const readerResult = (
+      await dexHelper.multiContract.methods
+        .aggregate(readerCallData)
+        .call({}, blockNumber)
+    ).returnData;
+    const expectedPrices = [0n].concat(
+      decodeReaderResult(readerResult, readerIface, funcName),
+    );
+
+    expect(prices).toEqual(expectedPrices);
+  };
+
+describe('Solidly integration tests', () => {
+  describe('Polygon', () => {
+    const network = Network.POLYGON;
+    const dexHelper = new DummyDexHelper(network);
+    const checkOnChainPricing = constructCheckOnChainPricing(dexHelper);
+
+    describe('Dystopia', function () {
+      const dexKey = 'Dystopia';
+      const dystopia = new Solidly(network, dexKey, dexHelper);
+
+      describe('UniswapV2 like pool', function () {
+        const TokenASymbol = 'WETH';
+        const tokenA = Tokens[network][TokenASymbol];
+        const TokenBSymbol = 'WMATIC';
+        const tokenB = Tokens[network][TokenBSymbol];
+
+        const amounts = amounts18;
+
+        it('getPoolIdentifiers and getPricesVolume', async function () {
+          const blocknumber = await dexHelper.web3Provider.eth.getBlockNumber();
+          const dystopia = new Solidly(network, dexKey, dexHelper);
+          const pools = await dystopia.getPoolIdentifiers(
+            tokenA,
+            tokenB,
+            SwapSide.SELL,
+            blocknumber,
+          );
+          console.log(
+            `${TokenASymbol} <> ${TokenBSymbol} Pool Identifiers: `,
+            pools,
+          );
+
+          expect(pools.length).toBeGreaterThan(0);
+
+          const poolPrices = await dystopia.getPricesVolume(
+            tokenA,
+            tokenB,
+            amounts,
+            SwapSide.SELL,
+            blocknumber,
+            pools,
+          );
+          console.log(
+            `${TokenASymbol} <> ${TokenBSymbol} Pool Prices: `,
+            poolPrices,
+          );
+
+          expect(poolPrices).not.toBeNull();
+          checkPoolPrices(poolPrices!, amounts, SwapSide.SELL, dexKey);
+
+          // Check if onchain pricing equals to calculated ones
+
+          for (const i in poolPrices || []) {
+            await checkOnChainPricing(
+              dystopia,
+              'getAmountOut',
+              blocknumber,
+              poolPrices![i].prices,
+              poolPrices![i].poolAddresses![0],
+              tokenA.address,
+              amounts,
+            );
+          }
+        });
+
+        it('getTopPoolsForToken', async function () {
+          const dexHelper = new DummyDexHelper(Network.POLYGON);
+          const dystopia = new Solidly(Network.POLYGON, dexKey, dexHelper);
+
+          const poolLiquidity = await dystopia.getTopPoolsForToken(
+            tokenA.address,
+            10,
+          );
+          console.log(`${TokenASymbol} Top Pools:`, poolLiquidity);
+
+          checkPoolsLiquidity(poolLiquidity, tokenA.address, dexKey);
+        });
+      });
+
+      describe('Curve like stable pool', function () {
+        const TokenASymbol = 'DAI'; // 'USDT';
+        const tokenA = Tokens[Network.POLYGON][TokenASymbol];
+        const TokenBSymbol = 'USDC';
+        const tokenB = Tokens[Network.POLYGON][TokenBSymbol];
+
+        const amounts = amounts18; // amounts6;
+
+        it('getPoolIdentifiers and getPricesVolume', async function () {
+          const dexHelper = new DummyDexHelper(Network.POLYGON);
+          const blocknumber = await dexHelper.web3Provider.eth.getBlockNumber();
+          const pools = await dystopia.getPoolIdentifiers(
+            tokenA,
+            tokenB,
+            SwapSide.SELL,
+            blocknumber,
+          );
+          console.log(
+            `${TokenASymbol} <> ${TokenBSymbol} Pool Identifiers: `,
+            pools,
+          );
+
+          expect(pools.length).toBeGreaterThan(0);
+
+          const poolPrices = await dystopia.getPricesVolume(
+            tokenA,
+            tokenB,
+            amounts,
+            SwapSide.SELL,
+            blocknumber,
+            pools,
+          );
+          console.log(
+            `${TokenASymbol} <> ${TokenBSymbol} Pool Prices: `,
+            poolPrices,
+          );
+
+          expect(poolPrices).not.toBeNull();
+          checkPoolPrices(poolPrices!, amounts, SwapSide.SELL, dexKey);
+
+          // Check if onchain pricing equals to calculated ones
+          for (const i in poolPrices || []) {
+            await checkOnChainPricing(
+              dystopia,
+              'getAmountOut',
+              blocknumber,
+              poolPrices![i].prices,
+              poolPrices![i].poolAddresses![0],
+              tokenA.address,
+              amounts,
+            );
+          }
+        });
+
+        it('getTopPoolsForToken', async function () {
+          const dexHelper = new DummyDexHelper(Network.POLYGON);
+          const dystopiaStable = new Solidly(
+            Network.POLYGON,
+            dexKey,
+            dexHelper,
+          );
+
+          const poolLiquidity = await dystopiaStable.getTopPoolsForToken(
+            tokenA.address,
+            10,
+          );
+          console.log(`${TokenASymbol} Top Pools:`, poolLiquidity);
+
+          checkPoolsLiquidity(poolLiquidity, tokenA.address, dexKey);
+        });
+      });
+    });
+  });
+});
