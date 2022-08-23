b19d1b340c87c175f9a5c3609e5f5ac7e27807d3
Verisana <account@0112.pro>
2022-08-23 15:25:03 +0200

    test: create e2e test

--- src/dex/synthetix/synthetix-e2e.test.ts
+++ src/dex/synthetix/synthetix-e2e.test.ts
@@ -2,94 +2,172 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { testE2E } from '../../../tests/utils-e2e';
-import { Tokens, Holders, NativeTokenSymbols } from '../../../tests/constants-e2e';
-import {
-  Network,
-  ContractMethod,
-  SwapSide,
-} from '../../constants';
+import { Tokens, Holders } from '../../../tests/constants-e2e';
+import { Network, ContractMethod, SwapSide } from '../../constants';
 import { StaticJsonRpcProvider } from '@ethersproject/providers';
 import { generateConfig } from '../../config';
 
-describe('Synthetix E2E', () => {
-  const dexKey = 'Synthetix';
+const dexKey = 'Synthetix';
 
-  describe('Synthetix MAINNET', () => {
-    const network = Network.MAINNET;
-    const tokens = Tokens[network];
-    const holders = Holders[network];
-    const provider = new StaticJsonRpcProvider(generateConfig(network).privateHttpProvider, network);
+async function testForNetwork(
+  network: Network,
+  tokenChainlinkSymbol: string,
+  tokenDexAggregatorSymbol: string,
+  sUSDSymbol: string,
+  tokenChainlinkAmount: string,
+  tokenDexAggregatorAmount: string,
+  sUSDAmount: string,
+) {
+  const tokens = Tokens[network];
+  const holders = Holders[network];
+  const provider = new StaticJsonRpcProvider(
+    generateConfig(network).privateHttpProvider,
+    network,
+  );
 
-    // TODO: Modify the tokenASymbol, tokenBSymbol, tokenAAmount;
-    const tokenASymbol: string = 'tokenASymbol';
-    const tokenBSymbol: string = 'tokenBSymbol';
-    const nativeTokenSymbol = NativeTokenSymbols[network];
-
-    const tokenAAmount: string = 'tokenAAmount';
-    const tokenBAmount: string = 'tokenBAmount';
-    const nativeTokenAmount = '1000000000000000000';
-
-    // TODO: Add any direct swap contractMethod name if it exists
-    // TODO: If buy is not supported remove the buy contract methods
-
-    const sideToContractMethods = new Map([
-      [SwapSide.SELL, [
+  const sideToContractMethods = new Map([
+    [
+      SwapSide.SELL,
+      [
         ContractMethod.simpleSwap,
         ContractMethod.multiSwap,
         ContractMethod.megaSwap,
-      ]],
-      [SwapSide.BUY, [
-        ContractMethod.simpleBuy,
-        ContractMethod.buy
-      ]],
-    ]);
+      ],
+    ],
+  ]);
 
-    sideToContractMethods.forEach((contractMethods, side) =>
-      contractMethods.forEach((contractMethod: ContractMethod) => {
-        describe(`${contractMethod}`, () => {
-          it(nativeTokenSymbol + ' -> TOKEN', async () => {
-            await testE2E(
-              tokens[nativeTokenSymbol],
-              tokens[tokenASymbol],
-              holders[nativeTokenSymbol],
-              side === SwapSide.SELL ? nativeTokenAmount : tokenAAmount,
-              side,
-              dexKey,
-              contractMethod,
-              network,
-              provider,
-            );
-          });
-          it('TOKEN -> ' + nativeTokenSymbol, async () => {
-            await testE2E(
-              tokens[tokenASymbol],
-              tokens[nativeTokenSymbol],
-              holders[tokenASymbol],
-              side === SwapSide.SELL ? tokenAAmount: nativeTokenAmount,
-              side,
-              dexKey,
-              contractMethod,
-              network,
-              provider,
-            );
-          });
-          it('TOKEN -> TOKEN', async () => {
-            await testE2E(
-              tokens[tokenASymbol],
-              tokens[tokenBSymbol],
-              holders[tokenASymbol],
-              side === SwapSide.SELL ? tokenAAmount: tokenBAmount,
-              side,
-              dexKey,
-              contractMethod,
-              network,
-              provider,
-            );
-          });
+  sideToContractMethods.forEach((contractMethods, side) =>
+    contractMethods.forEach((contractMethod: ContractMethod) => {
+      describe(`${contractMethod}`, () => {
+        it(`${network} ${side} ${contractMethod} ${sUSDSymbol} -> ${tokenChainlinkSymbol}`, async () => {
+          await testE2E(
+            tokens[sUSDSymbol],
+            tokens[tokenChainlinkSymbol],
+            holders[sUSDSymbol],
+            side === SwapSide.SELL ? sUSDAmount : tokenChainlinkAmount,
+            side,
+            dexKey,
+            contractMethod,
+            network,
+            provider,
+          );
         });
-      }),
-    );
+        it(`${network} ${side} ${contractMethod} ${sUSDSymbol} -> ${tokenDexAggregatorSymbol}`, async () => {
+          await testE2E(
+            tokens[sUSDSymbol],
+            tokens[tokenDexAggregatorSymbol],
+            holders[sUSDSymbol],
+            side === SwapSide.SELL ? sUSDAmount : tokenDexAggregatorAmount,
+            side,
+            dexKey,
+            contractMethod,
+            network,
+            provider,
+          );
+        });
+        it(`${network} ${side} ${contractMethod} ${tokenChainlinkSymbol} -> ${sUSDSymbol}`, async () => {
+          await testE2E(
+            tokens[tokenChainlinkSymbol],
+            tokens[sUSDSymbol],
+            holders[tokenChainlinkAmount],
+            side === SwapSide.SELL ? tokenChainlinkAmount : sUSDAmount,
+            side,
+            dexKey,
+            contractMethod,
+            network,
+            provider,
+          );
+        });
+        it(`${network} ${side} ${contractMethod} ${tokenDexAggregatorSymbol} -> ${sUSDSymbol}`, async () => {
+          await testE2E(
+            tokens[tokenDexAggregatorSymbol],
+            tokens[sUSDSymbol],
+            holders[tokenDexAggregatorSymbol],
+            side === SwapSide.SELL ? tokenDexAggregatorAmount : sUSDAmount,
+            side,
+            dexKey,
+            contractMethod,
+            network,
+            provider,
+          );
+        });
+        it(`${network} ${side} ${contractMethod} ${tokenChainlinkSymbol} -> ${tokenDexAggregatorSymbol}`, async () => {
+          await testE2E(
+            tokens[tokenChainlinkSymbol],
+            tokens[tokenDexAggregatorSymbol],
+            holders[tokenChainlinkAmount],
+            side === SwapSide.SELL
+              ? tokenChainlinkAmount
+              : tokenDexAggregatorAmount,
+            side,
+            dexKey,
+            contractMethod,
+            network,
+            provider,
+          );
+        });
+        it(`${network} ${side} ${contractMethod} ${tokenDexAggregatorSymbol} -> ${tokenChainlinkSymbol}`, async () => {
+          await testE2E(
+            tokens[tokenDexAggregatorSymbol],
+            tokens[tokenChainlinkSymbol],
+            holders[tokenDexAggregatorAmount],
+            side === SwapSide.SELL
+              ? tokenDexAggregatorAmount
+              : tokenChainlinkAmount,
+            side,
+            dexKey,
+            contractMethod,
+            network,
+            provider,
+          );
+        });
+      });
+    }),
+  );
+}
 
-    // TODO: Add any additional test cases required to test Synthetix
+describe('Synthetix E2E', () => {
+  describe('Synthetix MAINNET', () => {
+    const network = Network.MAINNET;
+
+    const tokenChainlinkSymbol = 'sETH';
+    const tokenDexAggregatorSymbol = 'sBTC';
+    const sUSDSymbol = 'sUSD';
+
+    const tokenChainlinkAmount = '1000000000000000000';
+    const tokenDexAggregatorAmount = '1000000000000000000';
+    const sUSDAmount = '1000000000000000000';
+
+    testForNetwork(
+      network,
+      tokenChainlinkSymbol,
+      tokenDexAggregatorSymbol,
+      sUSDSymbol,
+      tokenChainlinkAmount,
+      tokenDexAggregatorAmount,
+      sUSDAmount,
+    );
+  });
+
+  describe('Synthetix OPTIMISM', () => {
+    const network = Network.OPTIMISM;
+
+    const tokenChainlinkSymbol = 'sETH';
+    const tokenDexAggregatorSymbol = 'sBTC';
+    const sUSDSymbol = 'sUSD';
+
+    const tokenChainlinkAmount = '1000000000000000000';
+    const tokenDexAggregatorAmount = '1000000000000000000';
+    const sUSDAmount = '1000000000000000000';
+
+    testForNetwork(
+      network,
+      tokenChainlinkSymbol,
+      tokenDexAggregatorSymbol,
+      sUSDSymbol,
+      tokenChainlinkAmount,
+      tokenDexAggregatorAmount,
+      sUSDAmount,
+    );
   });
 });
