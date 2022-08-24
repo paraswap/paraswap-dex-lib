57fa39444cea8b38220c0d9bcd6e598156f294fb
Verisana <account@0112.pro>
2022-08-24 17:05:06 +0200

    feat: comment all Optimism related logic

--- src/dex/synthetix/contract-math/synthetix-math.ts
+++ src/dex/synthetix/contract-math/synthetix-math.ts
@@ -1,3 +1,4 @@
+import { _require } from '../../../utils';
 import { PoolState } from '../types';
 import { exchangeRatesWithDexPricing } from './ExchangeRatesWithDexPricing';
 import { SafeDecimalMath } from './SafeDecimalMath';
@@ -17,6 +18,88 @@ export class SynthetixMath {
     );
     return amountReceived;
   }
+
+  // TODO: Implement pricing for Optimism
+  // getAmountsForExchange(
+  //   state: PoolState,
+  //   sourceAmount: bigint,
+  //   sourceCurrencyKey: string,
+  //   destinationCurrencyKey: string,
+  // ) {
+  //   const [exchangeFeeRate, tooVolatile] = this._feeRateForExchange(
+  //     sourceCurrencyKey,
+  //     destinationCurrencyKey,
+  //   );
+
+  //   // check rates volatility result
+  //   _require(
+  //     !tooVolatile,
+  //     'exchange rates too volatile',
+  //     { sourceAmount, sourceCurrencyKey, destinationCurrencyKey, tooVolatile },
+  //     '!tooVolatile',
+  //   );
+
+  //   const destinationAmount = _effectiveValueAndRates(
+  //     sourceCurrencyKey,
+  //     sourceAmount,
+  //     destinationCurrencyKey,
+  //   );
+
+  //   return this._deductFeesFromAmount(destinationAmount, exchangeFeeRate);
+  // }
+
+  // private _feeRateForExchange(
+  //   state: PoolState,
+  //   sourceCurrencyKey: string,
+  //   destinationCurrencyKey: string,
+  // ): [bigint, boolean] {
+  //   // Get the exchange fee rate as per the source currencyKey and destination currencyKey
+  //   const baseRate =
+  //     this._getExchangeFeeRate(state, sourceCurrencyKey) +
+  //     this._getExchangeFeeRate(state, destinationCurrencyKey);
+
+  //   const [dynamicFee, tooVolatile] = this._dynamicFeeRateForExchange(
+  //     state,
+  //     sourceCurrencyKey,
+  //     destinationCurrencyKey,
+  //   );
+  //   return [baseRate + dynamicFee, tooVolatile];
+  // }
+
+  // private _dynamicFeeRateForExchange(
+  //   state: PoolState,
+  //   sourceCurrencyKey: string,
+  //   destinationCurrencyKey: string,
+  // ): [bigint, boolean] {
+  //   const config = getExchangeDynamicFeeConfig();
+  //   const [dynamicFeeDst, dstVolatile] = this._dynamicFeeRateForCurrency(
+  //     state,
+  //     destinationCurrencyKey,
+  //     config,
+  //   );
+  //   const [dynamicFeeSrc, srcVolatile] = this._dynamicFeeRateForCurrency(
+  //     state,
+  //     sourceCurrencyKey,
+  //     config,
+  //   );
+  //   let dynamicFee = dynamicFeeDst + dynamicFeeSrc;
+  //   // cap to maxFee
+  //   const overMax = dynamicFee > config.maxFee;
+  //   dynamicFee = overMax ? config.maxFee : dynamicFee;
+  //   return [dynamicFee, overMax || dstVolatile || srcVolatile];
+  // }
+
+  // private _dynamicFeeRateForCurrency(
+  //   state: PoolState,
+  //   currencyKey: string,
+  // ): [bigint, boolean] {
+  //   // no dynamic dynamicFee for sUSD or too few rounds
+  //   if (currencyKey === state.sUSDCurrencyKey || config.rounds <= 1) {
+  //     return [0n, false];
+  //   }
+  //   const roundId = exchangeRates().getCurrentRoundId(currencyKey);
+  //   return _dynamicFeeRateForCurrencyRound(currencyKey, roundId, config);
+  // }
 
   private _getAmountsForAtomicExchangeMinusFees(
     state: PoolState,
