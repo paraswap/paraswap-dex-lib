5699473f9e6459592ace0da52c6ddc837551f1aa
Verisana <account@0112.pro>
2022-09-26 18:03:01 +0200

    fix: revert event handler from to src

--- src/dex/curve-v1/pools/3pool.ts
+++ src/dex/curve-v1/pools/3pool.ts
@@ -67,7 +67,7 @@ export class ThreePool extends CurvePool {
   }
 
   handleCoinTransfer(event: any, state: PoolState, log: Log): PoolState {
-    const from = event.args.from;
+    const from = event.args.src;
     const coin = log.address;
 
     if (from.toLowerCase() == this.address.toLowerCase())
@@ -119,7 +119,7 @@ export class ThreePool extends CurvePool {
     const i = _.findIndex(
       this.COINS,
       c => c.toLowerCase() === this.lastTransferredCoin?.toLowerCase(),
-    )
+    );
     if (i == -1) {
       this.logger.error(
         `Error: expected coin to have a transfer event before RemoveLiquidityOne event`,
