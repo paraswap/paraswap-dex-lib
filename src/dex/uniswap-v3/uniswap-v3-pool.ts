ee83633bccf7f1b48ff0dc2b773737ddc7539f77
Verisana <account@0112.pro>
2022-10-20 19:18:19 +0100

    Revert "fix: typo in function"
    This reverts commit b2f929b76acc62447ca13e602df2c843e15b2592.

--- src/dex/uniswap-v3/uniswap-v3-pool.ts
+++ src/dex/uniswap-v3/uniswap-v3-pool.ts
@@ -268,7 +268,7 @@ export class UniswapV3EventPool extends StatefulEventSubscriber<PoolState> {
     const tickUpper = bigIntify(event.args.tickUpper);
     pool.blockTimestamp = bigIntify(blockHeader.timestamp);
 
-    if (this._isLiquidityChangedOutOfKnownRange(pool, tickLower, tickUpper)) {
+    if (this._isLiquidityChangeOutOfKnownRange(pool, tickLower, tickUpper)) {
       return pool;
     }
 
@@ -292,7 +292,7 @@ export class UniswapV3EventPool extends StatefulEventSubscriber<PoolState> {
     const tickUpper = bigIntify(event.args.tickUpper);
     pool.blockTimestamp = bigIntify(blockHeader.timestamp);
 
-    if (this._isLiquidityChangedOutOfKnownRange(pool, tickLower, tickUpper)) {
+    if (this._isLiquidityChangeOutOfKnownRange(pool, tickLower, tickUpper)) {
       return pool;
     }
 
@@ -333,7 +333,7 @@ export class UniswapV3EventPool extends StatefulEventSubscriber<PoolState> {
     return pool;
   }
 
-  private _isLiquidityChangedOutOfKnownRange(
+  private _isLiquidityChangeOutOfKnownRange(
     state: PoolState,
     tickLower: bigint,
     tickUpper: bigint,
