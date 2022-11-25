aad9e32d084e9bbae082e862e190d8c1e5644ad3
0xpercival <percival@camelot.fi>
2022-11-25 15:08:05 +0100

    camelot: update router address

--- src/dex/camelot/config.ts
+++ src/dex/camelot/config.ts
@@ -8,7 +8,7 @@ export const CamelotConfig: DexConfigMap<DexParams> = {
       subgraphURL:
         'https://api.thegraph.com/subgraphs/name/camelotlabs/camelot-amm-2',
       factoryAddress: '0x6EcCab422D763aC031210895C81787E87B43A652',
-      router: '0xd8f185769b6E2918B759e83F7EC268C882800EC7',
+      router: '0x1Be46c7A40906c19d91d07B3AE69Ef5893268F25',
       initCode:
         '0xa856464ae65f7619087bc369daaf7e387dae1e5af69cfa7935850ebf754b04c1',
       feeCode: 0, // this is ignored as Camelot uses dynamic fees
