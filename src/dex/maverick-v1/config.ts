108e430d341cc014c87590d6a4c8da6285b44d27
Mohamed Hassen Mami <mohamed@paraswap.io>
2023-03-07 18:30:00 +0100

    update subgraph url

--- src/dex/maverick-v1/config.ts
+++ src/dex/maverick-v1/config.ts
@@ -6,7 +6,7 @@ export const MaverickV1Config: DexConfigMap<DexParams> = {
   MaverickV1: {
     [Network.MAINNET]: {
       subgraphURL:
-        'https://gateway.thegraph.com/api/4b42aaaee3e81a3f29390e9df7010cbc/subgraphs/id/Ccu7XxsbU7SS4VfNfmy6YRBCbNrGYpKYpBEfxzy5Fkr1',
+        'https://gateway.thegraph.com/api/4b42aaaee3e81a3f29390e9df7010cbc/subgraphs/id/9n4uEbt1XV6DnYH7AGHJ84Gh2fpu68iwxwBKnWqVMX9G',
       routerAddress: '0x4a585e0f7c18e2c414221d6402652d5e0990e5f8',
       poolInspectorAddress: '0xaA5BF61a664109e959D69C38734d4EA7dF74e456',
     },
