a95698856a9a663bb90dbee382fac45a795489dc
Alexander Burkut <a.burkut90@gmail.com>
2023-03-07 19:09:45 +0300

    replace router for camelot

--- src/dex/camelot/config.ts
+++ src/dex/camelot/config.ts
@@ -8,7 +8,7 @@ export const CamelotConfig: DexConfigMap<DexParams> = {
       subgraphURL:
         'https://api.thegraph.com/subgraphs/name/camelotlabs/camelot-amm-2',
       factoryAddress: '0x6EcCab422D763aC031210895C81787E87B43A652',
-      router: '0x1Be46c7A40906c19d91d07B3AE69Ef5893268F25',
+      router: '0xb2634B3CBc1E401AB3C2743DB44d459C5c9aA662',
       initCode:
         '0xa856464ae65f7619087bc369daaf7e387dae1e5af69cfa7935850ebf754b04c1',
       feeCode: 0, // this is ignored as Camelot uses dynamic fees
