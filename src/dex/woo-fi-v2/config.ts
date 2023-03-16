091437ab3f1edbf00568909be838defe8e718e2e
Verisana <account@0112.pro>
2023-03-16 16:12:12 +0000

    feat: update adapter addresses for woo fi v2

--- src/dex/woo-fi-v2/config.ts
+++ src/dex/woo-fi-v2/config.ts
@@ -68,16 +68,20 @@ export const WooFiV2Config: DexConfigMap<DexParams> = {
 };
 
 export const Adapters: Record<number, AdapterMappings> = {
+  [Network.OPTIMISM]: {
+    [SwapSide.SELL]: [{ name: 'OptimismAdapter01', index: 11 }],
+  },
   [Network.BSC]: { [SwapSide.SELL]: [{ name: 'BscAdapter01', index: 13 }] },
-  [Network.AVALANCHE]: {
-    [SwapSide.SELL]: [{ name: 'AvalancheAdapter01', index: 12 }],
+  [Network.POLYGON]: {
+    [SwapSide.SELL]: [{ name: 'PolygonAdapter02', index: 4 }],
   },
   [Network.FANTOM]: {
     [SwapSide.SELL]: [{ name: 'FantomAdapter01', index: 8 }],
   },
-  [Network.POLYGON]: {
-    [SwapSide.SELL]: [{ name: 'PolygonAdapter02', index: 4 }],
+  [Network.ARBITRUM]: {
+    [SwapSide.SELL]: [{ name: 'ArbitrumAdapter02', index: 3 }],
   },
-
-  // TODO: Deploy new adapter for WooFiV2 on new chains and update this config
+  [Network.AVALANCHE]: {
+    [SwapSide.SELL]: [{ name: 'AvalancheAdapter01', index: 12 }],
+  },
 };
