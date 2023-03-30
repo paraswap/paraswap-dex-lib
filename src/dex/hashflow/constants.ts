6b8c6bb9d15e9cf0280facc0148f77a7faf6c919
Alexander Burkut <a.burkut90@gmail.com>
2023-03-30 14:22:43 +0300

    increased cache ttl for marker makers and prices

--- src/dex/hashflow/constants.ts
+++ src/dex/hashflow/constants.ts
@@ -1,13 +1,12 @@
-export const PRICE_LEVELS_TTL_SECONDS = 1;
 
 // 7 days
 export const HASHFLOW_BLACKLIST_TTL_S = 60 * 60 * 24 * 7;
 
 export const HASHFLOW_MM_RESTRICT_TTL_S = 60 * 60;
 
-export const HASHFLOW_PRICES_CACHES_TTL_S = 2;
+export const HASHFLOW_PRICES_CACHES_TTL_S = 3;
 
-export const HASHFLOW_MARKET_MAKERS_CACHES_TTL_S = 2;
+export const HASHFLOW_MARKET_MAKERS_CACHES_TTL_S = 3;
 
 export const HASHFLOW_API_POLLING_INTERVAL_MS = 0;
 
