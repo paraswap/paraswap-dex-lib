e060da2ee1f96b870edf1ee2a13486971c175613
paraschiv-r-toptal <razvan.paraschiv@toptal.com>
2023-05-25 14:45:48 +0300

    Add tests for multiple networks

--- src/dex/pancakeswap-v3/pancakeswap-v3-integration.test.ts
+++ src/dex/pancakeswap-v3/pancakeswap-v3-integration.test.ts
@@ -11,42 +11,361 @@ import { Tokens } from '../../../tests/constants-e2e';
 import PancakeswapV3QuoterABI from '../../abi/pancakeswap-v3/PancakeswapV3Quoter.abi.json';
 import { Address } from '@paraswap/core';
 import { PancakeswapV3 } from './pancakeswap-v3';
+import * as net from 'net';
 
-const network = Network.MAINNET;
-const stableASymbol = 'USDC';
-const stableA = Tokens[network][stableASymbol];
+const networks = [Network.MAINNET, Network.BSC];
 
-const stableBSymbol = 'USDT';
-const stableB = Tokens[network][stableBSymbol];
-
-const tokenASymbol = 'WBTC';
-const tokenA = Tokens[network][tokenASymbol];
-
-const tokenBSymbol = 'WETH';
-const tokenB = Tokens[network][tokenBSymbol];
-
-const amounts = [0n, 1n * BI_POWS[8], 2n * BI_POWS[8], 3n * BI_POWS[8]];
-
-const amountsBuy = [0n, 1n * BI_POWS[18], 2n * BI_POWS[18], 3n * BI_POWS[18]];
-
-const stableAmounts = [
-  0n,
-  10_000n * BI_POWS[6],
-  20_000n * BI_POWS[6],
-  30_000n * BI_POWS[6],
-];
-
-const stableAmountsBuy = [
-  0n,
-  1n * BI_POWS[6],
-  2n * BI_POWS[6],
-  3n * BI_POWS[6],
-];
-
-const dexHelper = new DummyDexHelper(network);
 const dexKey = 'PancakeswapV3';
 
 const quoterIface = new Interface(PancakeswapV3QuoterABI);
+
+const testingData: Partial<{ [key in Network]: any }> = {
+  [Network.MAINNET]: {
+    tokenA: Tokens[Network.MAINNET]['WBTC'],
+    tokenASymbol: 'WBTC',
+    stableA: Tokens[Network.MAINNET]['USDC'],
+    stableASymbol: 'USDC',
+    tokenB: Tokens[Network.MAINNET]['WETH'],
+    tokenBSymbol: 'WETH',
+    stableB: Tokens[Network.MAINNET]['USDT'],
+    stableBSymbol: 'USDT',
+    stableSellAmounts: [
+      0n,
+      10_000n * BI_POWS[6],
+      20_000n * BI_POWS[6],
+      30_000n * BI_POWS[6],
+    ],
+    stableBuyAmounts: [0n, 1n * BI_POWS[6], 2n * BI_POWS[6], 3n * BI_POWS[6]],
+    regularSellAmounts: [0n, 1n * BI_POWS[8], 2n * BI_POWS[8], 3n * BI_POWS[8]],
+    regularBuyAmounts: [
+      0n,
+      1n * BI_POWS[18],
+      2n * BI_POWS[18],
+      3n * BI_POWS[18],
+    ],
+  },
+  [Network.BSC]: {
+    tokenA: Tokens[Network.BSC]['bBTC'],
+    tokenASymbol: 'bBTC',
+    stableA: Tokens[Network.BSC]['USDC'],
+    stableASymbol: 'USDC',
+    tokenB: Tokens[Network.BSC]['WBNB'],
+    tokenBSymbol: 'WBNB',
+    stableB: Tokens[Network.BSC]['USDT'],
+    stableBSymbol: 'USDT',
+    stableSellAmounts: [
+      0n,
+      10_000n * BI_POWS[6],
+      20_000n * BI_POWS[6],
+      30_000n * BI_POWS[6],
+    ],
+    stableBuyAmounts: [0n, 1n * BI_POWS[6], 2n * BI_POWS[6], 3n * BI_POWS[6]],
+    regularSellAmounts: [
+      0n,
+      1n * BI_POWS[18],
+      2n * BI_POWS[18],
+      3n * BI_POWS[18],
+    ],
+    regularBuyAmounts: [
+      0n,
+      1n * BI_POWS[18],
+      2n * BI_POWS[18],
+      3n * BI_POWS[18],
+    ],
+  },
+};
+
+describe('PancakeswapV3', function () {
+  networks.forEach(network =>
+    describe(network, function () {
+      let blockNumber: number;
+      let pancakeswapV3: PancakeswapV3;
+      const testData = testingData[network];
+      if (testData) {
+        const {
+          stableA,
+          stableASymbol,
+          stableB,
+          stableBSymbol,
+          stableSellAmounts,
+          stableBuyAmounts,
+          tokenA,
+          tokenASymbol,
+          tokenB,
+          tokenBSymbol,
+          regularSellAmounts,
+          regularBuyAmounts,
+        } = testData;
+        const dexHelper = new DummyDexHelper(network);
+
+        beforeEach(async () => {
+          blockNumber = await dexHelper.web3Provider.eth.getBlockNumber();
+          pancakeswapV3 = new PancakeswapV3(network, dexKey, dexHelper);
+        });
+
+        describe('Stable pairs', function () {
+          it('getPoolIdentifiers and getPricesVolume SELL stable pairs', async function () {
+            const pools = await pancakeswapV3.getPoolIdentifiers(
+              stableA,
+              stableB,
+              SwapSide.SELL,
+              blockNumber,
+            );
+            console.log(
+              `${stableASymbol} <> ${stableBSymbol} Pool Identifiers: `,
+              pools,
+            );
+
+            expect(pools.length).toBeGreaterThan(0);
+
+            const poolPrices = await pancakeswapV3.getPricesVolume(
+              stableA,
+              stableB,
+              stableSellAmounts,
+              SwapSide.SELL,
+              blockNumber,
+              pools,
+            );
+            console.log(
+              `${stableASymbol} <> ${stableBSymbol} Pool Prices: `,
+              poolPrices,
+            );
+
+            expect(poolPrices).not.toBeNull();
+            checkPoolPrices(
+              poolPrices!.filter(pp => pp.unit !== 0n),
+              stableSellAmounts,
+              SwapSide.SELL,
+              dexKey,
+            );
+
+            // Check if onchain pricing equals to calculated ones
+            let falseChecksCounter = 0;
+            await Promise.all(
+              poolPrices!.map(async price => {
+                const fee =
+                  pancakeswapV3.eventPools[price.poolIdentifier!]!.feeCode;
+                const res = await checkOnChainPricing(
+                  pancakeswapV3,
+                  dexHelper,
+                  'quoteExactInputSingle',
+                  blockNumber,
+                  price.prices,
+                  stableA.address,
+                  stableB.address,
+                  fee,
+                  stableSellAmounts,
+                );
+                if (res === false) falseChecksCounter++;
+              }),
+            );
+            expect(falseChecksCounter).toBeLessThan(poolPrices!.length);
+          });
+
+          it('getPoolIdentifiers and getPricesVolume BUY stable pairs', async function () {
+            const pools = await pancakeswapV3.getPoolIdentifiers(
+              stableA,
+              stableB,
+              SwapSide.BUY,
+              blockNumber,
+            );
+            console.log(
+              `${stableASymbol} <> ${stableBSymbol} Pool Identifiers: `,
+              pools,
+            );
+
+            expect(pools.length).toBeGreaterThan(0);
+
+            const poolPrices = await pancakeswapV3.getPricesVolume(
+              stableA,
+              stableB,
+              stableBuyAmounts,
+              SwapSide.BUY,
+              blockNumber,
+              pools,
+            );
+            console.log(
+              `${stableASymbol} <> ${stableBSymbol} Pool Prices: `,
+              poolPrices,
+            );
+
+            expect(poolPrices).not.toBeNull();
+            checkPoolPrices(
+              poolPrices!.filter(pp => pp.unit !== 0n),
+              stableBuyAmounts,
+              SwapSide.BUY,
+              dexKey,
+            );
+
+            // Check if onchain pricing equals to calculated ones
+            let falseChecksCounter = 0;
+            await Promise.all(
+              poolPrices!.map(async price => {
+                const fee =
+                  pancakeswapV3.eventPools[price.poolIdentifier!]!.feeCode;
+                const res = await checkOnChainPricing(
+                  pancakeswapV3,
+                  dexHelper,
+                  'quoteExactOutputSingle',
+                  blockNumber,
+                  price.prices,
+                  stableA.address,
+                  stableB.address,
+                  fee,
+                  stableBuyAmounts,
+                );
+                if (res === false) falseChecksCounter++;
+              }),
+            );
+            expect(falseChecksCounter).toBeLessThan(poolPrices!.length);
+          });
+
+          it('getTopPoolsForToken', async function () {
+            const poolLiquidity = await pancakeswapV3.getTopPoolsForToken(
+              stableA.address,
+              10,
+            );
+            console.log(`${stableASymbol} Top Pools:`, poolLiquidity);
+
+            if (!pancakeswapV3.hasConstantPriceLargeAmounts) {
+              checkPoolsLiquidity(poolLiquidity, stableA.address, dexKey);
+            }
+          });
+        });
+
+        describe('Regular pairs', function () {
+          it('getPoolIdentifiers and getPricesVolume SELL', async function () {
+            const pools = await pancakeswapV3.getPoolIdentifiers(
+              tokenA,
+              tokenB,
+              SwapSide.SELL,
+              blockNumber,
+            );
+            console.log(
+              `${tokenASymbol} <> ${tokenBSymbol} Pool Identifiers: `,
+              pools,
+            );
+
+            expect(pools.length).toBeGreaterThan(0);
+
+            const poolPrices = await pancakeswapV3.getPricesVolume(
+              tokenA,
+              tokenB,
+              regularSellAmounts,
+              SwapSide.SELL,
+              blockNumber,
+              pools,
+            );
+            console.log(
+              `${tokenASymbol} <> ${tokenBSymbol} Pool Prices: `,
+              poolPrices,
+            );
+
+            expect(poolPrices).not.toBeNull();
+            checkPoolPrices(
+              poolPrices!,
+              regularSellAmounts,
+              SwapSide.SELL,
+              dexKey,
+            );
+
+            let falseChecksCounter = 0;
+            await Promise.all(
+              poolPrices!.map(async price => {
+                const fee =
+                  pancakeswapV3.eventPools[price.poolIdentifier!]!.feeCode;
+                const res = await checkOnChainPricing(
+                  pancakeswapV3,
+                  dexHelper,
+                  'quoteExactInputSingle',
+                  blockNumber,
+                  price.prices,
+                  tokenA.address,
+                  tokenB.address,
+                  fee,
+                  regularSellAmounts,
+                );
+                if (res === false) falseChecksCounter++;
+              }),
+            );
+
+            expect(falseChecksCounter).toBeLessThan(poolPrices!.length);
+          });
+
+          it('getPoolIdentifiers and getPricesVolume BUY', async function () {
+            const pools = await pancakeswapV3.getPoolIdentifiers(
+              tokenA,
+              tokenB,
+              SwapSide.BUY,
+              blockNumber,
+            );
+            console.log(
+              `${stableASymbol} <> ${stableBSymbol} Pool Identifiers: `,
+              pools,
+            );
+
+            expect(pools.length).toBeGreaterThan(0);
+
+            const poolPrices = await pancakeswapV3.getPricesVolume(
+              tokenA,
+              tokenB,
+              regularBuyAmounts,
+              SwapSide.BUY,
+              blockNumber,
+              pools,
+            );
+            console.log(
+              `${tokenASymbol} <> ${tokenBSymbol} Pool Prices: `,
+              poolPrices,
+            );
+
+            expect(poolPrices).not.toBeNull();
+            checkPoolPrices(
+              poolPrices!,
+              regularBuyAmounts,
+              SwapSide.BUY,
+              dexKey,
+            );
+
+            // Check if onchain pricing equals to calculated ones
+            let falseChecksCounter = 0;
+            await Promise.all(
+              poolPrices!.map(async price => {
+                const fee =
+                  pancakeswapV3.eventPools[price.poolIdentifier!]!.feeCode;
+                const res = await checkOnChainPricing(
+                  pancakeswapV3,
+                  dexHelper,
+                  'quoteExactOutputSingle',
+                  blockNumber,
+                  price.prices,
+                  tokenA.address,
+                  tokenB.address,
+                  fee,
+                  regularBuyAmounts,
+                );
+                if (res === false) falseChecksCounter++;
+              }),
+            );
+            expect(falseChecksCounter).toBeLessThan(poolPrices!.length);
+          });
+
+          it('getTopPoolsForToken', async function () {
+            const poolLiquidity = await pancakeswapV3.getTopPoolsForToken(
+              tokenA.address,
+              10,
+            );
+            console.log(`${stableASymbol} Top Pools:`, poolLiquidity);
+
+            if (!pancakeswapV3.hasConstantPriceLargeAmounts) {
+              checkPoolsLiquidity(poolLiquidity, stableA.address, dexKey);
+            }
+          });
+        });
+      }
+    }),
+  );
+});
 
 function getReaderCalldata(
   exchangeAddress: string,
@@ -78,6 +397,7 @@ function decodeReaderResult(
 
 async function checkOnChainPricing(
   pancakeswapV3: PancakeswapV3,
+  dexHelper: DummyDexHelper,
   funcName: string,
   blockNumber: number,
   prices: bigint[],
@@ -138,230 +458,3 @@ async function checkOnChainPricing(
   );
   return true;
 }
-
-describe('PancakeswapV3', function () {
-  let blockNumber: number;
-  let pancakeswapV3: PancakeswapV3;
-
-  beforeEach(async () => {
-    blockNumber = await dexHelper.web3Provider.eth.getBlockNumber();
-    pancakeswapV3 = new PancakeswapV3(network, dexKey, dexHelper);
-  });
-
-  it('getPoolIdentifiers and getPricesVolume SELL', async function () {
-    const pools = await pancakeswapV3.getPoolIdentifiers(
-      tokenA,
-      tokenB,
-      SwapSide.SELL,
-      blockNumber,
-    );
-    console.log(`${tokenASymbol} <> ${tokenBSymbol} Pool Identifiers: `, pools);
-
-    expect(pools.length).toBeGreaterThan(0);
-
-    const poolPrices = await pancakeswapV3.getPricesVolume(
-      tokenA,
-      tokenB,
-      amounts,
-      SwapSide.SELL,
-      blockNumber,
-      pools,
-    );
-    console.log(`${tokenASymbol} <> ${tokenBSymbol} Pool Prices: `, poolPrices);
-
-    expect(poolPrices).not.toBeNull();
-    checkPoolPrices(poolPrices!, amounts, SwapSide.SELL, dexKey);
-
-    let falseChecksCounter = 0;
-    await Promise.all(
-      poolPrices!.map(async price => {
-        const fee = pancakeswapV3.eventPools[price.poolIdentifier!]!.feeCode;
-        const res = await checkOnChainPricing(
-          pancakeswapV3,
-          'quoteExactInputSingle',
-          blockNumber,
-          price.prices,
-          tokenA.address,
-          tokenB.address,
-          fee,
-          amounts,
-        );
-        if (res === false) falseChecksCounter++;
-      }),
-    );
-
-    expect(falseChecksCounter).toBeLessThan(poolPrices!.length);
-  });
-
-  it('getPoolIdentifiers and getPricesVolume BUY', async function () {
-    const pools = await pancakeswapV3.getPoolIdentifiers(
-      tokenA,
-      tokenB,
-      SwapSide.BUY,
-      blockNumber,
-    );
-    console.log(
-      `${stableASymbol} <> ${stableBSymbol} Pool Identifiers: `,
-      pools,
-    );
-
-    expect(pools.length).toBeGreaterThan(0);
-
-    const poolPrices = await pancakeswapV3.getPricesVolume(
-      tokenA,
-      tokenB,
-      amountsBuy,
-      SwapSide.BUY,
-      blockNumber,
-      pools,
-    );
-    console.log(`${tokenASymbol} <> ${tokenBSymbol} Pool Prices: `, poolPrices);
-
-    expect(poolPrices).not.toBeNull();
-    checkPoolPrices(poolPrices!, amountsBuy, SwapSide.BUY, dexKey);
-
-    // Check if onchain pricing equals to calculated ones
-    let falseChecksCounter = 0;
-    await Promise.all(
-      poolPrices!.map(async price => {
-        const fee = pancakeswapV3.eventPools[price.poolIdentifier!]!.feeCode;
-        const res = await checkOnChainPricing(
-          pancakeswapV3,
-          'quoteExactOutputSingle',
-          blockNumber,
-          price.prices,
-          tokenA.address,
-          tokenB.address,
-          fee,
-          amountsBuy,
-        );
-        if (res === false) falseChecksCounter++;
-      }),
-    );
-    expect(falseChecksCounter).toBeLessThan(poolPrices!.length);
-  });
-
-  it('getPoolIdentifiers and getPricesVolume SELL stable pairs', async function () {
-    const pools = await pancakeswapV3.getPoolIdentifiers(
-      stableA,
-      stableB,
-      SwapSide.SELL,
-      blockNumber,
-    );
-    console.log(
-      `${stableASymbol} <> ${stableBSymbol} Pool Identifiers: `,
-      pools,
-    );
-
-    expect(pools.length).toBeGreaterThan(0);
-
-    const poolPrices = await pancakeswapV3.getPricesVolume(
-      stableA,
-      stableB,
-      stableAmounts,
-      SwapSide.SELL,
-      blockNumber,
-      pools,
-    );
-    console.log(
-      `${stableASymbol} <> ${stableBSymbol} Pool Prices: `,
-      poolPrices,
-    );
-
-    expect(poolPrices).not.toBeNull();
-    checkPoolPrices(
-      poolPrices!.filter(pp => pp.unit !== 0n),
-      stableAmounts,
-      SwapSide.SELL,
-      dexKey,
-    );
-
-    // Check if onchain pricing equals to calculated ones
-    let falseChecksCounter = 0;
-    await Promise.all(
-      poolPrices!.map(async price => {
-        const fee = pancakeswapV3.eventPools[price.poolIdentifier!]!.feeCode;
-        const res = await checkOnChainPricing(
-          pancakeswapV3,
-          'quoteExactInputSingle',
-          blockNumber,
-          price.prices,
-          stableA.address,
-          stableB.address,
-          fee,
-          stableAmounts,
-        );
-        if (res === false) falseChecksCounter++;
-      }),
-    );
-    expect(falseChecksCounter).toBeLessThan(poolPrices!.length);
-  });
-
-  it('getPoolIdentifiers and getPricesVolume BUY stable pairs', async function () {
-    const pools = await pancakeswapV3.getPoolIdentifiers(
-      stableA,
-      stableB,
-      SwapSide.BUY,
-      blockNumber,
-    );
-    console.log(
-      `${stableASymbol} <> ${stableBSymbol} Pool Identifiers: `,
-      pools,
-    );
-
-    expect(pools.length).toBeGreaterThan(0);
-
-    const poolPrices = await pancakeswapV3.getPricesVolume(
-      stableA,
-      stableB,
-      stableAmountsBuy,
-      SwapSide.BUY,
-      blockNumber,
-      pools,
-    );
-    console.log(
-      `${stableASymbol} <> ${stableBSymbol} Pool Prices: `,
-      poolPrices,
-    );
-
-    expect(poolPrices).not.toBeNull();
-    checkPoolPrices(
-      poolPrices!.filter(pp => pp.unit !== 0n),
-      stableAmountsBuy,
-      SwapSide.BUY,
-      dexKey,
-    );
-
-    // Check if onchain pricing equals to calculated ones
-    let falseChecksCounter = 0;
-    await Promise.all(
-      poolPrices!.map(async price => {
-        const fee = pancakeswapV3.eventPools[price.poolIdentifier!]!.feeCode;
-        const res = await checkOnChainPricing(
-          pancakeswapV3,
-          'quoteExactOutputSingle',
-          blockNumber,
-          price.prices,
-          stableA.address,
-          stableB.address,
-          fee,
-          stableAmountsBuy,
-        );
-        if (res === false) falseChecksCounter++;
-      }),
-    );
-    expect(falseChecksCounter).toBeLessThan(poolPrices!.length);
-  });
-
-  it('getTopPoolsForToken', async function () {
-    const poolLiquidity = await pancakeswapV3.getTopPoolsForToken(
-      Tokens[Network.MAINNET]['USDC'].address,
-      10,
-    );
-    console.log(`${stableASymbol} Top Pools:`, poolLiquidity);
-
-    if (!pancakeswapV3.hasConstantPriceLargeAmounts) {
-      checkPoolsLiquidity(poolLiquidity, stableA.address, dexKey);
-    }
-  });
-});
