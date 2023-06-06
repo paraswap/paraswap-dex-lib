453cbbe7a134f0405505ce5f6e0bf6fc3acadba8
Tiago Silva <tfn.da.silva@gmail.com>
2023-06-06 19:40:03 +0100

    Add Carbon integration template

---
+++ src/dex/carbon/config.ts
@@ -0,0 +1,15 @@
+import { DexParams } from './types';
+import { DexConfigMap, AdapterMappings } from '../../types';
+import { Network, SwapSide } from '../../constants';
+
+export const CarbonConfig: DexConfigMap<DexParams> = {
+  Carbon: {
+    // TODO: complete me!
+  },
+};
+
+export const Adapters: Record<number, AdapterMappings> = {
+  // TODO: add adapters for each chain
+  // This is an example to copy
+  [Network.MAINNET]: { [SwapSide.SELL]: [{ name: '', index: 0 }] },
+};
