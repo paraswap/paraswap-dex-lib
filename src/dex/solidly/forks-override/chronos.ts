c1eab798f02dae7c69451af3385d7afadff38301
Alexander Burkut <a.burkut90@gmail.com>
2023-06-13 16:40:36 +0300

    add getTopPoolsForToken method for chronos class

--- src/dex/solidly/forks-override/chronos.ts
+++ src/dex/solidly/forks-override/chronos.ts
@@ -1,11 +1,13 @@
 import { Solidly } from '../solidly';
 import { SolidlyPair } from '../types';
-import { Network } from '../../../constants';
+import { Network, SUBGRAPH_TIMEOUT } from '../../../constants';
 import { IDexHelper } from '../../../dex-helper';
 import { Interface } from '@ethersproject/abi';
 import { getDexKeysWithNetwork } from '../../../utils';
 import { SolidlyConfig } from '../config';
 import _ from 'lodash';
+import { Address, PoolLiquidity } from '../../../types';
+import BigNumber from 'bignumber.js';
 
 const ChronosFactoryABI = [
   {
@@ -18,6 +20,15 @@ const ChronosFactoryABI = [
 ];
 
 const chronosFactoryIface = new Interface(ChronosFactoryABI);
+
+type ChronosSubgraphPool = {
+  id: string;
+  isStable: boolean;
+  token0: { id: string; decimals: string };
+  reserve0: string;
+  reserve1: string;
+  token1: { id: string; decimals: string; };
+};
 
 export class Chronos extends Solidly {
   public static dexKeysWithNetwork: { key: string; networks: Network[] }[] =
@@ -52,5 +63,144 @@ export class Chronos extends Solidly {
       callEntry,
       callDecoder,
     };
+  }
+
+  async getTopPoolsForToken(
+    tokenAddress: Address,
+    count: number,
+  ): Promise<PoolLiquidity[]> {
+    if (!this.subgraphURL) return [];
+
+    const query = `query ($token: Bytes!, $count: Int) {
+      pools0: pairs(first: $count, orderBy: reserve0, orderDirection: desc, where: {token0: $token, reserve0_gt: 1, reserve1_gt: 1}) {
+        id
+        isStable
+        token0 {
+          id
+          decimals
+        }
+        token1 {
+          id
+          decimals
+        }
+        reserve0,
+        reserve1,
+      }
+      pools1: pairs(first: $count, orderBy: reserve1, orderDirection: desc, where: {token1: $token, reserve0_gt: 1, reserve1_gt: 1}) {
+        id
+        isStable
+        token0 {
+          id
+          decimals
+        }
+        token1 {
+          id
+          decimals
+        },
+        reserve0,
+        reserve1,
+      }
+    }`;
+
+    const { data } = await this.dexHelper.httpRequest.post(
+      this.subgraphURL,
+      {
+        query,
+        variables: { token: tokenAddress.toLowerCase(), count },
+      },
+      SUBGRAPH_TIMEOUT,
+    );
+
+    if (!(data && data.pools0 && data.pools1))
+      throw new Error("Couldn't fetch the pools from the subgraph");
+
+    const pools0 = await this.prepareSubgraphPools(data.pools0, (pool,{
+      address1, decimals1, liquidityUSDToken0, liquidityUSDToken1,
+    }) => ({
+      exchange: this.dexKey,
+      stable: pool.isStable,
+      address: pool.id.toLowerCase(),
+      connectorTokens: [
+        {
+          address: address1,
+          decimals: decimals1,
+        },
+      ],
+      liquidityUSD: parseFloat((liquidityUSDToken0 + liquidityUSDToken1).toFixed()),
+    }));
+
+    const pools1 = await this.prepareSubgraphPools(data.pools1, (pool,{
+      address0, decimals0, liquidityUSDToken0, liquidityUSDToken1,
+    }) => ({
+      exchange: this.dexKey,
+      stable: pool.isStable,
+      address: pool.id.toLowerCase(),
+      connectorTokens: [
+        {
+          address: address0,
+          decimals: decimals0,
+        },
+      ],
+      liquidityUSD: parseFloat((liquidityUSDToken0 + liquidityUSDToken1).toFixed()),
+    }));
+
+    return _.slice(
+      _.sortBy(_.concat(pools0, pools1), [pool => -1 * pool.liquidityUSD]),
+      0,
+      count,
+    );
+  }
+
+  private async prepareSubgraphPools(
+    pools: ChronosSubgraphPool[],
+    iterator: (
+      pool: ChronosSubgraphPool, {
+        address0,
+        address1,
+        decimals0,
+        decimals1,
+        reserve0,
+        reserve1,
+        liquidityUSDToken0,
+        liquidityUSDToken1,
+      }: {
+        address0: string,
+        address1: string,
+        decimals0: number,
+        decimals1: number,
+        reserve0: bigint,
+        reserve1: bigint,
+        liquidityUSDToken0: number,
+        liquidityUSDToken1: number,
+      }) => PoolLiquidity
+  ): Promise<PoolLiquidity[]> {
+    return Promise.all(pools.map(async (
+      pool: ChronosSubgraphPool,
+    ) => {
+      const address0 = pool.token0.id.toLowerCase();
+      const address1 = pool.token1.id.toLowerCase();
+
+      const decimals0 = parseInt(pool.token0.decimals);
+      const decimals1 = parseInt(pool.token1.decimals);
+
+      const reserve0 = BigInt(new BigNumber(pool.reserve0).multipliedBy(10 ** decimals0).toFixed());
+      const reserve1 = BigInt(new BigNumber(pool.reserve1).multipliedBy(10 ** decimals1).toFixed());
+
+      const liquidityUSDToken0 = await this.dexHelper.getTokenUSDPrice({
+        address: address0,
+        decimals: decimals0,
+      }, reserve0);
+
+      const liquidityUSDToken1 = await this.dexHelper.getTokenUSDPrice({
+        address: address1,
+        decimals: decimals1,
+      }, reserve1);
+
+      return iterator(
+        pool, {
+          address0, address1, decimals0, decimals1, reserve0, reserve1, liquidityUSDToken0, liquidityUSDToken1,
+        },
+      );
+    }));
   }
 }
