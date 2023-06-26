01b8d2f93ae881939e294f7f0124e3e347d07d03
Alexander Burkut <a.burkut90@gmail.com>
2023-06-26 13:37:29 +0300

    debug

--- src/dex/solidly/forks-override/ramses.ts
+++ src/dex/solidly/forks-override/ramses.ts
@@ -1,10 +1,10 @@
-import { Velodrome } from './velodrome';
 import { Network } from '../../../constants';
 import { getDexKeysWithNetwork } from '../../../utils';
 import { SolidlyConfig } from '../config';
 import _ from 'lodash';
+import { Chronos } from './chronos';
 
-export class Ramses extends Velodrome {
+export class Ramses extends Chronos {
   public static dexKeysWithNetwork: { key: string; networks: Network[] }[] =
     getDexKeysWithNetwork(_.pick(SolidlyConfig, ['Ramses']));
 }
