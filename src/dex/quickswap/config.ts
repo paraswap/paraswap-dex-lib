99aff0ec4e3004f21cab181b32c6c39ceb754d95
Tudor Anastasiu <tg.anastasiu@gmail.com>
2023-06-27 17:52:39 +0300

    Add config for QuickSwapV3 on Polygon zkEVM.

--- src/dex/quickswap/config.ts
+++ src/dex/quickswap/config.ts
@@ -38,6 +38,13 @@ export const QuickSwapConfig: DexConfigMap<DexParams> = {
       initCode:
         '0x6ec6c9c8091d160c0aa74b2b14ba9c1717e95093bd3ac085cee99a49aab294a4',
     },
+    [Network.ZKEVM]: {
+      factoryAddress: '0x4B9f4d2435Ef65559567e5DbFC1BbB37abC43B57',
+      router: '0xF6Ad3CcF71Abb3E12beCf6b3D2a74C963859ADCd',
+      quoteAddress: '0x55BeE1bD3Eb9986f6d2d963278de09eE92a3eF1D',
+      initCode:
+        '0x6ec6c9c8091d160c0aa74b2b14ba9c1717e95093bd3ac085cee99a49aab294a4',
+    },
   },
   SpiritSwapV3: {
     [Network.FANTOM]: {
