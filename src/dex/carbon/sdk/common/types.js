3a0004b486f767181171feb4ba1bc6786863989c
Tiago Silva <tfn.da.silva@gmail.com>
2023-07-10 09:54:01 +0100

    Remove sdk files not needed

--- src/dex/carbon/sdk/common/types.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.MatchType = void 0;
-var MatchType;
-(function (MatchType) {
-  MatchType['Fast'] = 'Fast';
-  MatchType['Best'] = 'Best';
-})((MatchType = exports.MatchType || (exports.MatchType = {})));
