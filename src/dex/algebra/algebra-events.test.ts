576557c24557b5d78824740cd26e680eddd53dea
Mohamed Hassen Mami <mohamed@paraswap.io>
2023-07-11 16:54:41 +0200

    add event tests

--- src/dex/algebra/algebra-events.test.ts
+++ src/dex/algebra/algebra-events.test.ts
@@ -2,105 +2,119 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { AlgebraEventPool } from './algebra-pool';
+import _ from 'lodash';
+import { AlgebraConfig } from './config';
 import { Network } from '../../constants';
-import { Address } from '../../types';
 import { DummyDexHelper } from '../../dex-helper/index';
 import { testEventSubscriber } from '../../../tests/utils-events';
 import { PoolState } from './types';
+import { Interface } from '@ethersproject/abi';
+import ERC20ABI from '../../abi/erc20.json';
+import StateMulticallABI from '../../abi/algebra/AlgebraStateMulticall.abi.json';
+import { AbiItem } from 'web3-utils';
+import { AlgebraEventPool } from './algebra-pool';
 
-/*
-  README
-  ======
+jest.setTimeout(300 * 1000);
+const dexKey = 'QuickSwapV3';
+const network = Network.POLYGON;
+const config = AlgebraConfig[dexKey][network];
 
-  This test script adds unit tests for Algebra event based
-  system. This is done by fetching the state on-chain before the
-  event block, manually pushing the block logs to the event-subscriber,
-  comparing the local state with on-chain state.
-
-  Most of the logic for testing is abstracted by `testEventSubscriber`.
-  You need to do two things to make the tests work:
-
-  1. Fetch the block numbers where certain events were released. You
-  can modify the `./scripts/fetch-event-blocknumber.ts` to get the
-  block numbers for different events. Make sure to get sufficient
-  number of blockNumbers to cover all possible cases for the event
-  mutations.
-
-  2. Complete the implementation for fetchPoolState function. The
-  function should fetch the on-chain state of the event subscriber
-  using just the blocknumber.
-
-  The template tests only include the test for a single event
-  subscriber. There can be cases where multiple event subscribers
-  exist for a single DEX. In such cases additional tests should be
-  added.
-
-  You can run this individual test script by running:
-  `npx jest src/dex/<dex-name>/<dex-name>-events.test.ts`
-
-  (This comment should be removed from the final implementation)
-*/
-
-jest.setTimeout(50 * 1000);
-
-async function fetchPoolState(
-  algebraPools: AlgebraEventPool,
+async function fetchPoolStateFromContract(
+  algebraPool: AlgebraEventPool,
   blockNumber: number,
   poolAddress: string,
 ): Promise<PoolState> {
-  // TODO: complete me!
-  return {};
+  const message = `Algebra: ${poolAddress} blockNumber ${blockNumber}`;
+  console.log(`Fetching state ${message}`);
+  // Be careful to not request state prior to contract deployment
+  // Otherwise need to use manual state sourcing from multicall
+  // We had that mechanism, but removed it with this commit
+  // You can restore it, but better just to find block after state multicall
+  // deployment
+  const state = algebraPool.generateState(blockNumber);
+  console.log(`Done ${message}`);
+  return state;
 }
 
-// eventName -> blockNumbers
-type EventMappings = Record<string, number[]>;
+describe('Algebra Event', function () {
+  const poolAddress = '0x5b41eedcfc8e0ae47493d4945aa1ae4fe05430ff';
+  const token0 = '0xc2132d05d31c914a87c6611c10748aeb04b58e8f';
+  const token1 = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270';
 
-describe('Algebra EventPool Mainnet', function () {
-  const dexKey = 'Algebra';
-  const network = Network.MAINNET;
-  const dexHelper = new DummyDexHelper(network);
-  const logger = dexHelper.getLogger(dexKey);
-  let algebraPool: AlgebraEventPool;
-
-  // poolAddress -> EventMappings
-  const eventsToTest: Record<Address, EventMappings> = {
-    // TODO: complete me!
+  const blockNumbers: { [eventName: string]: number[] } = {
+    // topic0 - 0x598b9f043c813aa6be3426ca60d1c65d17256312890be5118dab55b0775ebe2a
+    ['Fee']: [
+      44940320, 44940321, 44940347, 44940402, 44940419, 44940438, 44940443,
+      44940447, 44940451, 44940482, 44940505, 44940509, 44940512, 44940530,
+      44940557, 44940560, 44940577, 44940609, 44940647, 44940648, 44940654,
+      44940659, 44940664, 44940673, 44940675, 44940711, 44940715, 44940717,
+      44940719, 44940768, 44940769, 44940783, 44940791,
+    ],
+    // topic0 - 0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67
+    ['Swap']: [
+      44940320, 44940320, 44940321, 44940347, 44940402, 44940419, 44940438,
+      44940443, 44940447, 44940451, 44940482, 44940505, 44940509, 44940512,
+      44940530, 44940557, 44940560, 44940577, 44940609, 44940647, 44940648,
+      44940654, 44940659, 44940664, 44940673, 44940675, 44940711, 44940715,
+      44940719, 44940768, 44940769, 44940783, 44940791,
+    ],
+    // topic0 - 0x7a53080ba414158be7ec69b987b5fb7d07dee101fe85488f0853ae16239d0bde
+    ['Mint']: [44940717],
+    // topic0 - 0x0c396cd989a39f4459b5fa1aed6a9a8dcdbc45908acfd67e028cd568da98982c
+    ['Burn']: [44940695],
+    // topic0 - 0xbdbdb71d7860376ba52b25a5028beea23581364a40522f6bcfb86bb1f2dca633
+    ['Flash']: [],
+    // topic0 - 0x70935338e69775456a85ddef226c395fb668b63fa0115f5f20610b388e6ca9c0
+    ['Collect']: [],
+    // topic0 - 0x9e22b964b08e25c3aaa72102bb0071c089258fb82d51271a8ddf5c24921356ee
+    ['CommunityFee']: [],
   };
 
-  beforeEach(async () => {
-    algebraPool = new AlgebraEventPool(
-      dexKey,
-      network,
-      dexHelper,
-      logger,
-      /* TODO: Put here additional constructor arguments if needed */
-    );
-  });
+  describe('AlgebraEventPool', function () {
+    Object.keys(blockNumbers).forEach((event: string) => {
+      blockNumbers[event].forEach((blockNumber: number) => {
+        it(`${event}:${blockNumber} - should return correct state`, async function () {
+          const dexHelper = new DummyDexHelper(network);
+          // await dexHelper.init();
 
-  Object.entries(eventsToTest).forEach(
-    ([poolAddress, events]: [string, EventMappings]) => {
-      describe(`Events for ${poolAddress}`, () => {
-        Object.entries(events).forEach(
-          ([eventName, blockNumbers]: [string, number[]]) => {
-            describe(`${eventName}`, () => {
-              blockNumbers.forEach((blockNumber: number) => {
-                it(`State after ${blockNumber}`, async function () {
-                  await testEventSubscriber(
-                    algebraPool,
-                    algebraPool.addressesSubscribed,
-                    (_blockNumber: number) =>
-                      fetchPoolState(algebraPool, _blockNumber, poolAddress),
-                    blockNumber,
-                    `${dexKey}_${poolAddress}`,
-                    dexHelper.provider,
-                  );
-                });
-              });
-            });
-          },
-        );
+          const logger = dexHelper.getLogger(dexKey);
+
+          const algebraPool = new AlgebraEventPool(
+            dexHelper,
+            dexKey,
+            new dexHelper.web3Provider.eth.Contract(
+              StateMulticallABI as AbiItem[],
+              config.algebraStateMulticall,
+            ),
+            new Interface(ERC20ABI),
+            config.factory,
+            token0,
+            token1,
+            logger,
+            undefined,
+            config.initHash,
+            config.deployer,
+          );
+
+          // It is done in generateState. But here have to make it manually
+          algebraPool.poolAddress = poolAddress.toLowerCase();
+          algebraPool.addressesSubscribed[0] = poolAddress;
+
+          await testEventSubscriber(
+            algebraPool,
+            algebraPool.addressesSubscribed,
+            (_blockNumber: number) =>
+              fetchPoolStateFromContract(
+                algebraPool,
+                _blockNumber,
+                poolAddress,
+              ),
+            blockNumber,
+            `${dexKey}_${poolAddress}`,
+            dexHelper.provider,
+          );
+        });
       });
-    },
-  );
+    });
+  });
 });
