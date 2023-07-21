7fc495b290fb3ae797b33702eee89a1c8f7e93be
sameepsi <sameepsi@gmail.com>
2023-07-21 14:39:04 +0530

    Config for QuickPerps on Polygon zkevm

--- src/dex/gmx/config.ts
+++ src/dex/gmx/config.ts
@@ -21,6 +21,16 @@ export const GMXConfig: DexConfigMap<DexParams> = {
       usdg: '0x45096e7aA921f27590f8F19e457794EB09678141',
     },
   },
+  QuickPerps: {
+    [Network.ZKEVM]: {
+      vault: '0x99B31498B0a1Dae01fc3433e3Cb60F095340935C',
+      reader: '0xf1CFB75854DE535475B88Bb6FBad317eea98c0F9',
+      priceFeed: '0x5b1F500134bdD7f4359F5B2adC65f839737290f4',
+      fastPriceFeed: '0x73903fEc691a80Ec47bc830bf3F0baD127A06e30',
+      fastPriceEvents: '0x08bC8ef0b71238055f9Ee6BBc90869D8d0DBdCCa',
+      usdg: '0x48aC594dd00c4aAcF40f83337fc6dA31F9F439A7',
+    },
+  },
 };
 
 export const Adapters: {
@@ -41,6 +51,14 @@ export const Adapters: {
       {
         name: 'ArbitrumAdapter01',
         index: 9,
+      },
+    ],
+  },
+  [Network.ZKEVM]: {
+    [SwapSide.SELL]: [
+      {
+        name: 'PolygonZkEvmAdapter01',
+        index: 2,
       },
     ],
   },
