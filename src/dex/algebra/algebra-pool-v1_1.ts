f5c31fa0d14e9234af32f0fc5fc49f935486e4a7
Verisana <account@0112.pro>
2023-08-25 16:24:00 +0100

    fix: fix pool address mismatch

--- src/dex/algebra/algebra-pool-v1_1.ts
+++ src/dex/algebra/algebra-pool-v1_1.ts
@@ -379,7 +379,7 @@ export class AlgebraEventPoolV1_1 extends StatefulEventSubscriber<PoolStateV1_1>
       },
     ]);
     assert(
-      poolAddress === this.poolAddress,
+      poolAddress.toLowerCase() === this.poolAddress.toLowerCase(),
       `Pool address mismatch: ${poolAddress.toLowerCase()} != ${this.poolAddress.toLowerCase()}`,
     );
 
