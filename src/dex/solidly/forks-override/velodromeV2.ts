ac9751c979502cb95bde7471d543bcb5fa5c1aaa
Alexander Burkut <a.burkut90@gmail.com>
2023-08-30 11:17:53 +0300

    add tests

--- src/dex/solidly/forks-override/velodromeV2.ts
+++ src/dex/solidly/forks-override/velodromeV2.ts
@@ -4,7 +4,7 @@ import { getDexKeysWithNetwork } from '../../../utils';
 import { SolidlyConfig } from '../config';
 import _ from 'lodash';
 
-export class Velodrome extends Solidly {
+export class VelodromeV2 extends Solidly {
   public static dexKeysWithNetwork: { key: string; networks: Network[] }[] =
     getDexKeysWithNetwork(_.pick(SolidlyConfig, ['VelodromeV2']));
 }
