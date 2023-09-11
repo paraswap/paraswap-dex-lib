21be0cfd3e3c23ce37f8ca582b1d85e0ee3a5c81
A.L <xenide@users.noreply.github.com>
2023-09-11 15:46:42 -0500

    feat: generateState

--- src/dex/reservoir/reservoir-pool.ts
+++ src/dex/reservoir/reservoir-pool.ts
@@ -1,11 +1,11 @@
-import { Interface } from '@ethersproject/abi';
+import { AbiCoder, Interface } from '@ethersproject/abi';
 import { DeepReadonly } from 'ts-essentials';
 import { Log, Logger, Token } from '../../types';
-import { catchParseLogError } from '../../utils';
 import { StatefulEventSubscriber } from '../../stateful-event-subscriber';
 import { IDexHelper } from '../../dex-helper/idex-helper';
 import { ReservoirPoolState, ReservoirPoolTypes } from './types';
 import ReservoirPairABI from '../../abi/reservoir/ReservoirPair.json';
+import StablePairABI from '../../abi/reservoir/StablePair.json';
 import { Address } from '@paraswap/core';
 
 const LogCallTopics = [
@@ -23,6 +23,7 @@ export class ReservoirEventPool extends StatefulEventSubscriber<ReservoirPoolSta
   } = {};
 
   decoder = (log: Log) => this.reservoirIface.parseLog(log);
+  coder = new AbiCoder();
 
   constructor(
     readonly parentName: string,
@@ -33,6 +34,7 @@ export class ReservoirEventPool extends StatefulEventSubscriber<ReservoirPoolSta
     private curveId: ReservoirPoolTypes,
     logger: Logger,
     protected reservoirIface = new Interface(ReservoirPairABI),
+    protected stablePairIface = new Interface(StablePairABI),
   ) {
     const poolName = token0.address + '-' + token1.address + '-' + curveId;
     super(parentName, poolName, dexHelper, logger);
@@ -63,7 +65,6 @@ export class ReservoirEventPool extends StatefulEventSubscriber<ReservoirPoolSta
           swapFee: state.swapFee,
           ampCoefficient: state.ampCoefficient,
         };
-
       // TODO: also handle SwapFee(fees change) and (RampA) ampCoefficient changes
     }
 
@@ -80,16 +81,52 @@ export class ReservoirEventPool extends StatefulEventSubscriber<ReservoirPoolSta
    * @returns state of the event subscriber at blocknumber
    */
   async generateState(
-    blockNumber: number,
+    blockNumber: number | 'latest' = 'latest',
   ): Promise<DeepReadonly<ReservoirPoolState>> {
-    // TODO: complete me!
+    let calldata = [
+      {
+        target: this.poolAddress,
+        callData: this.reservoirIface.encodeFunctionData('getReserves', []),
+      },
+    ];
+
+    // get swap fee
+    calldata.push({
+      target: this.poolAddress,
+      callData: this.reservoirIface.encodeFunctionData('swapFee', []),
+    });
+
+    // get amp coefficient(if applicable)
+    if (this.curveId == ReservoirPoolTypes.Stable) {
+      calldata.push({
+        target: this.poolAddress,
+        callData: this.stablePairIface.encodeFunctionData('ampData', []),
+      });
+    }
+
+    const data: { returnData: any[] } =
+      await this.dexHelper.multiContract.methods
+        .aggregate(calldata)
+        .call({}, blockNumber);
+
+    const reserves = this.coder.decode(
+      ['uint104', 'uint104', 'uint32', 'uint16'],
+      data.returnData[0],
+    );
+    const swapFee = this.coder.decode(['uint256'], data.returnData[1]);
+    // TODO: to actually calculate the effective A at the moment
+    // given the current timestamp (how do we get this?)
+    const ampCoefficient = this.coder.decode(
+      ['uint64', 'uint64', 'uint64', 'uint64'],
+      data.returnData[2],
+    );
 
     return {
-      reserve0: '0',
-      reserve1: '0',
-      curveId: 0,
-      swapFee: 0n,
-      ampCoefficient: 0n,
+      reserve0: reserves[0].toString(),
+      reserve1: reserves[1].toString(),
+      curveId: this.curveId,
+      swapFee: BigInt(swapFee.toString()),
+      ampCoefficient: BigInt(ampCoefficient[1]),
     };
   }
 }
