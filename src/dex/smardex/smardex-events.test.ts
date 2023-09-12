caa724f1124c48da02d03d076ae55bc5a8d881d9
Esteban <gonzalesteb@gmail.com>
2023-09-12 16:05:31 +0200

    tests(events): add bsc and arbitrum

--- src/dex/smardex/smardex-events.test.ts
+++ src/dex/smardex/smardex-events.test.ts
@@ -55,9 +55,35 @@ const NETWORK_CONFIGS: NetworkConfig[] = [
             18064060,
             18064194,
             18065266,
-            18066464, // the last one contains multiple Snyc events
+            18066464, // the last one contains multiple Sync events
           ],
           ['Transfer']: [18064025, 18064045, 18065266],
+        },
+      },
+
+      {
+        address: '0xf3a4B8eFe3e3049F6BC71B47ccB7Ce6665420179',
+        symbol0: 'SDEX',
+        symbol1: 'ETH',
+        events: {
+          ['Swap']: [
+            18120396,
+            18120367, // multiple Swap events
+            18119305,
+            18118991,
+            18120209,
+          ],
+          ['Burn']: [18114357, 18089610, 18026115],
+          ['Mint']: [18115825, 18109975, 18093408],
+          // ['FeesChanged']: [], // none on L1
+          ['Sync']: [
+            18120396,
+            18120367, // multiple Sync events
+            18119305,
+            18118991,
+            18120209,
+          ],
+          ['Transfer']: [18108643, 18105778, 18105793],
         },
       },
     ],
@@ -77,6 +103,86 @@ const NETWORK_CONFIGS: NetworkConfig[] = [
           ['Mint']: [47417315, 47414686, 47288517],
           ['FeesChanged']: [45861051],
           ['Sync']: [47436997, 47437089, 47437096, 47445056, 47445066],
+          ['Transfer']: [47423778, 47403369, 47343062],
+        },
+      },
+      {
+        address: '0x7130d1Ab6d9C657240331A4DE3e88b5497Be9cEB',
+        symbol0: 'WMATIC',
+        symbol1: 'USDC',
+        events: {
+          ['Swap']: [47448993, 47449888, 47449861, 47448993, 47449214],
+          ['Burn']: [47434291, 47433030, 47426957],
+          ['Mint']: [47354112, 47268128, 47252146],
+          ['FeesChanged']: [], // Fees didn't change on this pool
+          ['Sync']: [47448993, 47449888, 47449861, 47448993, 47449214],
+          ['Transfer']: [47338250, 47338178, 47333469],
+        },
+      },
+    ],
+  },
+  {
+    name: 'arbitrum',
+    network: Network.ARBITRUM,
+    tokens: Tokens[Network.ARBITRUM],
+    pools: [
+      {
+        address: '0xacCDd9EE1DCd393E346e5d6350230439b4DA09ab',
+        symbol0: 'SDEX',
+        symbol1: 'USDC',
+        events: {
+          ['Swap']: [130424308, 130422563, 130418994, 130415253, 130382808],
+          ['Burn']: [129969327, 129138853, 126200497],
+          ['Mint']: [130028302, 129940404, 129728771],
+          ['FeesChanged']: [], // Fees didn't change on this pool
+          ['Sync']: [130424308, 130422563, 130418994, 130415253, 130382808],
+          ['Transfer']: [130028373, 129918802, 129668897],
+        },
+      },
+      {
+        address: '0xD87899d10Eaa10F3adE05038A38251F758E5C0eb',
+        symbol0: 'ARB',
+        symbol1: 'USDC',
+        events: {
+          ['Swap']: [130429535, 130419879, 130417306, 130414469, 130416990],
+          ['Burn']: [130200966, 127933595, 125698828],
+          ['Mint']: [130263032, 130331095, 128594710],
+          ['FeesChanged']: [], // Fees didn't change on this pool
+          ['Sync']: [130429535, 130419879, 130417306, 130414469, 130416990],
+          ['Transfer']: [130423567, 130423514, 130331095],
+        },
+      },
+    ],
+  },
+  {
+    name: 'bsc',
+    network: Network.BSC,
+    tokens: Tokens[Network.BSC],
+    pools: [
+      {
+        address: '0xe7b89CbD4E833510F393CCfbE7D433EDbb137aB2',
+        symbol0: 'USDT',
+        symbol1: 'SDEX',
+        events: {
+          ['Swap']: [31678152, 31678000, 31674992, 31670581, 31675013],
+          ['Burn']: [31660547, 31655719, 31539131],
+          ['Mint']: [31652485, 31595468, 31571640],
+          ['FeesChanged']: [30531399],
+          ['Sync']: [31678152, 31678000, 31674992, 31670581, 31675013],
+          ['Transfer']: [31660547, 31655777, 31655736],
+        },
+      },
+      {
+        address: '0xf315833053a1187fd5e9813E38BD59937492857a',
+        symbol0: 'USDT',
+        symbol1: 'BNB',
+        events: {
+          ['Swap']: [31678534, 31675409, 31678534, 31675014, 31674391],
+          ['Burn']: [31666734, 31586726, 31462652],
+          ['Mint']: [31662048, 31554796, 31524123],
+          ['FeesChanged']: [], // Fees didn't change on this pool
+          ['Sync']: [31678534, 31675409, 31678534, 31675014, 31674391],
+          ['Transfer']: [31666734, 31632200, 31586970],
         },
       },
     ],
