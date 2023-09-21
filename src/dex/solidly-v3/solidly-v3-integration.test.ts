d110722b4dc70de7b7c8d14fea05f4a581de779e
Autistic Dev <temp@gmail.com>
2023-09-21 03:51:20 -0400

    don't hardcode tokens

--- src/dex/solidly-v3/solidly-v3-integration.test.ts
+++ src/dex/solidly-v3/solidly-v3-integration.test.ts
@@ -278,9 +278,8 @@ describe('SolidlyV3', function () {
             blockNumber,
             solidlyV3.eventPools[price.poolIdentifier!]!.poolAddress,
             price.prices,
-            // TODO: don't hardcode tokens
-            '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-            '0xdac17f958d2ee523a2206206994597c13d831ec7',
+            WETH.address,
+            USDT.address,
             tickSpacing,
             amounts,
           );
