9e6c742bf46e9a8a4c8e753acfab243d4fba2c97
Daniel Pham <daniel.pham765@gmail.com>
2023-11-10 18:43:52 +0700

    test(ks-elastic): Fix BUY stable pairs

--- src/dex/kyberswap-elastic/kyberswap-elastic-integration.test.ts
+++ src/dex/kyberswap-elastic/kyberswap-elastic-integration.test.ts
@@ -484,8 +484,8 @@ describe('KyberswapElastic', function () {
             KyberElasticFunctions.quoteExactOutputSingle,
             blockNumber,
             price.prices,
-            TokenB.address,
             TokenA.address,
+            TokenB.address,
             swapFeeUnits,
             amountsBuy,
           );
