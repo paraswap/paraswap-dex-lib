9a0290cf7a4fbb7485a5b7cf747f0f10ce749b1d
chicken-juju <chickenjuju@proton.me>
2023-11-15 16:46:24 +0400

    feat: Integrate Voodoo Trade

--- src/dex/gmx/gmx-integration.test.ts
+++ src/dex/gmx/gmx-integration.test.ts
@@ -209,3 +209,101 @@ describe('Morphex BSC', function () {
     }
   });
 });
+
+describe('Voodoo BASE', function () {
+  const dexKey = 'Voodoo';
+  const network = Network.BASE;
+  const TokenASymbol = 'USDC';
+  const TokenA = Tokens[network][TokenASymbol];
+
+  const TokenBSymbol = 'WETH';
+  const TokenB = Tokens[network][TokenBSymbol];
+
+  const amounts = [
+    0n,
+    1000000000000000000n,
+    2000000000000000000n,
+    3000000000000000000n,
+    4000000000000000000n,
+    5000000000000000000n,
+  ];
+
+  const readerInterface = new Interface(ReaderABI);
+  const params = GMXConfig[dexKey][network];
+  const readerAddress = params.reader;
+
+  it('getPoolIdentifiers and getPricesVolume SELL', async function () {
+    const dexHelper = new DummyDexHelper(network);
+    const blocknumber = await dexHelper.web3Provider.eth.getBlockNumber();
+    const gmx = new GMX(network, dexKey, dexHelper);
+
+    await gmx.initializePricing(blocknumber);
+
+    const pools = await gmx.getPoolIdentifiers(
+      TokenA,
+      TokenB,
+      SwapSide.SELL,
+      blocknumber,
+    );
+    console.log(`${TokenASymbol} <> ${TokenBSymbol} Pool Identifiers: `, pools);
+
+    expect(pools.length).toBeGreaterThan(0);
+
+    const poolPrices = await gmx.getPricesVolume(
+      TokenA,
+      TokenB,
+      amounts,
+      SwapSide.SELL,
+      blocknumber,
+      pools,
+    );
+    console.log(`${TokenASymbol} <> ${TokenBSymbol} Pool Prices: `, poolPrices);
+
+    expect(poolPrices).not.toBeNull();
+    if (gmx.hasConstantPriceLargeAmounts) {
+      checkConstantPoolPrices(poolPrices!, amounts, dexKey);
+    } else {
+      checkPoolPrices(poolPrices!, amounts, SwapSide.SELL, dexKey);
+    }
+
+    // Do on chain pricing based on reader to compare
+    const readerCallData = amounts.map(a => ({
+      target: readerAddress,
+      callData: readerInterface.encodeFunctionData('getAmountOut', [
+        params.vault,
+        TokenA.address,
+        TokenB.address,
+        a.toString(),
+      ]),
+    }));
+
+    const readerResult = (
+      await dexHelper.multiContract.methods
+        .aggregate(readerCallData)
+        .call({}, blocknumber)
+    ).returnData;
+    const expectedPrices = readerResult.map((p: any) =>
+      BigInt(
+        readerInterface.decodeFunctionResult('getAmountOut', p)[0].toString(),
+      ),
+    );
+
+    expect(poolPrices![0].prices).toEqual(expectedPrices);
+  });
+
+  it('getTopPoolsForToken', async function () {
+    const dexHelper = new DummyDexHelper(network);
+    const gmx = new GMX(network, dexKey, dexHelper);
+
+    await gmx.updatePoolState();
+    const poolLiquidity = await gmx.getTopPoolsForToken(TokenA.address, 10);
+    console.log(
+      `${TokenASymbol} Top Pools:`,
+      JSON.stringify(poolLiquidity, null, 2),
+    );
+
+    if (!gmx.hasConstantPriceLargeAmounts) {
+      checkPoolsLiquidity(poolLiquidity, TokenA.address, dexKey);
+    }
+  });
+});
