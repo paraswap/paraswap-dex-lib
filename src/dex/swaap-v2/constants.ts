52a18ffa9a092032be64774919e698130db47120
David Bouba <d.m.bouba@gmail.com>
2023-11-16 17:43:42 +0100

    feature: updated swaap-v2 gas estimate

--- src/dex/swaap-v2/constants.ts
+++ src/dex/swaap-v2/constants.ts
@@ -30,7 +30,9 @@ export const SWAAP_429_TTL_S = 60 * 60 * 1; // 1 hour
 
 export const SWAAP_POOL_RESTRICT_TTL_S = 60 * 30; // 30 minutes
 
-export const GAS_COST_ESTIMATION = 170_000;
+export const STABLE_SWAP_GAS_COST_ESTIMATION = 105_000;
+
+export const VOLATILE_SWAP_GAS_COST_ESTIMATION = 135_000;
 
 export const BATCH_SWAP_SELECTOR = '0x945bcec9';
 
--- src/dex/swaap-v2/utils.ts
+++ src/dex/swaap-v2/utils.ts
@@ -1,5 +1,7 @@
-import { Address } from '../../types';
+import { Address, Token } from '../../types';
 import { ETHER_ADDRESS, NULL_ADDRESS } from '../../constants';
+import { Network } from '../../../src/constants';
+import { STABLE_COINS } from './stable-coins';
 
 export const getIdentifierPrefix = (
   dexKey: string,
@@ -26,4 +28,19 @@ export const normalizeTokenAddress = (address: string): string => {
   return address.toLowerCase() === ETHER_ADDRESS.toLowerCase()
     ? NULL_ADDRESS
     : address.toLowerCase();
+};
+
+export const isStablePair = (
+  network: Network,
+  normalizedSrcTokenAddress: string,
+  normalizedDestTokenAddress: string,
+): boolean => {
+  const networkStableCoins = STABLE_COINS[network];
+  if (networkStableCoins == undefined) {
+    return false;
+  }
+  return (
+    networkStableCoins[normalizedSrcTokenAddress] === true &&
+    networkStableCoins[normalizedDestTokenAddress] === true
+  );
 };
