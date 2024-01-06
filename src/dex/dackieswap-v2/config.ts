d9adbe27f4e88a21f3d788389e6851e86676e608
andreapn.eth <fifi.hate.lonely@gmail.com>
2024-01-06 15:10:15 +0700

    Init DackieSwap V2 & V3

--- src/dex/dackieswap-v2/config.ts
+++ src/dex/dackieswap-v2/config.ts
@@ -4,12 +4,22 @@ import { Network, SwapSide } from '../../constants';
 
 export const DackieswapV2Config: DexConfigMap<DexParams> = {
   DackieswapV2: {
-    // TODO: complete me!
+    [Network.BASE]: {
+      // There is no subgraph for Aerodrome
+      factoryAddress: '0x591f122D1df761E616c13d265006fcbf4c6d6551',
+      router: '0xCa4EAa32E7081b0c4Ba47e2bDF9B7163907Fe56f',
+      initCode:
+        '0xaaaacde43ad77b69fcbcdc68ccb757c3c634ad20e330a951b4a267f1180c6520',
+      poolGasCost: 180 * 1000,
+      feeCode: 0,
+      subgraphURL:
+        'https://api.studio.thegraph.com/query/50473/subgraphs-exchange-v2/version/latest',
+    },
   },
 };
 
 export const Adapters: Record<number, AdapterMappings> = {
-  // TODO: add adapters for each chain
-  // This is an example to copy
-  [Network.MAINNET]: { [SwapSide.SELL]: [{ name: '', index: 0 }] },
+  [Network.BASE]: {
+    [SwapSide.SELL]: [{ name: 'BaseAdapter01', index: 1 }], // dackieswap-v2
+  },
 };
