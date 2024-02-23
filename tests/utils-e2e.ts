a49abc66d31a120380b44b20e75e11ad9347d8b7
Danylo Kanievskyi <danylokaniev@gmail.com>
2024-02-23 17:09:15 +0200

    chore: return price route log

--- tests/utils-e2e.ts
+++ tests/utils-e2e.ts
@@ -494,6 +494,7 @@ export async function testE2E(
       forceRoute,
     );
 
+    console.log('PRICE ROUTE: ', util.inspect(priceRoute, false, null, true));
     expect(parseFloat(priceRoute.destAmount)).toBeGreaterThan(0);
 
     const config = generateConfig(network);
