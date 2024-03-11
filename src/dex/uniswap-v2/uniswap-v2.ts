8bca2f30edca794e05ff1d69fa5d5ab613a348ab
Yevhen Shulha <jackshel45@gmail.com>
2024-03-11 17:22:19 +0200

    fix: bakeryswap encoding

--- src/dex/uniswap-v2/uniswap-v2.ts
+++ src/dex/uniswap-v2/uniswap-v2.ts
@@ -864,7 +864,17 @@ export class UniswapV2
     let targetExchange: Address;
     let dexFuncHasRecipient: boolean;
 
-    if (side === SwapSide.SELL) {
+    if (this.dexKey === 'BakerySwap') {
+      const weth = this.getWETHAddress(srcToken, destToken, data.weth);
+
+      exchangeData = this.exchangeRouterInterface.encodeFunctionData(
+        UniswapV2Functions.swap,
+        [srcToken, srcAmount, destAmount, weth, pools],
+      );
+      specialDexFlag = SpecialDex.DEFAULT;
+      targetExchange = data.router;
+      dexFuncHasRecipient = false;
+    } else if (side === SwapSide.SELL) {
       // 28 bytes are prepended in the Bytecode builder
       const exchangeDataTypes = ['bytes4', 'bytes32', 'bytes32'];
       const exchangeDataToPack = [
