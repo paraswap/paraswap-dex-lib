cedef10c05c186d079937d412a9b651e3d5f065f
Yevhen Shulha <jackshel45@gmail.com>
2024-04-26 15:29:19 +0300

    fix: update router & cleaning

--- src/dex/solidly/config.ts
+++ src/dex/solidly/config.ts
@@ -164,13 +164,11 @@ export const SolidlyConfig: DexConfigMap<DexParams> = {
       subgraphURL:
         'https://api.studio.thegraph.com/query/66247/pharaoh-legacy/version/latest',
       factoryAddress: '0xAAA16c016BF556fcD620328f0759252E29b1AB57',
-      router: '0xAAA45c8F5ef92a000a121d102F4e89278a711Faa',
+      router: '0x609AcD8Fc955Dd7E744c7DFFc9930a7A6654DE43',
       initCode:
         '0xbf2404274de2b11f05e5aebd49e508de933034cb5fa2d0ac3de8cbd4bcef47dc',
       poolGasCost: 180 * 1000,
       feeCode: 0,
-      // stableFee: 5, // TODO: ?
-      // volatileFee: 25 // TODO: ?,
     },
   },
   Equalizer: {
