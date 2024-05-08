5f9b9bd5a572383c6a217184cc85272b2d1eeede
Yevhen Shulha <jackshel45@gmail.com>
2024-05-09 00:45:29 +0300

    fix: lowecase addresses

--- src/dex/angle-staked-stable/config.ts
+++ src/dex/angle-staked-stable/config.ts
@@ -6,47 +6,47 @@ export const AngleStakedStableConfig: DexConfigMap<DexParams> = {
   // USDA <-> stUSD
   AngleStakedStableUSD: {
     [Network.ARBITRUM]: {
-      agToken: '0x0000206329b97DB379d5E1Bf586BbDB969C63274',
-      stakeToken: '0x0022228a2cc5E7eF0274A7Baa600d44da5aB5776',
+      agToken: '0x0000206329b97db379d5e1bf586bbdb969c63274',
+      stakeToken: '0x0022228a2cc5e7ef0274a7baa600d44da5ab5776',
     },
     [Network.MAINNET]: {
-      agToken: '0x0000206329b97DB379d5E1Bf586BbDB969C63274',
-      stakeToken: '0x0022228a2cc5E7eF0274A7Baa600d44da5aB5776',
+      agToken: '0x0000206329b97db379d5e1bf586bbdb969c63274',
+      stakeToken: '0x0022228a2cc5e7ef0274a7baa600d44da5ab5776',
     },
     [Network.OPTIMISM]: {
-      agToken: '0x0000206329b97DB379d5E1Bf586BbDB969C63274',
-      stakeToken: '0x0022228a2cc5E7eF0274A7Baa600d44da5aB5776',
+      agToken: '0x0000206329b97db379d5e1bf586bbdb969c63274',
+      stakeToken: '0x0022228a2cc5e7ef0274a7baa600d44da5ab5776',
     },
     [Network.POLYGON]: {
-      agToken: '0x0000206329b97DB379d5E1Bf586BbDB969C63274',
-      stakeToken: '0x0022228a2cc5E7eF0274A7Baa600d44da5aB5776',
+      agToken: '0x0000206329b97db379d5e1bf586bbdb969c63274',
+      stakeToken: '0x0022228a2cc5e7ef0274a7baa600d44da5ab5776',
     },
     [Network.BASE]: {
-      agToken: '0x0000206329b97DB379d5E1Bf586BbDB969C63274',
-      stakeToken: '0x0022228a2cc5E7eF0274A7Baa600d44da5aB5776',
+      agToken: '0x0000206329b97db379d5e1bf586bbdb969c63274',
+      stakeToken: '0x0022228a2cc5e7ef0274a7baa600d44da5ab5776',
     },
     [Network.BSC]: {
-      agToken: '0x0000206329b97DB379d5E1Bf586BbDB969C63274',
-      stakeToken: '0x0022228a2cc5E7eF0274A7Baa600d44da5aB5776',
+      agToken: '0x0000206329b97db379d5e1bf586bbdb969c63274',
+      stakeToken: '0x0022228a2cc5e7ef0274a7baa600d44da5ab5776',
     },
   },
   // EURA <-> stEUR
   AngleStakedStableEUR: {
     [Network.ARBITRUM]: {
       agToken: '0xfa5ed56a203466cbbc2430a43c66b9d8723528e7',
-      stakeToken: '0x004626A008B1aCdC4c74ab51644093b155e59A23',
+      stakeToken: '0x004626a008b1acdc4c74ab51644093b155e59a23',
     },
     [Network.MAINNET]: {
-      agToken: '0x1a7e4e63778B4f12a199C062f3eFdD288afCBce8',
-      stakeToken: '0x004626A008B1aCdC4c74ab51644093b155e59A23',
+      agToken: '0x1a7e4e63778b4f12a199c062f3efdd288afcbce8',
+      stakeToken: '0x004626a008b1acdc4c74ab51644093b155e59a23',
     },
     [Network.OPTIMISM]: {
       agToken: '0x9485aca5bbbe1667ad97c7fe7c4531a624c8b1ed',
-      stakeToken: '0x004626A008B1aCdC4c74ab51644093b155e59A23',
+      stakeToken: '0x004626a008b1acdc4c74ab51644093b155e59a23',
     },
     [Network.POLYGON]: {
       agToken: '0xe0b52e49357fd4daf2c15e02058dce6bc0057db4',
-      stakeToken: '0x004626A008B1aCdC4c74ab51644093b155e59A23',
+      stakeToken: '0x004626a008b1acdc4c74ab51644093b155e59a23',
     },
   },
 };
