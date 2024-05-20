b5670a4f11203135a67f15f84530da10e8952685
Alexander Burkut <a.burkut90@gmail.com>
2024-05-20 14:18:48 +0300

    add fix for factory

--- src/dex/uniswap-v3/forks/velodrome-slipstream/velodrome-slipstream-factory.ts
+++ src/dex/uniswap-v3/forks/velodrome-slipstream/velodrome-slipstream-factory.ts
@@ -1,10 +1,27 @@
-import { UniswapV3Factory } from '../../uniswap-v3-factory';
+import {
+  OnPoolCreatedCallback,
+  UniswapV3Factory,
+} from '../../uniswap-v3-factory';
 import { LogDescription } from 'ethers/lib/utils';
 import { Interface } from '@ethersproject/abi';
 import FactoryABI from '../../../../abi/velodrome-slipstream/VelodromeSlipstreamFactory.abi.json';
+import { IDexHelper } from '../../../../dex-helper';
+import { Address, Logger } from '../../../../types';
 
 export class VelodromeSlipstreamFactory extends UniswapV3Factory {
   public readonly factoryIface = new Interface(FactoryABI);
+
+  constructor(
+    readonly dexHelper: IDexHelper,
+    parentName: string,
+    protected readonly factoryAddress: Address,
+    logger: Logger,
+    protected readonly onPoolCreated: OnPoolCreatedCallback,
+    mapKey: string = '',
+  ) {
+    super(dexHelper, parentName, factoryAddress, logger, onPoolCreated, mapKey);
+    this.handlers['PoolCreated'] = this.handleNewPool.bind(this);
+  }
 
   async handleNewPool(event: LogDescription) {
     this.logger.info(`handle new pool event ${JSON.stringify(event)}`);
