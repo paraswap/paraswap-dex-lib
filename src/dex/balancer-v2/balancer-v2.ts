55c3271fc85efa43eab6c6952bb6022a28ca9f0c
Mohamed Hassen Mami <mohamed@paraswap.io>
2024-05-28 15:37:32 +0700

    v6/balancerV2: relax single swap condition to make it available for all chains

--- src/dex/balancer-v2/balancer-v2.ts
+++ src/dex/balancer-v2/balancer-v2.ts
@@ -1410,7 +1410,7 @@ export class BalancerV2
       hexZeroPad(hexlify(blockNumber), 16),
     ]);
 
-    const isSingleSwap = data.swaps.length === 1 && this.network === 137; // FIXME: release cond when all chains redeployed
+    const isSingleSwap = data.swaps.length === 1;
 
     const balancerParams = isSingleSwap
       ? this.getBalancerV2SwapParam(
