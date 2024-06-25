23cb9e1b7504cde347ee0033b828e1cc8429c97d
Yevhen Shulha <jackshel45@gmail.com>
2024-06-26 00:24:13 +0300

    fix: tests

--- tests/utils-e2e.ts
+++ tests/utils-e2e.ts
@@ -567,7 +567,6 @@ export async function testE2E(
     );
 
     const swapTx = await ts.simulate(swapParams);
-    if (!swapTx.success) console.log('url swap: ', swapTx.url);
     // Only log gas estimate if testing against API
     if (useAPI) {
       const gasUsed = swapTx.gasUsed || '0';
