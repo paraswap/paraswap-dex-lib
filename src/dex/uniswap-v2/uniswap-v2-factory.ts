c102a2d6803e94c54332bb32b1626857c97c8f80
Alexander Burkut <a.burkut90@gmail.com>
2024-06-25 16:00:49 +0300

    set masterPoolNeeded=false for UniV2 factory

--- src/dex/uniswap-v2/uniswap-v2-factory.ts
+++ src/dex/uniswap-v2/uniswap-v2-factory.ts
@@ -37,7 +37,14 @@ export class UniswapV2Factory extends StatefulEventSubscriber<FactoryState> {
     protected readonly onPoolCreated: OnPoolCreatedCallback,
     mapKey: string = '',
   ) {
-    super(parentName, `${parentName} Factory`, dexHelper, logger, true, mapKey);
+    super(
+      parentName,
+      `${parentName} Factory`,
+      dexHelper,
+      logger,
+      false,
+      mapKey,
+    );
 
     this.addressesSubscribed = [factoryAddress];
 
