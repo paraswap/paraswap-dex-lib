fc805fa263390d0249aadec0990ab0265ab93d8f
Yevhen Shulha <jackshel45@gmail.com>
2024-09-23 20:18:11 +0300

    fix: add integration tests

--- src/dex/spark/spark-integration.test.ts
+++ src/dex/spark/spark-integration.test.ts
@@ -10,20 +10,20 @@ import { Spark } from './spark';
 
 const network = Network.MAINNET;
 
-const SDaiSymbol = 'sDAI';
-const SDaiToken = Tokens[network][SDaiSymbol];
-
-const DaiSymbol = 'DAI';
-const DaiToken = Tokens[network][DaiSymbol];
-
 const amounts = [0n, BI_POWS[18], 2000000000000000000n];
 
-const dexKey = 'Spark';
 const dexHelper = new DummyDexHelper(network);
 let blocknumber: number;
 let spark: Spark;
 
 describe('Spark', function () {
+  const dexKey = 'Spark';
+  const SDaiSymbol = 'sDAI';
+  const SDaiToken = Tokens[network][SDaiSymbol];
+
+  const DaiSymbol = 'DAI';
+  const DaiToken = Tokens[network][DaiSymbol];
+
   beforeAll(async () => {
     blocknumber = await dexHelper.web3Provider.eth.getBlockNumber();
     spark = new Spark(network, dexKey, dexHelper);
@@ -140,6 +140,140 @@ describe('Spark', function () {
   });
 
   it('SDai getTopPoolsForToken', async function () {
+    const poolLiquidity = await spark.getTopPoolsForToken(
+      SDaiToken.address,
+      10,
+    );
+    console.log(`${SDaiSymbol} Top Pools:`, poolLiquidity);
+
+    checkPoolsLiquidity(poolLiquidity, SDaiToken.address, dexKey);
+  });
+});
+
+describe('sUSDS', function () {
+  const dexKey = 'sUSDS';
+  const SDaiSymbol = 'USDS';
+  const SDaiToken = Tokens[network][SDaiSymbol];
+
+  const DaiSymbol = 'sUSDS';
+  const DaiToken = Tokens[network][DaiSymbol];
+
+  beforeAll(async () => {
+    blocknumber = await dexHelper.web3Provider.eth.getBlockNumber();
+    spark = new Spark(network, dexKey, dexHelper);
+    if (spark.initializePricing) {
+      await spark.initializePricing(blocknumber);
+    }
+  });
+
+  it('getPoolIdentifiers and getPricesVolume USDS -> sUSDS SELL', async function () {
+    const pools = await spark.getPoolIdentifiers(
+      DaiToken,
+      SDaiToken,
+      SwapSide.SELL,
+      blocknumber,
+    );
+    console.log(`${DaiSymbol} <> ${SDaiSymbol} Pool Identifiers: `, pools);
+
+    expect(pools.length).toBeGreaterThan(0);
+
+    const poolPrices = await spark.getPricesVolume(
+      DaiToken,
+      SDaiToken,
+      amounts,
+      SwapSide.SELL,
+      blocknumber,
+      pools,
+    );
+    console.log(`${DaiSymbol} <> ${SDaiSymbol} Pool Prices: `, poolPrices);
+
+    expect(poolPrices).not.toBeNull();
+    checkPoolPrices(poolPrices!, amounts, SwapSide.SELL, dexKey);
+  });
+
+  it('getPoolIdentifiers and getPricesVolume sUSDS -> USDS SELL', async function () {
+    const pools = await spark.getPoolIdentifiers(
+      SDaiToken,
+      DaiToken,
+      SwapSide.SELL,
+      blocknumber,
+    );
+    console.log(`${SDaiSymbol} <> ${DaiSymbol} Pool Identifiers: `, pools);
+
+    expect(pools.length).toBeGreaterThan(0);
+
+    const poolPrices = await spark.getPricesVolume(
+      SDaiToken,
+      DaiToken,
+      amounts,
+      SwapSide.SELL,
+      blocknumber,
+      pools,
+    );
+    console.log(`${SDaiSymbol} <> ${DaiSymbol} Pool Prices: `, poolPrices);
+
+    expect(poolPrices).not.toBeNull();
+    checkPoolPrices(poolPrices!, amounts, SwapSide.SELL, dexKey);
+  });
+
+  it('getPoolIdentifiers and getPricesVolume USDS -> sUSDS BUY', async function () {
+    const pools = await spark.getPoolIdentifiers(
+      DaiToken,
+      SDaiToken,
+      SwapSide.BUY,
+      blocknumber,
+    );
+    console.log(`${DaiSymbol} <> ${SDaiSymbol} Pool Identifiers: `, pools);
+
+    expect(pools.length).toBeGreaterThan(0);
+
+    const poolPrices = await spark.getPricesVolume(
+      DaiToken,
+      SDaiToken,
+      amounts,
+      SwapSide.BUY,
+      blocknumber,
+      pools,
+    );
+    console.log(`${DaiSymbol} <> ${SDaiSymbol} Pool Prices: `, poolPrices);
+
+    expect(poolPrices).not.toBeNull();
+    checkPoolPrices(poolPrices!, amounts, SwapSide.BUY, dexKey);
+  });
+
+  it('getPoolIdentifiers and getPricesVolume sUSDS -> USDS BUY', async function () {
+    const pools = await spark.getPoolIdentifiers(
+      SDaiToken,
+      DaiToken,
+      SwapSide.BUY,
+      blocknumber,
+    );
+    console.log(`${SDaiSymbol} <> ${DaiSymbol} Pool Identifiers: `, pools);
+
+    expect(pools.length).toBeGreaterThan(0);
+
+    const poolPrices = await spark.getPricesVolume(
+      SDaiToken,
+      DaiToken,
+      amounts,
+      SwapSide.BUY,
+      blocknumber,
+      pools,
+    );
+    console.log(`${SDaiSymbol} <> ${DaiSymbol} Pool Prices: `, poolPrices);
+
+    expect(poolPrices).not.toBeNull();
+    checkPoolPrices(poolPrices!, amounts, SwapSide.BUY, dexKey);
+  });
+
+  it('USDS getTopPoolsForToken', async function () {
+    const poolLiquidity = await spark.getTopPoolsForToken(DaiToken.address, 10);
+    console.log(`${DaiSymbol} Top Pools:`, poolLiquidity);
+
+    checkPoolsLiquidity(poolLiquidity, DaiToken.address, dexKey);
+  });
+
+  it('sUSDS getTopPoolsForToken', async function () {
     const poolLiquidity = await spark.getTopPoolsForToken(
       SDaiToken.address,
       10,
