2a36478231d05c5fadc2c0c6dc29a915defb9e42
johngrantuk <johngrantuk@googlemail.com>
2024-10-11 14:25:18 +0100

    debug: Add run-dex-integration-v3 script.

---
+++ scripts/run-dex-integration-v3.ts
@@ -0,0 +1,68 @@
+// npx ts-node scripts/run-dex-integration-v3.ts
+/* eslint-disable no-console */
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { Network, SwapSide } from '../src/constants';
+import { BalancerV3 } from '../src/dex/balancer-v3/balancer-v3';
+import { DummyDexHelper } from '../src/dex-helper/index';
+import { BI_POWS } from '../src/bigint-constants';
+
+const stataUSDC = {
+  address: '0x8a88124522dbbf1e56352ba3de1d9f78c143751e'.toLowerCase(),
+  decimals: 6,
+};
+
+const stataDAI = {
+  address: '0xde46e43f46ff74a23a65ebb0580cbe3dfe684a17'.toLowerCase(),
+  decimals: 18,
+};
+
+const bal = {
+  address: '0xb19382073c7a0addbb56ac6af1808fa49e377b75'.toLowerCase(),
+  decimals: 18,
+};
+
+const daiAave = {
+  address: '0xff34b3d4aee8ddcd6f9afffb6fe49bd371b8a357'.toLowerCase(),
+  decimals: 18,
+};
+
+const amounts = [0n, BI_POWS[18], 2000000000000000000n];
+
+async function main() {
+  const dexHelper = new DummyDexHelper(Network.SEPOLIA);
+  const blocknumber = await dexHelper.web3Provider.eth.getBlockNumber();
+
+  const balancerV3 = new BalancerV3(Network.SEPOLIA, 'BalancerV3', dexHelper);
+
+  await balancerV3.initializePricing(blocknumber);
+
+  // const from = stataDAI;
+  // const to = stataUSDC;
+  const from = bal;
+  const to = daiAave;
+
+  const pools = await balancerV3.getPoolIdentifiers(
+    from,
+    to,
+    SwapSide.SELL,
+    blocknumber,
+  );
+  console.log('Pool Identifiers: ', from.address, to.address, pools);
+
+  const prices = await balancerV3.getPricesVolume(
+    from,
+    to,
+    amounts,
+    SwapSide.SELL,
+    blocknumber,
+    pools,
+  );
+  console.log('Pool Prices: ', prices);
+
+  const poolLiquidity = await balancerV3.getTopPoolsForToken(from.address, 10);
+  console.log('Top Pools:', poolLiquidity);
+}
+
+main();
