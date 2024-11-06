344590ee4e8f38206955b115f15367b807594f14
Danylo Kanievskyi <danylokaniev@gmail.com>
2024-11-06 20:28:35 +0200

    feat: add guard on max iterations on AlgebraMath

--- src/dex/algebra/lib/AlgebraMath.ts
+++ src/dex/algebra/lib/AlgebraMath.ts
@@ -579,7 +579,11 @@ class AlgebraMathClass {
       tickCount: 0,
     };
     // swap until there is remaining input or output tokens or we reach the price limit
+    let iterationsCount = 0;
     while (true) {
+      iterationsCount++;
+      _require(iterationsCount < 100, 'Max iteration reached on AlgebraMath');
+
       step.stepSqrtPrice = currentPrice;
 
       //equivalent of tickTable.nextTickInTheSameRow(currentTick, zeroToOne);
