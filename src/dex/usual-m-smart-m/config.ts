a5f4f2d30f56aa00c5088353706a97db01d9c15c
toninorair <antonina.norair@gmail.com>
2024-11-26 21:22:57 -0500

    Provide mainnet address of UsualM

--- src/dex/usual-m-smart-m/config.ts
+++ src/dex/usual-m-smart-m/config.ts
@@ -6,7 +6,7 @@ export const UsualMSmartMConfig: DexConfigMap<DexParams> = {
   UsualMSmartM: {
     [Network.MAINNET]: {
       smartMAddress: '0x437cc33344a0B27A429f795ff6B469C72698B291',
-      usualMAddress: '0x0000000000000000000000000000000000000000', //TODO: replace with actual address
+      usualMAddress: '0xFe274C305b365dC38e188E8f01c4FAe2171ce927',
     },
   },
 };
--- src/dex/usual-m-usd0/config.ts
+++ src/dex/usual-m-usd0/config.ts
@@ -5,7 +5,7 @@ import { Network, SwapSide } from '../../constants';
 export const UsualMUsd0Config: DexConfigMap<DexParams> = {
   UsualMUsd0: {
     [Network.MAINNET]: {
-      usualMAddress: '0x0000000000000000000000000000000000000000', //TODO: replace with actual address
+      usualMAddress: '0xFe274C305b365dC38e188E8f01c4FAe2171ce927',
       usd0Address: '0x73A15FeD60Bf67631dC6cd7Bc5B6e8da8190aCF5',
       usualDaoCollateralAddress: '0xde6e1F680C4816446C8D515989E2358636A38b04',
     },
