60f1a24a0f69f202e2fedaab624698a647dbf80b
nazariikyselevych <nazarkysel@gmail.com>
2025-01-29 02:45:20 +0200

    test: more sample estimation tests

---
+++ src/dex/uniswap-v3/uniswap-v3-gas-estimation.test.ts
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+import dotenv from 'dotenv';
+import { testGasEstimation } from '../../../tests/utils-e2e';
+import { Tokens } from '../../../tests/constants-e2e';
+import { Network, SwapSide } from '../../constants';
+import { ContractMethodV6 } from '@paraswap/core';
+
+dotenv.config();
+
+describe('UniswapV3 Gas Estimation', () => {
+  const dexKey = 'UniswapV3';
+  const network = Network.MAINNET;
+  const srcToken = Tokens[network]['USDC'];
+  const destToken = Tokens[network]['USDT'];
+  const methods: ContractMethodV6[] = [
+    ContractMethodV6.swapExactAmountInOnUniswapV3,
+    ContractMethodV6.swapExactAmountIn,
+  ];
+  const amount = 1111100000n;
+
+  methods.forEach(async method => {
+    it(method, async () => {
+      await testGasEstimation(
+        network,
+        srcToken,
+        destToken,
+        amount,
+        SwapSide.SELL,
+        dexKey,
+        method,
+      );
+    });
+  });
+});
