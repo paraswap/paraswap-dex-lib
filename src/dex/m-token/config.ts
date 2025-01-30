ff92d4054bb653a53f19b433e4b3d4c6e8751410
Jonathan Alvarez <2167222+jonalvarezz@users.noreply.github.com>
2025-01-30 08:35:22 -0500

    init-integration
    Signed-off-by: Jonathan Alvarez <2167222+jonalvarezz@users.noreply.github.com>

---
+++ src/dex/m-token/config.ts
@@ -0,0 +1,9 @@
+import { DexParams } from './types';
+import { DexConfigMap } from '../../types';
+import { Network, SwapSide } from '../../constants';
+
+export const MWrappedMConfig: DexConfigMap<DexParams> = {
+  MWrappedM: {
+    // TODO: complete me!
+  },
+};
