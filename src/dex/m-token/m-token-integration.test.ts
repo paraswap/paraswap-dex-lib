9b6e958330f4bc9344bb76d6f38589e420a290c5
Jonathan Alvarez <2167222+jonalvarezz@users.noreply.github.com>
2025-01-31 14:20:31 -0500

    tests: add m-token integration tests
    Signed-off-by: Jonathan Alvarez <2167222+jonalvarezz@users.noreply.github.com>

--- src/dex/m-token/m-token-integration.test.ts
+++ src/dex/m-token/m-token-integration.test.ts
@@ -2,10 +2,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { Interface, Result } from '@ethersproject/abi';
 import { DummyDexHelper } from '../../dex-helper/index';
 import { Network, SwapSide } from '../../constants';
 import { BI_POWS } from '../../bigint-constants';
+import { MToken } from './m-token';
 import { MWrappedM } from './m-wrapped-m';
 import {
   checkPoolPrices,
@@ -14,84 +14,8 @@ import {
 } from '../../../tests/utils';
 import { Tokens } from '../../../tests/constants-e2e';
 
-/*
-  README
-  ======
-
-  This test script adds tests for MWrappedM general integration
-  with the DEX interface. The test cases below are example tests.
-  It is recommended to add tests which cover MWrappedM specific
-  logic.
-
-  You can run this individual test script by running:
-  `npx jest src/dex/<dex-name>/<dex-name>-integration.test.ts`
-
-  (This comment should be removed from the final implementation)
-*/
-
-function getReaderCalldata(
-  exchangeAddress: string,
-  readerIface: Interface,
-  amounts: bigint[],
-  funcName: string,
-  // TODO: Put here additional arguments you need
-) {
-  return amounts.map(amount => ({
-    target: exchangeAddress,
-    callData: readerIface.encodeFunctionData(funcName, [
-      // TODO: Put here additional arguments to encode them
-      amount,
-    ]),
-  }));
-}
-
-function decodeReaderResult(
-  results: Result,
-  readerIface: Interface,
-  funcName: string,
-) {
-  // TODO: Adapt this function for your needs
-  return results.map(result => {
-    const parsed = readerIface.decodeFunctionResult(funcName, result);
-    return BigInt(parsed[0]._hex);
-  });
-}
-
-async function checkOnChainPricing(
-  mWrappedM: MWrappedM,
-  funcName: string,
-  blockNumber: number,
-  prices: bigint[],
-  amounts: bigint[],
-) {
-  const exchangeAddress = ''; // TODO: Put here the real exchange address
-
-  // TODO: Replace dummy interface with the real one
-  // Normally you can get it from mWrappedM.Iface or from eventPool.
-  // It depends on your implementation
-  const readerIface = new Interface('');
-
-  const readerCallData = getReaderCalldata(
-    exchangeAddress,
-    readerIface,
-    amounts.slice(1),
-    funcName,
-  );
-  const readerResult = (
-    await mWrappedM.dexHelper.multiContract.methods
-      .aggregate(readerCallData)
-      .call({}, blockNumber)
-  ).returnData;
-
-  const expectedPrices = [0n].concat(
-    decodeReaderResult(readerResult, readerIface, funcName),
-  );
-
-  expect(prices).toEqual(expectedPrices);
-}
-
 async function testPricingOnNetwork(
-  mWrappedM: MWrappedM,
+  mWrappedM: MToken,
   network: Network,
   dexKey: string,
   blockNumber: number,
@@ -137,13 +61,7 @@ async function testPricingOnNetwork(
   }
 
   // Check if onchain pricing equals to calculated ones
-  await checkOnChainPricing(
-    mWrappedM,
-    funcNameToCheck,
-    blockNumber,
-    poolPrices![0].prices,
-    amounts,
-  );
+  checkPoolPrices(poolPrices!, amounts, side, dexKey);
 }
 
 describe('MWrappedM', function () {
@@ -157,10 +75,10 @@ describe('MWrappedM', function () {
 
     const tokens = Tokens[network];
 
-    // TODO: Put here token Symbol to check against
+    const srcTokenSymbol = 'M';
+    const destTokenSymbol = 'WrappedM';
+
     // Don't forget to update relevant tokens in constant-e2e.ts
-    const srcTokenSymbol = 'srcTokenSymbol';
-    const destTokenSymbol = 'destTokenSymbol';
 
     const amountsForSell = [
       0n,
@@ -176,25 +94,11 @@ describe('MWrappedM', function () {
       10n * BI_POWS[tokens[srcTokenSymbol].decimals],
     ];
 
-    const amountsForBuy = [
-      0n,
-      1n * BI_POWS[tokens[destTokenSymbol].decimals],
-      2n * BI_POWS[tokens[destTokenSymbol].decimals],
-      3n * BI_POWS[tokens[destTokenSymbol].decimals],
-      4n * BI_POWS[tokens[destTokenSymbol].decimals],
-      5n * BI_POWS[tokens[destTokenSymbol].decimals],
-      6n * BI_POWS[tokens[destTokenSymbol].decimals],
-      7n * BI_POWS[tokens[destTokenSymbol].decimals],
-      8n * BI_POWS[tokens[destTokenSymbol].decimals],
-      9n * BI_POWS[tokens[destTokenSymbol].decimals],
-      10n * BI_POWS[tokens[destTokenSymbol].decimals],
-    ];
-
     beforeAll(async () => {
       blockNumber = await dexHelper.web3Provider.eth.getBlockNumber();
       mWrappedM = new MWrappedM(network, dexKey, dexHelper);
       if (mWrappedM.initializePricing) {
-        await mWrappedM.initializePricing(blockNumber);
+        await mWrappedM.initializePricing();
       }
     });
 
@@ -208,44 +112,56 @@ describe('MWrappedM', function () {
         destTokenSymbol,
         SwapSide.SELL,
         amountsForSell,
-        '', // TODO: Put here proper function name to check pricing
+        '',
       );
     });
 
-    it('getPoolIdentifiers and getPricesVolume BUY', async function () {
-      await testPricingOnNetwork(
-        mWrappedM,
-        network,
-        dexKey,
-        blockNumber,
-        srcTokenSymbol,
-        destTokenSymbol,
-        SwapSide.BUY,
-        amountsForBuy,
-        '', // TODO: Put here proper function name to check pricing
-      );
-    });
-
-    it('getTopPoolsForToken', async function () {
-      // We have to check without calling initializePricing, because
-      // pool-tracker is not calling that function
+    it('getTopPoolsForToken: M', async function () {
       const newMWrappedM = new MWrappedM(network, dexKey, dexHelper);
       if (newMWrappedM.updatePoolState) {
         await newMWrappedM.updatePoolState();
       }
+
+      const symbol = srcTokenSymbol;
+
       const poolLiquidity = await newMWrappedM.getTopPoolsForToken(
-        tokens[srcTokenSymbol].address,
+        tokens[symbol].address,
         10,
       );
-      console.log(`${srcTokenSymbol} Top Pools:`, poolLiquidity);
+      console.log(
+        `${symbol} Top Pools:`,
+        JSON.stringify(poolLiquidity, null, 2),
+      );
 
-      if (!newMWrappedM.hasConstantPriceLargeAmounts) {
-        checkPoolsLiquidity(
-          poolLiquidity,
-          Tokens[network][srcTokenSymbol].address,
-          dexKey,
-        );
+      checkPoolsLiquidity(
+        poolLiquidity,
+        Tokens[network][symbol].address,
+        dexKey,
+      );
+    });
+
+    it('getTopPoolsForToken: WrappedM', async function () {
+      const newMWrappedM = new MWrappedM(network, dexKey, dexHelper);
+      if (newMWrappedM.updatePoolState) {
+        await newMWrappedM.updatePoolState();
       }
+
+      const symbol = destTokenSymbol;
+
+      const poolLiquidity = await newMWrappedM.getTopPoolsForToken(
+        tokens[symbol].address,
+        10,
+      );
+      console.log(
+        `${symbol} Top Pools:`,
+        JSON.stringify(poolLiquidity, null, 2),
+      );
+
+      checkPoolsLiquidity(
+        poolLiquidity,
+        Tokens[network][symbol].address,
+        dexKey,
+      );
     });
   });
 });
