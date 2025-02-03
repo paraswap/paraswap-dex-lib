32582df7dcc19e470e8bc4ac1fc7a6a6541d5c93
nazariikyselevych <nazarkysel@gmail.com>
2025-02-03 18:17:57 +0200

    test: multipath tests

--- src/dex/uniswap-v2/uniswap-v2-gas-estimation.test.ts
+++ src/dex/uniswap-v2/uniswap-v2-gas-estimation.test.ts
@@ -1,58 +1,98 @@
 /* eslint-disable no-console */
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import { testGasEstimation } from '../../../tests/utils-e2e';
 import { Tokens } from '../../../tests/constants-e2e';
 import { Network, SwapSide } from '../../constants';
 import { ContractMethodV6 } from '@paraswap/core';
 import { ethers } from 'ethers';
 
+dotenv.config();
+
 describe('UniswapV2 Gas Estimation', () => {
   const dexKey = 'UniswapV2';
   const network = Network.MAINNET;
+  const tokens = Tokens[network];
+  const ETH = tokens['ETH'];
+  const WETH = tokens['WETH'];
+  const USDT = tokens['USDT'];
+  const USDC = tokens['USDC'];
+  const WBTC = tokens['WBTC'];
+  const amount = 100000000n; // 100 usdt
 
-  describe('swapExactAmountInOnUniswapV2', () => {
-    const ETH = Tokens[network]['ETH'];
-    const WETH = Tokens[network]['WETH'];
-    const USDT = Tokens[network]['USDT'];
-    const USDC = Tokens[network]['USDC'];
-    const amount = 100000000n; // 100 usdt
+  const methods: ContractMethodV6[] = [
+    ContractMethodV6.swapExactAmountInOnUniswapV2,
+    ContractMethodV6.swapExactAmountIn,
+  ];
 
-    it('one swap', async () => {
-      await testGasEstimation(
-        network,
-        USDT,
-        USDC,
-        amount,
-        SwapSide.SELL,
-        dexKey,
-        ContractMethodV6.swapExactAmountInOnUniswapV2,
-      );
-    });
+  methods.forEach(async method => {
+    describe(method, () => {
+      it('one swap', async () => {
+        await testGasEstimation(
+          network,
+          USDT,
+          USDC,
+          amount,
+          SwapSide.SELL,
+          dexKey,
+          method,
+        );
+      });
 
-    it('one swap with unwrap', async () => {
-      await testGasEstimation(
-        network,
-        USDT,
-        ETH,
-        amount,
-        SwapSide.SELL,
-        dexKey,
-        ContractMethodV6.swapExactAmountInOnUniswapV2,
-      );
-    });
+      it('one swap with unwrap', async () => {
+        await testGasEstimation(
+          network,
+          USDT,
+          ETH,
+          amount,
+          SwapSide.SELL,
+          dexKey,
+          method,
+        );
+      });
 
-    it('one swap with wrap', async () => {
-      const oneEth = ethers.utils.parseEther('1').toBigInt();
+      it('one swap with wrap', async () => {
+        const oneEth = ethers.utils.parseEther('1').toBigInt();
 
-      await testGasEstimation(
-        network,
-        ETH,
-        USDT,
-        oneEth,
-        SwapSide.SELL,
-        dexKey,
-        ContractMethodV6.swapExactAmountInOnUniswapV2,
-      );
+        await testGasEstimation(
+          network,
+          ETH,
+          USDT,
+          oneEth,
+          SwapSide.SELL,
+          dexKey,
+          method,
+        );
+      });
+
+      it('two swaps', async () => {
+        const route = [USDT.address, WETH.address, USDC.address];
+
+        await testGasEstimation(
+          network,
+          USDT,
+          USDC,
+          amount,
+          SwapSide.SELL,
+          dexKey,
+          method,
+          route,
+        );
+      });
+
+      it('three swaps', async () => {
+        const route = [USDT.address, WETH.address, WBTC.address, USDC.address];
+
+        await testGasEstimation(
+          network,
+          USDT,
+          USDC,
+          amount,
+          SwapSide.SELL,
+          dexKey,
+          method,
+          route,
+        );
+      });
     });
   });
 });
--- src/dex/uniswap-v3/uniswap-v3-gas-estimation.test.ts
+++ src/dex/uniswap-v3/uniswap-v3-gas-estimation.test.ts
@@ -4,55 +4,95 @@ import { testGasEstimation } from '../../../tests/utils-e2e';
 import { Tokens } from '../../../tests/constants-e2e';
 import { Network, SwapSide } from '../../constants';
 import { ContractMethodV6 } from '@paraswap/core';
+import { ethers } from 'ethers';
 
 dotenv.config();
 
 describe('UniswapV3 Gas Estimation', () => {
   const dexKey = 'UniswapV3';
   const network = Network.MAINNET;
-  const srcToken = Tokens[network]['USDC'];
-  const destToken = Tokens[network]['USDT'];
+  const tokens = Tokens[network];
+  const ETH = tokens['ETH'];
+  const WETH = tokens['WETH'];
+  const USDT = tokens['USDT'];
+  const USDC = tokens['USDC'];
+  const WBTC = tokens['WBTC'];
+  const amount = 100000000n; // 100 usdt
+
   const methods: ContractMethodV6[] = [
     ContractMethodV6.swapExactAmountInOnUniswapV3,
     ContractMethodV6.swapExactAmountIn,
   ];
-  const amount = 1111100000n;
 
   methods.forEach(async method => {
-    it(method, async () => {
-      await testGasEstimation(
-        network,
-        srcToken,
-        destToken,
-        amount,
-        SwapSide.SELL,
-        dexKey,
-        method,
-      );
+    describe(method, () => {
+      it('one swap', async () => {
+        await testGasEstimation(
+          network,
+          USDT,
+          USDC,
+          amount,
+          SwapSide.SELL,
+          dexKey,
+          method,
+        );
+      });
+
+      it('one swap with unwrap', async () => {
+        await testGasEstimation(
+          network,
+          USDT,
+          ETH,
+          amount,
+          SwapSide.SELL,
+          dexKey,
+          method,
+        );
+      });
+
+      it('one swap with wrap', async () => {
+        const oneEth = ethers.utils.parseEther('1').toBigInt();
+
+        await testGasEstimation(
+          network,
+          ETH,
+          USDT,
+          oneEth,
+          SwapSide.SELL,
+          dexKey,
+          method,
+        );
+      });
+
+      it('two swaps', async () => {
+        const route = [USDT.address, WETH.address, USDC.address];
+
+        await testGasEstimation(
+          network,
+          USDT,
+          USDC,
+          amount,
+          SwapSide.SELL,
+          dexKey,
+          method,
+          route,
+        );
+      });
+
+      it('three swaps', async () => {
+        const route = [USDT.address, WETH.address, WBTC.address, USDC.address];
+
+        await testGasEstimation(
+          network,
+          USDT,
+          USDC,
+          amount,
+          SwapSide.SELL,
+          dexKey,
+          method,
+          route,
+        );
+      });
     });
-  });
-
-  it('Wrap `swapExactAmountInOnUniswapV3`', async () => {
-    await testGasEstimation(
-      network,
-      Tokens[network]['ETH'],
-      destToken,
-      100000000000000000n,
-      SwapSide.SELL,
-      dexKey,
-      ContractMethodV6.swapExactAmountInOnUniswapV3,
-    );
-  });
-
-  it('Unwrap `swapExactAmountInOnUniswapV3`', async () => {
-    await testGasEstimation(
-      network,
-      srcToken,
-      Tokens[network]['ETH'],
-      amount,
-      SwapSide.SELL,
-      dexKey,
-      ContractMethodV6.swapExactAmountInOnUniswapV3,
-    );
   });
 });
