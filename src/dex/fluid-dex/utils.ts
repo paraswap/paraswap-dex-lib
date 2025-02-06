bc35274982625f4b5e1523ce1ec2b13786e28575
Samarendra Gouda <samarendragouda@gmail.com>
2025-02-06 16:21:07 +0530

    fix: update normalisation logic

--- src/dex/fluid-dex/utils.ts
+++ src/dex/fluid-dex/utils.ts
@@ -18,8 +18,9 @@ export function adjustTestSwapOutAmount(
   amountOut: bigint,
   outDecimals: number,
 ) {
+  let normalisationDecimalFactor = outDecimals > 12 ? outDecimals - 12 : 0;
   return (
     ((amountOut * BigInt(10 ** 12)) / BigInt(10 ** outDecimals)) *
-    BigInt(10 ** Math.abs(outDecimals - 12))
+    BigInt(10 ** normalisationDecimalFactor)
   ).toString();
 }
