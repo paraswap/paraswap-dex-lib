b2cc499d29a342b9156c90dd896337f55762b16a
Danylo Kanievskyi <danylokaniev@gmail.com>
2025-04-03 17:05:21 +0300

    fix: add USDC as base token on CurveV1 for pool-tracker

--- src/dex/curve-v1/config.ts
+++ src/dex/curve-v1/config.ts
@@ -28,6 +28,11 @@ export const CurveV1Config: DexConfigMap<DexParams> = {
   CurveV1: {
     [Network.MAINNET]: {
       baseTokens: {
+        '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+          address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+          decimals: 6,
+          reasonableVolume: 1000000000n,
+        },
         '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': {
           address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
           decimals: 18,
