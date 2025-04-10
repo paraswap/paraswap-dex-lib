9464493dfe3e11ab079e690b32d8ecd379ea38fe
Alexander Burkut <a.burkut90@gmail.com>
2025-04-10 09:58:47 +0300

    rollback uni v3 changes

--- src/dex/uniswap-v3/uniswap-v3-pool.ts
+++ src/dex/uniswap-v3/uniswap-v3-pool.ts
@@ -382,7 +382,6 @@ export class UniswapV3EventPool extends StatefulEventSubscriber<PoolState> {
     const amount1 = bigIntify(event.args.amount1);
     const newTick = bigIntify(event.args.tick);
     const newLiquidity = bigIntify(event.args.liquidity);
-
     pool.blockTimestamp = bigIntify(blockHeader.timestamp);
 
     if (amount0 <= 0n && amount1 <= 0n) {
