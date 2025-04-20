8f9698d86252788100cda2a6efc12768775e18a7
hart <chocolly@163.com>
2025-04-20 11:40:46 +0800

    feat: add ring-v2 init code base on template

---
+++ src/dex/ring-v2/config.ts
@@ -0,0 +1,9 @@
+import { DexParams } from './types';
+import { DexConfigMap } from '../../types';
+import { Network, SwapSide } from '../../constants';
+
+export const RingV2Config: DexConfigMap<DexParams> = {
+  RingV2: {
+    // TODO: complete me!
+  },
+};
