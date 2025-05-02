a2e5dd5ab450e9cc92d8d36292ec73c47b55820d
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2025-05-02 17:19:30 +0300

    fix: remove unused `returnAmountPos` param

--- src/dex/miro-migrator/miro-migrator.ts
+++ src/dex/miro-migrator/miro-migrator.ts
@@ -201,7 +201,6 @@ export class MiroMigrator
       dexFuncHasRecipient: false,
       exchangeData: swapData,
       targetExchange: this.migratorAddress,
-      returnAmountPos: undefined,
     };
   }
 
